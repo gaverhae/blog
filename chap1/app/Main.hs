17383d1fa2b927f96eea707328203c094b3b25c1
Gary Verhaegen <gary.verhaegen@gmail.com>
2019-06-06 18:25:51 +0100

    thrice broken

--- chap1/app/Main.hs
+++ chap1/app/Main.hs
@@ -11,10 +11,10 @@ eval :: E -> (String -> E) -> E
 eval exp env = case exp of
   Var s -> env s
   Abs s e -> exp
-  App abs arg -> case abs of
+  App abs arg -> case eval abs env of
     Abs n e -> eval e (\s -> if s == n then arg else env s)
     _ -> error $ "Trying to apply non-function value: " ++ show abs
-  Add a b -> case (eval a env, b) of
+  Add a b -> case (eval a env, eval b env) of
     (Num x, Num y) -> Num (x + y)
     _ -> error $ "Trying to add non-numbers: (" ++ show a ++ ", " ++ show b ++ ")"
   Num i -> Num i
@@ -37,4 +37,21 @@ main = do
   -- (\x.x + 1) 10 => 11
   assertExp (App (Abs "x" (Add (Var "x") (Num 1))) (Num 10))
             (Num 11)
+  -- (\x.x) (\y.y) => (\y.y)
+  assertExp (App (Abs "x" (Var "x")) (Abs "y" (Var "y")))
+            (Abs "y" (Var "y"))
+  -- faking names with extra nesting
+  -- thrice = \f.\x.(f(f(f x)))
+  -- inc = \x.(x + 1)
+  -- thrice inc 100
+  -- \thrice.\inc.(thrice inc 100) (\f\x.(f(f(f x)))) (\x. x + 1)
+--  assertExp (App (Abs "thrice" (App (Abs "inc" (App (App (Var "thrice")
+--                                                         (Var "inc"))
+--                                                    (Num 100)))
+--                                    (Abs "x" (Add (Var "x") (Num 1)))))
+--                 (Abs "f" (Abs "x" (App (Var "f")
+--                                        (App (Var "f")
+--                                             (App (Var "f")
+--                                                  (Var "x")))))))
+--            (Num 103)
   putStrLn "All good"
