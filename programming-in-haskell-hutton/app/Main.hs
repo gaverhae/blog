1e00601277c7b059dd1084d9ed934195c52d8554
Gary Verhaegen <gary.verhaegen@gmail.com>
2019-07-03 18:51:54 +0100

    pih/c3

--- programming-in-haskell-hutton/app/Main.hs
+++ programming-in-haskell-hutton/app/Main.hs
@@ -84,11 +84,76 @@ last xs = head (drop (length xs - 1) xs)
 last xs = head (reverse xs)
 
 5. Show how the library function init that removes the last element from a
-non-empty list could similarly be defined in two different ways.
+  non-empty list could similarly be defined in two different ways.
 
 init xs = take (length xs - 1) xs
 
 init xs = reverse (tail (reverse xs))
+
+-}
+
+{-
+
+# Chapter 3
+
+1. What are the types of the following values?
+  ['a', 'b', 'c']
+  ('a', 'b', 'c')
+  [(False , 'O'), (True , '1')]
+  ([False, True], ['0', '1'])
+  [tail, init, reverse]
+
+['a', 'b', 'c'] :: [Char]
+('a', 'b', 'c') :: (Char, Char, Char)
+[(False , 'O'), (True , '1')] :: [(Bool, Char)]
+([False, True], ['0', '1']) :: ([Bool], [Char])
+[tail, init, reverse] :: [[a] -> [a]]
+
+2. What are the types of the following functions?
+  second xs = head (tail xs)
+  swap (x,y) = (y,x)
+  pair x y = (x,y)
+  double x = x * 2
+  palindrome xs = reverse xs == xs
+  twice f x = f(fx)
+  Hint: take care to include the necessary class constraints if the functions
+  are defined using overloaded operators.
+
+second :: [a] -> a
+second xs = head (tail xs)
+
+swap :: (a, b) -> (b, a)
+swap (x,y) = (y,x)
+
+pair :: a -> b -> (a, b)
+pair x y = (x,y)
+
+double :: Num a => a -> a
+double x = x * 2
+
+palindrome :: [a] -> Bool
+palindrome xs = reverse xs == xs
+
+twice :: (a -> a) -> a -> a
+twice f x = f(fx)
+
+3. Check your answers to the preceding two questions using Hugs.
+
+:shrug:
+
+4. Why is it not feasible in general for function types to be instances of the
+  Eq class? When is it feasible? Hint: two functions of the same type are equal
+  if they always return equal results for equal arguments.
+
+It could be feasible if we defined function equality as syntactic (i.e. literal
+string) equality, or as alpha equivalence. However, as we (apparently) want to
+define function equality based on external behaviour, checking two arbitrary
+functions for equality would require either testing both of them for all
+possible inputs (easy for input types like Bool or Int but harder for types
+like Integer) or some sort of automated theorem prover that can test arbitrary
+expressions for equivalence. Both approaches would result in very long calls to
+(==), and neither could be guaranteeed correct for all types in the current
+state of mathematical knowledge.
 
 -}
 
