ed7752770ac548744d6a9b6d56ac6f94e6eb5b23
Gary Verhaegen <gary.verhaegen@gmail.com>
2019-08-19 19:17:49 +0100

    hr/abbr/haskell: attempt 2: memoize

--- hackerrank/abbr/haskell/app/Main.hs
+++ hackerrank/abbr/haskell/app/Main.hs
@@ -13,20 +13,30 @@ import System.Environment
 import System.IO
 import System.IO.Unsafe
 import qualified Data.Char as Char
+import qualified Data.Set as Set
 
 -- Complete the abbreviation function below.
-abbreviation :: String -> String -> String
-abbreviation x y = case ab x y of
-  True -> "YES"
-  False -> "NO"
-  where ab [] [] = True
-        ab [] bs = False
-        ab (a:as) [] | Char.isUpper a = False
-                     | otherwise = ab as []
-        ab (a:as) (b:bs) | a == b = ab as bs
-                         | Char.isUpper a = False
-                         | otherwise = ab ((Char.toUpper a):as) (b:bs)
-                                       || ab as (b:bs)
+abbreviation :: String -> String -> Bool
+abbreviation x y =
+  snd $ h mem x y
+    where
+      mem = Set.empty :: Set (String, String)
+      h mem' x y =
+        if Set.member (x,y) mem'
+        then (mem', False)
+        else let mem'' = Set.insert (x,y) mem'
+             in case (x, y) of
+               ([], []) -> (mem'', True)
+               ([], y) -> (mem'', False)
+               (x, []) -> (mem'', all Char.isLower x)
+               (a:as, b:bs) | a == b -> h mem'' as bs
+                            | Char.isUpper a -> (mem'', False)
+                            | otherwise -> case h mem'' ((Char.toUpper a):as) (b:bs) of
+                                             (mem''', True) -> (mem''', True)
+                                             (mem''', False) -> h mem''' as (b:bs)
+
+ab_wrap :: String -> String -> String
+ab_wrap a b = if abbreviation a b then "YES" else "NO"
 
 main :: IO()
 main = do
@@ -36,7 +46,7 @@ main = do
   forM_ [1..q] $ \q_itr -> do
     a <- getLine
     b <- getLine
-    let result = abbreviation a b
+    let result = ab_wrap a b
     hPutStrLn fptr result
   hFlush fptr
   hClose fptr
