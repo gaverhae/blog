047ef326deb084870e71307eb0ab13879260aca3
Gary Verhaegen <gary.verhaegen@gmail.com>
2019-08-20 13:51:24 +0100

    hr/abbr/hs: bit twiddling, got one more test to pass (14/15)

--- hackerrank/abbr/haskell/app/Main.hs
+++ hackerrank/abbr/haskell/app/Main.hs
@@ -19,23 +19,25 @@ import qualified Control.Monad.State.Lazy as State
 -- Complete the abbreviation function below.
 abbreviation :: String -> String -> Bool
 abbreviation x y =
-  State.evalState (h x y) $ Set.empty
-  where h x y = do
+  State.evalState (h x y (length x) (length y) (length $ Data.List.filter Char.isUpper x)) Set.empty
+  where h x y dx dy ux = do
           s <- State.get
-          if Set.member (x, y) s
+          if Set.member (x, y) s || dy > dx || ux > dy
           then return False
+          else if dy == dx && x == y
+          then return True
           else do
             State.modify (Set.insert (x, y))
-            case (x, y) of
-              ([], []) -> return True
-              ([], y) -> return False
-              (x, []) -> return $ all Char.isLower x
-              (a:as, b:bs) | a == b -> h as bs
-                           | Char.isUpper a -> return False
-                           | Char.toUpper a /= b -> h as (b:bs)
-                           | otherwise -> h as bs >>= \case
-                               True -> return True
-                               False -> h as (b:bs)
+            case (x, y, dx, dy) of
+              (_, _, 0, 0) -> return True
+              (_, _, 0, _) -> return False
+              (_, _, _, 0) -> return $ all Char.isLower x
+              (a:as, b:bs, dx, dy) | a == b -> h as bs (dx - 1) (dy - 1) (ux - 1)
+                                   | Char.isUpper a -> return False
+                                   | Char.toUpper a /= b -> h as (b:bs) (dx - 1) dy ux
+                                   | otherwise -> h as bs (dx - 1) (dy - 1) ux >>= \case
+                                       True -> return True
+                                       False -> h as (b:bs) (dx - 1) dy ux
 
 ab_wrap :: String -> String -> String
 ab_wrap a b = if abbreviation a b then "YES" else "NO"
