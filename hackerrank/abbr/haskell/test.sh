5afea4213fb55bfa2a8e7e2aa8e6fa00cb54785c
Gary Verhaegen <gary.verhaegen@gmail.com>
2019-08-22 21:57:15 +0200

    hr/abbr: move test files up

--- hackerrank/abbr/haskell/test.sh
+++ hackerrank/abbr/haskell/test.sh
@@ -3,10 +3,10 @@
 set -euo pipefail
 
 stack build
-for f in $(ls test_data/input/); do
+for f in $(ls ../test_data/input/); do
     export OUTPUT_PATH=$(mktemp)
-    stack run < test_data/input/$f
-    if diff $OUTPUT_PATH test_data/output/$f; then
+    stack run < ../test_data/input/$f
+    if diff $OUTPUT_PATH ../test_data/output/$f; then
         echo success
     else
         echo unexpected output
