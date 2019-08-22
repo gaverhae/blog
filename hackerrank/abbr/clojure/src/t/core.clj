d3cde4e287a7c27326c7b95d597118588d4ce7be
Gary Verhaegen <gary.verhaegen@gmail.com>
2019-08-23 00:00:51 +0200

    hr/abbr/clj: bit twiddling

--- hackerrank/abbr/clojure/src/t/core.clj
+++ hackerrank/abbr/clojure/src/t/core.clj
@@ -2,35 +2,41 @@
   (:require [clojure.java.io :as io])
   (:gen-class))
 
+;; This is done as a macro to avoid boxing to Integer or converting to long.
+(defmacro add [todo imod imat]
+  `(if (.get ~'seen (unchecked-add-int ~imod (unchecked-multiply-int ~imat ~'nmod)))
+     ~todo
+     (conj ~todo [~imod ~imat])))
+
 (defn abbr [^String to-modify ^String to-match]
-  (let [nmod (count to-modify)
-        nmat (count to-match)
+  (let [nmod (int (count to-modify))
+        nmat (int (count to-match))
         up (fn [^Character c] (Character/toUpperCase c))
-        is-up? (fn [^Character c] (Character/isUpperCase c))]
-    (loop [seen-before? #{}
-           [[imod imat] & r :as todo] [[0 0]]]
-      (cond
-        (empty? todo) false
-        (seen-before? [imod imat]) (recur seen-before? r)
-        :else
-        (let [seen-now (conj seen-before? [imod imat])]
+        is-up? (fn [^Character c] (Character/isUpperCase c))
+        seen (java.util.BitSet. (unchecked-multiply-int nmod nmat))]
+    (loop [todo [[0 0]]]
+      (if (empty? todo) false
+        (let [cur (peek todo)
+              imod (int (cur 0))
+              imat (int (cur 1))]
+          (.flip seen (unchecked-add-int imod (unchecked-multiply-int imat nmod)))
           (cond
             (and (== imod nmod) (== imat nmat)) true
             (== imat nmat) (if (= (subs to-modify imod)
                                   (.toLowerCase (subs to-modify imod)))
                              true
-                             (recur seen-now r))
-            (== imod nmod) (recur seen-now r)
+                             (recur (pop todo)))
+            (== imod nmod) (recur (pop todo))
             :else
             (let [cmod (.charAt to-modify imod)
                   cmat (.charAt to-match imat)]
               (cond
-                (= cmod cmat) (recur seen-now (conj r [(inc imod) (inc imat)]))
-                (is-up? cmod) (recur seen-now r)
-                (not= (up cmod) cmat) (recur seen-now (conj r [(inc imod) imat]))
-                :else (recur seen-now
-                             (conj r [(inc imod) (inc imat)]
-                                     [(inc imod) imat]))))))))))
+                (= cmod cmat) (recur (add (pop todo) (unchecked-inc-int imod) (unchecked-inc-int imat)))
+                (is-up? cmod) (recur (pop todo))
+                (not= (up cmod) cmat) (recur (add (pop todo) (unchecked-inc-int imod) imat))
+                :else (recur (add (add (pop todo)
+                                       (unchecked-inc-int imod) (unchecked-inc-int imat))
+                                  (unchecked-inc-int imod) imat))))))))))
 
 (defn -main
   [& _args]
--- hackerrank/abbr/clojure/test.sh
+++ hackerrank/abbr/clojure/test.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 lein uberjar
 for f in $(ls ../test_data/input/); do
     export OUTPUT_PATH=$(mktemp)
-    java -jar target/uberjar/t-app-standalone.jar < ../test_data/input/$f
+    time java -jar target/uberjar/t-app-standalone.jar < ../test_data/input/$f
     if diff $OUTPUT_PATH ../test_data/output/$f; then
         echo success
     else
