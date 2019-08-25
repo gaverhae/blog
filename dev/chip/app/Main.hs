2eaf3530de9d1307287bb8cb5af9cae03de90160
Gary Verhaegen <gary.verhaegen@gmail.com>
2019-08-25 18:34:52 +0200

    dev/chip: initial state representation

--- dev/chip/app/Main.hs
+++ dev/chip/app/Main.hs
@@ -1,6 +1,14 @@
 module Main where
 
 import Lib
+import Data.Int (Int8)
+import qualified Data.Vector.Unboxed as Vector
+
+data ChipState = ChipState { memory :: Vector.Vector Int8, registers :: Vector.Vector Int8 }
+
+init :: ChipState
+init = ChipState { memory = Vector.replicate 4000 0, registers = Vector.replicate 16 0 }
+
 
 main :: IO ()
 main = someFunc
