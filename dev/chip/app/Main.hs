746d1ad275b2bc1ea1a11cd943c24161f888cff2
Gary Verhaegen <gary.verhaegen@gmail.com>
2019-08-26 16:05:54 +0200

    dev/chip: slightly better run scaffold

--- dev/chip/app/Main.hs
+++ dev/chip/app/Main.hs
@@ -5,12 +5,12 @@ import Data.Word (Word8)
 import qualified Data.List as List
 import qualified Data.Vector as Boxed
 import qualified Data.Vector.Unboxed as Vector
-import qualified Text.Printf as Printf
+import Text.Printf (printf)
 
 data ChipState = ChipState {
   memory :: Vector.Vector Word8,
   registers :: Vector.Vector Word8,
-  address :: (Word8, Word8),
+  address :: Int,
   program_counter :: Int,
   screen :: Boxed.Vector (Vector.Vector Bool)
 }
@@ -22,7 +22,7 @@ init :: [Word8] -> ChipState
 init code = ChipState {
   memory = Vector.generate 4096 (\x -> if ((x - 0x200) >= 0 && (x - 0x200) < length code) then code !! (x - 0x200) else 0),
   registers = Vector.replicate 16 0,
-  address = (0, 0),
+  address = 0,
   program_counter = 0x200,
   screen = blank_screen
 }
@@ -51,7 +51,7 @@ step cs = case next_instruction cs of
   (0x0, 0x0, 0xE, 0x0) -> clear_screen cs
   (0x0,   _,   _,   _) -> error "jump to native not implemented"
   (0x6,   r,   a,   b) -> set_register cs r $ fromIntegral (a * 16 + b)
-  (a, b, c, d) -> error $ "unknown bytecode: " <> Printf.printf "0x%x%x%x%x" a b c d
+  (a, b, c, d) -> error $ "unknown bytecode: " <> printf "0x%x%x%x%x" a b c d
 
 print_screen :: ChipState -> String
 print_screen cs =
@@ -66,14 +66,24 @@ print_memory cs =
         part n l = case l of {[] -> []; _ -> (take n l) : part n (drop n l)}
         mem_list = [m Vector.! (i - 1) | i <- [1..(Vector.length m)]]
         lines = [line r | r <- part 64 mem_list]
-        line r = concat [Printf.printf "%02x%02x " x y | (x:y:[]) <- part 2 r]
+        line r = concat [printf "%02x%02x " x y | (x:y:[]) <- part 2 r]
 
 maze :: [Word8]
 maze = [0x60, 0x00, 0x61, 0x00, 0xa2, 0x22, 0xc2, 0x01, 0x32, 0x01, 0xa2, 0x1e, 0xd0, 0x14, 0x70, 0x04,
         0x30, 0x40, 0x12, 0x04, 0x60, 0x00, 0x71, 0x04, 0x31, 0x20, 0x12, 0x04, 0x12, 0x1c, 0x80, 0x40,
         0x20, 0x10, 0x20, 0x40, 0x80, 0x10]
 
+step_n :: ChipState -> Int -> ChipState
+step_n cs 0 = cs
+step_n cs n = step_n (step cs) (n - 1)
+
+print_state :: ChipState -> IO ()
+print_state cs = do
+  putStrLn $ print_memory $ cs
+  putStrLn $ printf "PC: %8x, Rs: %sI: %8x" (program_counter cs) ((concatMap (\x -> printf "%02x, " x) $ Vector.toList (registers cs)) :: String) (address cs)
+  putStrLn $ print_screen $ cs
+
 main :: IO ()
 main = do
-  putStrLn $ print_memory $ step $ Main.init maze
-  putStrLn $ print_screen $ step $ Main.init maze
+  let state = step_n (Main.init maze) 2
+  print_state state
