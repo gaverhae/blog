37d806e143df07bf1fca77641cacdb6a180662db
Gary Verhaegen <gary.verhaegen@gmail.com>
2019-08-26 16:06:37 +0200

    dev/chip: skip if equal (3XNN)

--- dev/chip/app/Main.hs
+++ dev/chip/app/Main.hs
@@ -76,6 +76,7 @@ step :: ChipState -> State.State Int ChipState
 step cs = case next_instruction cs of
   (0x0, 0x0, 0xE, 0x0) -> return $ inc_pc $ clear_screen cs
   (0x0,   _,   _,   _) -> error "jump to native not implemented"
+  (0x3,   r,  n1,  n2) -> return $ inc_pc $ if (get_register cs r) == (byte n1 n2) then inc_pc cs else cs
   (0x6,   r,  n1,  n2) -> return $ inc_pc $ set_register cs r $ (byte n1 n2)
   (0xa,  n1,  n2,  n3) -> return $ inc_pc $ set_address cs $ fromIntegral (n1 * 256 + n2 * 16 + n3)
   (0xc,   r,  n1,  n2) -> do
@@ -122,5 +123,5 @@ print_state cs = do
 
 main :: IO ()
 main = do
-  let state = step_n (Main.init maze) 4
+  let state = step_n (Main.init maze) 5
   print_state state
