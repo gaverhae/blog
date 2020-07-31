c5589a1943bb408f1a4a6534965f883ccf149621
Gary Verhaegen <gary.verhaegen@gmail.com>
2020-07-31 16:38:48 +0200

    aoc: 6/50

--- advent_of_code/2019/adventofcode.com/src/Day3.hs
+++ advent_of_code/2019/adventofcode.com/src/Day3.hs
@@ -8,6 +8,7 @@ import Prelude hiding (Left,Right)
 import qualified Data.List as List
 import qualified Data.List.Split as Split
 import qualified Data.Set as Set
+import qualified Data.Map.Strict as Map
 
 data Direction = Left | Right | Up | Down
 data Move = Move Direction Int
@@ -26,17 +27,18 @@ parse s =
                   'D' -> Down
                   _ -> undefined
 
-path :: [Move] -> Set.Set (Int, Int)
-path ms = walk Set.empty ms (0, 0)
+path :: [Move] -> Map.Map (Int, Int) Int
+path ms = walk Map.empty ms ((0, 0), 0)
   where walk been [] _ = been
         walk been ((Move direction distance):t) pos = walk new_been t new_pos
           where path = take distance $ drop 1 $ iterate (move direction) pos
                 new_pos = last path
-                new_been = been `Set.union` (Set.fromList path)
-                move Right (x, y) = (x + 1, y    )
-                move Left  (x, y) = (x - 1, y    )
-                move Up    (x, y) = (x    , y + 1)
-                move Down  (x, y) = (x    , y - 1)
+                -- Map.union default to left value if conflict
+                new_been = been `Map.union` (Map.fromList path)
+                move Right ((x, y), d) = ((x + 1, y    ), d + 1)
+                move Left  ((x, y), d) = ((x - 1, y    ), d + 1)
+                move Up    ((x, y), d) = ((x    , y + 1), d + 1)
+                move Down  ((x, y), d) = ((x    , y - 1), d + 1)
 
 distance_to_o :: (Int, Int) -> Int
 distance_to_o (x, y) = (abs x) + (abs y)
@@ -45,9 +47,13 @@ distance_to_o (x, y) = (abs x) + (abs y)
 (|>) a f = f a
 
 solution :: ([Move], [Move]) -> (Int, Int)
-solution (l1, l2) = (closest_cross, 0)
-  where closest_cross = (path l1) `Set.intersection` (path l2)
+solution (l1, l2) = (closest_cross, fastest_cross)
+  where closest_cross = (Map.keysSet $ path l1) `Set.intersection` (Map.keysSet $ path l2)
                       |> Set.toList
                       |> map distance_to_o
+                      |> List.sort
+                      |> head
+        fastest_cross = Map.intersectionWith (+) (path l1) (path l2)
+                      |> Map.elems
                       |> List.sort
                       |> head
