2232ddb6718a9376b9f567b7c3d94f9a21626c2d
Gary Verhaegen <gary.verhaegen@gmail.com>
2020-12-01 10:43:38 +0100

    aoc/2020/clj: day 1 part 2

--- advent_of_code/2020/clj/src/t/core.clj
+++ advent_of_code/2020/clj/src/t/core.clj
@@ -19,10 +19,29 @@
         [x y] (sum-2020 data)]
     (* x y)))
 
+(defn day-1-part-2
+  []
+  (let [data (->> (slurp "data/day-1-part-1")
+                  (clojure.string/split-lines)
+                  (map #(Long/parseLong %))
+                  vec)]
+    (first (for [x (range (count data))
+                 y (range x (count data))
+                 z (range y (count data))
+                 :let [dx (data x)
+                       dy (data y)
+                       dz (data z)]
+                 :when (= 2020 (+ dx dy dz))]
+                         (* dx dy dz)))))
+
+
 (comment
 
   (day-1-part-1)
 712075
+
+(day-1-part-2)
+145245270
 
   )
 
