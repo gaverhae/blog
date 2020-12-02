627ea0d961caf06bb61a6be68c557e7c334a3e54
Gary Verhaegen <gary.verhaegen@gmail.com>
2020-12-02 14:20:51 +0100

    aoc/2020/clj: day 2

--- advent_of_code/2020/clj/src/t/core.clj
+++ advent_of_code/2020/clj/src/t/core.clj
@@ -34,6 +34,40 @@
                  :when (= 2020 (+ dx dy dz))]
                          (* dx dy dz)))))
 
+(defn parse-day-2
+  [f]
+  (->> f
+       (clojure.string/split-lines)
+       (map #(clojure.string/split % #" |: |-"))
+       (map (fn [[min max c pw]]
+              [(Long/parseLong min)
+               (Long/parseLong max)
+               (first c)
+               pw]))))
+
+(defn count-good-passwords
+  [lines]
+  (->> lines
+       (filter (fn [[min max c pw]]
+                 (<= min
+                     (count (filter #(= c %) pw))
+                     max)))
+       count))
+
+(defn count-good-passwords-2
+  [lines]
+  (->> lines
+       (filter (fn [[min max c ^String pw]]
+                 (let [a (= c (.charAt pw (dec min)))
+                       b (= c (.charAt pw (dec max)))]
+                   (if a (not b) b))))
+       count))
+
+(defn day-2-part-1
+  []
+  (->> (slurp "data/day2")
+       parse-day-2
+       count-good-passwords))
 
 (comment
 
@@ -42,6 +76,14 @@
 
 (day-1-part-2)
 145245270
+
+(day-2-part-1)
+666
+
+(->> (slurp "data/day2")
+     parse-day-2
+     count-good-passwords-2)
+348
 
   )
 
--- advent_of_code/2020/clj/test/t/core_test.clj
+++ advent_of_code/2020/clj/test/t/core_test.clj
@@ -12,3 +12,17 @@
                       1456])))
   (is (= 712075 (t/day-1-part-1))))
 
+(deftest day-2
+  (is (= [[1 3 \a "abcde"]
+          [1 3 \b "cdefg"]
+          [2 9 \c "ccccccccc"]]
+         (t/parse-day-2 "1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc")))
+  (is (= 2
+         (t/count-good-passwords [[1 3 \a "abcde"]
+                                  [1 3 \b "cdefg"]
+                                  [2 9 \c "ccccccccc"]])))
+  (is (= 1
+         (t/count-good-passwords-2 [[1 3 \a "abcde"]
+                                    [1 3 \b "cdefg"]
+                                    [2 9 \c "ccccccccc"]]))))
+
