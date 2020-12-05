31fa70574506a27d4fe4fa954269bd449510b3f6
Gary Verhaegen <gary.verhaegen@gmail.com>
2020-12-06 00:09:01 +0100

    aoc/2020/clj: cleanup day 4

--- advent_of_code/2020/clj/src/t/core.clj
+++ advent_of_code/2020/clj/src/t/core.clj
@@ -1,66 +1,5 @@
 (ns t.core
-  (:require [clojure.string :as string]
-            [clojure.set :as set]))
-
-(defn day-3-parse
-  [s]
-  (->> s
-       (string/split-lines)
-       (map #(map {\. 0 \# 1} %))
-       (map cycle)))
-
-(defn slope
-  [[right down] geo]
-  (loop [n 0
-         pos geo]
-    (if (empty? pos)
-      n
-      (recur (+ n (ffirst pos))
-             (drop down (map #(drop right %) pos))))))
-
-(defn day-3-part-2
-  [data]
-  (let [terrain (day-3-parse data)]
-    (->> [[1 1] [3 1] [5 1] [7 1] [1 2]]
-         (map #(slope % terrain))
-         (reduce *))))
-
-(defn parse4
-  [s]
-  (->> (string/split-lines s)
-       (map (fn [line] (->> (string/split line #" ")
-                            (map (fn [word] (string/split word #":"))))))
-       (partition-by #(= % [[""]]))
-       (remove #(= % [[[""]]]))
-       (map #(reduce concat %))
-       (map #(reduce (fn [acc [k v]] (assoc acc (keyword k) v)) {} %))))
-
-(defn d41
-  [maps]
-  (->> maps
-       (filter #(set/subset? #{:byr :iyr :eyr :hgt :hcl :ecl :pid} (set (keys %))))
-       count))
-
-(defn d42
-  [maps]
-  (->> maps
-       (filter #(set/subset? #{:byr :iyr :eyr :hgt :hcl :ecl :pid} (set (keys %))))
-       (filter (fn [p]
-                 (and (re-matches #"\d\d\d\d" (:byr p))
-                      (<= 1920 (Long/parseLong (:byr p)) 2002)
-                      (re-matches #"\d\d\d\d" (:iyr p))
-                      (<= 2010 (Long/parseLong (:iyr p)) 2020)
-                      (re-matches #"\d\d\d\d" (:eyr p))
-                      (<= 2020 (Long/parseLong (:eyr p)) 2030)
-                      (let [[_ n units] (re-matches #"(\d+)(in|cm)" (:hgt p))]
-                        (case units
-                          "cm" (<= 150 (Long/parseLong n) 193)
-                          "in" (<= 59 (Long/parseLong n) 76)
-                          false))
-                      (re-matches #"#[0-9a-f]{6}" (:hcl p))
-                      (#{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"} (:ecl p))
-                      (re-matches #"\d{9}" (:pid p)))))
-       count))
+  (:require [clojure.string :as string]))
 
 (defn parse5
   [s]
@@ -84,29 +23,5 @@
        (partition 2 1)
        (filter (fn [[p n]] (= 2 (- n p)))))
 ((677 679))
-
-  )
-
-
-
-(comment
-
-(->> (slurp "data/day3")
-     (day-3-parse)
-     (slope [3 1]))
-214
-
-(->> (slurp "data/day3")
-     (day-3-part-2))
-8336352024
-
-(->> (slurp "data/day4")
-     parse4
-     d41)
-210
-
-(->> (slurp "data/day4")
-     parse4
-     d42)
 
   )
--- advent_of_code/2020/clj/test/t/core_test.clj
+++ advent_of_code/2020/clj/test/t/core_test.clj
@@ -4,7 +4,8 @@
             [t.core :as t]
             [t.day1 :as day1]
             [t.day2 :as day2]
-            [t.day3 :as day3]))
+            [t.day3 :as day3]
+            [t.day4 :as day4]))
 
 (let [read (fn [s i] (string/split-lines (slurp (str "data/" s i))))]
   (defn sample [i] (read "sample" i))
@@ -32,28 +33,15 @@
   (is (= 214 (day3/part1 (day3/parse (data 3)))))
   (is (= 8336352024 (day3/part2 (day3/parse (data 3))))))
 
-(def sample4
-"ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
-byr:1937 iyr:2017 cid:147 hgt:183cm
-
-iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
-hcl:#cfa07d byr:1929
-
-hcl:#ae17e1 iyr:2013
-eyr:2024
-ecl:brn pid:760753108 byr:1931
-hgt:179cm
-
-hcl:#cfa07d eyr:2025 pid:166559648
-iyr:2011 ecl:brn hgt:59in")
-
-(deftest day-4
+(deftest day4
   (is (= [{:ecl "gry", :pid "860033327", :eyr "2020", :hcl "#fffffd", :byr "1937", :iyr "2017", :cid "147", :hgt "183cm"}
           {:iyr "2013", :ecl "amb", :cid "350", :eyr "2023", :pid "028048884", :hcl "#cfa07d", :byr "1929"}
           {:hcl "#ae17e1", :iyr "2013", :eyr "2024", :ecl "brn", :pid "760753108", :byr "1931", :hgt "179cm"}
           {:hcl "#cfa07d", :eyr "2025", :pid "166559648", :iyr "2011", :ecl "brn", :hgt "59in"}]
-         (t/parse4 sample4)))
-  (is (= 2 (t/d41 (t/parse4 sample4)))))
+         (day4/parse (sample 4))))
+  (is (= 2 (day4/part1 (day4/parse (sample 4)))))
+  (is (= 210 (day4/part1 (day4/parse (data 4)))))
+  (is (= 131 (day4/part2 (day4/parse (data 4))))))
 
 (def sample5
 "FBFBBFFRLR
