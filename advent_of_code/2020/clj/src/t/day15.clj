39341f41bb8a781c171b22b5a932844076a05433
Gary Verhaegen <gary.verhaegen@gmail.com>
2020-12-15 08:29:55 +0100

    aoc/2020/clj: day 15: faster constants (~70s -> ~1.5s)

--- advent_of_code/2020/clj/src/t/day15.clj
+++ advent_of_code/2020/clj/src/t/day15.clj
@@ -3,18 +3,21 @@
 
 (defn parse
   [[line]]
-  (map #(Long/parseLong %) (string/split line #",")))
+  (mapv #(Long/parseLong %) (string/split line #",")))
 
 (defn brute-force
   [stop input]
-  (let [m (into {} (map-indexed (fn [idx n] [n (inc idx)]) (butlast input)))]
-    (loop [n (count input)
-           cur (last input)
-           prevs m]
-      (cond (= n stop) cur
-            (prevs cur) (recur (inc n) (- n (prevs cur)) (assoc prevs cur n))
-            :else (recur (inc n) 0 (assoc prevs cur n))))))
-
+  (let [prevs (int-array stop)]
+    (dotimes [n (dec (count input))]
+      (aset ^"[I" prevs (unchecked-int (input n)) (unchecked-inc-int (unchecked-int n))))
+    (loop [n (unchecked-int (count input))
+           cur (unchecked-int (last input))]
+      (let [last-said (unchecked-int (aget ^"[I" prevs (unchecked-int cur)))
+            _ (aset ^"[I" prevs (unchecked-int cur) (unchecked-int n))]
+        (cond (zero? (unchecked-subtract-int (unchecked-int n) (unchecked-int stop))) cur
+              (zero? (unchecked-int last-said)) (recur (unchecked-inc-int n) (unchecked-int 0))
+              :else (recur (unchecked-inc-int n)
+                           (unchecked-subtract-int (unchecked-int n) (unchecked-int last-said))))))))
 
 (defn part1
   [input]
