b681e0864863b66ea65c64748736a7a9ef1e9fd0
Gary Verhaegen <gary.verhaegen@gmail.com>
2021-02-15 17:34:30 +0100

    aoc/2020/clj/24: count all at once
    3.5s -> 1s

--- advent_of_code/2020/clj/src/t/day24.clj
+++ advent_of_code/2020/clj/src/t/day24.clj
@@ -32,20 +32,20 @@
   (let [neighbours (fn [[x y z]]
                      (map (fn [[dx dy dz]] [(+ x dx) (+ y dy) (+ z dz)])
                           [[-1 0 1] [0 -1 1] [-1 1 0] [1 0 -1] [0 1 -1] [1 -1 0]]))]
-    (loop [n 0
-           is-black? input]
-      (if (= n 100)
-        (count is-black?)
-        (recur (inc n)
-               (->> is-black?
-                    (mapcat (fn [t] (cons t (neighbours t))))
-                    (filter (fn [pos]
-                              (let [bn (->> (neighbours pos)
-                                            (filter is-black?)
-                                            count)
-                                    black? (is-black? pos)]
-                                (cond (and black? (zero? bn)) false
-                                      (and black? (> bn 2)) false
-                                      (and (not black?) (= 2 bn)) true
-                                      :else black?))))
-                    (reduce conj #{})))))))
+    (->> (range 100)
+         (reduce (fn [prev _]
+                   (->> (mapcat neighbours prev)
+                        (reduce (fn [acc el]
+                                  (update acc el (fnil inc 0)))
+                                {})
+                        (keep (fn [[pos num-black-neighbours]]
+                                ;; if zero black neighbours, does not appear
+                                (when (or (and (or (== 1 num-black-neighbours)
+                                                   (== 2 num-black-neighbours))
+                                               (contains? prev pos))
+                                          (and (== 2 num-black-neighbours)
+                                               (not (contains? prev pos))))
+                                  pos)))
+                        set))
+                 input)
+         count)))
