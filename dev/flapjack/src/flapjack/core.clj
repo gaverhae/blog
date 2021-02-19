6cadd99fe0d05171693fbf1062346460a2ed645f
Gary Verhaegen <gary.verhaegen@gmail.com>
2021-02-19 17:16:41 +0100

    dev/flapjack: cleanup

--- dev/flapjack/src/flapjack/core.clj
+++ dev/flapjack/src/flapjack/core.clj
@@ -49,64 +49,6 @@
           (stop? h) [h (best-points h)]
           :else (recur (rest d) (conj h (first d))))))
 
-(defprotocol Dealer
-  (start-game [_] "Returns a token for a new game.")
-  (draw [_ token] "Returns the next card.")
-  (stick [_ token score] "Ends the round and declare the client's score.")
-  (end-game [_ token winner] "Ends the game and declares the winner."))
-
-(defn in-memory-dealer
-  "Note: not thread-safe."
-  ([] (in-memory-dealer (atom {})))
-  ([state]
-    (reify Dealer
-      (start-game [_]
-        (let [game-id (.toString (UUID/randomUUID))]
-          (swap! state assoc game-id {:rounds-played 0
-                                      :rounds [{:taken 0
-                                                :deck (half-deck)}]})
-          [:ok game-id]))
-      (draw [_ game-id]
-        (let [game-state (get @state game-id)
-              {:keys [taken, deck]} (-> game-state :rounds last)]
-          (if (and (< (:rounds-played game-state) 5)
-                   (or (zero? taken)
-                       (not (busted? (take taken deck))))
-                   (< taken (count deck)))
-            (do (swap! state update-in [game-id :rounds (:rounds-played game-state) :taken] inc)
-                [:ok (get deck taken)])
-            ;; trying to start a 6th round, trying to pull more cards than in
-            ;; deck, or trying to keep drawing when score >25.
-            [:error "Invalid command for current state."])))
-      (stick [_ game-id client-points]
-        ;; FIXME: server needs to play too
-        (let [server (unattended-round (fn [_] (> (rand) 0.8)) (half-deck))
-              {:as game-state, :keys [rounds-played rounds]} (get @state game-id)
-              {:keys [taken, deck]} (last rounds)]
-          (if (and (< (:rounds-played game-state) 5)
-                   (>= taken 1)
-                   (contains? (set (points (take taken deck))) client-points))
-            (do (swap! state update game-id #(-> %
-                                                 (update :rounds-played inc)
-                                                 (assoc-in [:rounds rounds-played :client-points] client-points)
-                                                 (assoc-in [:rounds rounds-played :server] server)))
-                [:ok (get-in @state [game-id :rounds rounds-played :server 1])])
-            [:error "Invalid command for current state."])))
-      (end-game [_ game-id winner-claim]
-        (let [{:as game-state, :keys [rounds-played rounds]} (get @state game-id)
-              {:keys [taken, deck]} (last rounds)
-              client-score (->> rounds
-                                (map :client-points)
-                                (reduce +))
-              server-score (->> rounds (map :server-points) (reduce +))
-              winner (cond (> client-score server-score) :server
-                           (< client-score server-score) :client
-                           :else :draw)]
-          (if (and (= rounds-played 5)
-                   (= winner winner-claim))
-            [:ok]
-            [:error "Invalid command for current state."]))))))
-
 (defn test-strategy
   [stop? n]
   (->> (repeatedly n half-deck)
@@ -126,15 +68,19 @@
        (map second)
        (reduce +)))
 
-(comment
-(time (dotimes [_ 10000] (half-deck)))
-(with-out-str (time (dotimes [_ 10000]
-        (best-play (half-deck)))))
-"\"Elapsed time: 12456.804358 msecs\"\n"
-(with-out-str (time (dotimes [_ 10000]
-        (best-play (half-deck)))))
-"\"Elapsed time: 459.123759 msecs\"\n"
-)
+(defn slow-best-play
+  [deck]
+  (let [all-hands (rest (reductions conj [] deck))
+        first-bust (or (some->> all-hands
+                                (filter busted?)
+                                first
+                                count)
+                       (inc (count all-hands)))]
+    (->> all-hands
+         (take (dec first-bust))
+         (sort-by best-points)
+         first
+         count)))
 
 (defn best-play
   "Given a deck, returns the best place to stop drawing and the corresponding
@@ -164,21 +110,14 @@
                  (inc n)
                  (rest d)))))))
 
-(defn previous-best-play
-  [deck]
-  (let [all-hands (rest (reductions conj [] deck))
-        first-bust (or (some->> all-hands
-                                (filter busted?)
-                                first
-                                count)
-                       (inc (count all-hands)))]
-    (->> all-hands
-         (take (dec first-bust))
-         (sort-by best-points)
-         first
-         count)))
 
 (comment
+(with-out-str (time (dotimes [_ 10000]
+        (slow-best-play (half-deck)))))
+"\"Elapsed time: 12989.714633 msecs\"\n"
+(with-out-str (time (dotimes [_ 10000]
+        (best-play (half-deck)))))
+"\"Elapsed time: 459.123759 msecs\"\n"
 
   (loop [d (half-deck)
          n 1000]
@@ -214,22 +153,32 @@
                                  n)]
         (> (count hand) average-best-play)))))
 
+(defn one-card-probs
+  "Always keeps playing if current points is 10. If current points are better,
+  stops if probability of going bust is greater than bust-max, or if
+  probability of improving the score is no greater than imp-min."
+  [bust-max imp-min]
+  (fn [hand]
+      (let [p (best-points hand)]
+        (and (not= 10 p)
+             (let [possible-hands (->> hand
+                                       (reduce (fn [acc el]
+                                                 (if (= 1 (acc el))
+                                                   (dissoc acc el)
+                                                   (update acc el dec)))
+                                               (frequencies (half-deck)))
+                                       (mapcat (fn [[i n]] (repeat n i)))
+                                       (map (fn [c] (conj hand c))))
+                   p-bust (/ (count (filter busted? possible-hands))
+                             (count possible-hands))
+                   p-imp (/ (count (filter #(< (best-points %) p) possible-hands))
+                            (count possible-hands))]
+               (or (> p-bust bust-max)
+                   (< p-imp imp-min)))))))
+
 
 
 (comment
-
-  (def dealer-state (atom {}))
-  (def d (in-memory-dealer dealer-state))
-  (def h (atom []))
-  (def t (second (start-game d)))
-
-  (-> t)
-  (-> @dealer-state)
-  (-> @h)
-
-  (let [[ok? card] (draw d t)] (if (= :ok ok?) (do (swap! h conj card) [@h (best-points @h) (count @h)]) [ok? card]))
-
-  (stick d t (best-points @h))
 
   (for [n (range 0 9)]
     [n (test-strategy (fn [_] (> (rand) (/ n 10))) 10000)])
@@ -285,15 +234,17 @@
     (strat-take-n (best-play deck)))
   10000)
 45761
-(test-strategy-oracle
-  (fn [deck]
-    (strat-take-n (previous-best-play deck)))
-  10000)
-44900
 
 (test-strategy (monte-carlo-ish 100) 100)
 902
 100
 96
+
+(->> (for [n (range 0 11)
+           m (range 0 11)]
+       [n m (test-strategy (one-card-probs (/ n 10) (/ m 10)) 1000)])
+     (sort-by #(nth % 2)))
+([9 2 5712] [6 2 5716] [3 2 5731] [8 2 5750] [7 2 5818] [5 2 5839] [6 1 5850] [4 2 5861] [2 1 5871] [10 2 5878] [3 1 5880] [2 2 5904] [10 1 5944] [9 1 6016] [8 1 6020] [7 1 6040] [4 1 6058] [5 1 6059] [0 1 6121] [1 2 6137] [5 3 6183] [6 3 6203] [0 2 6248] [1 1 6276] [3 3 6289] [2 3 6294] [10 3 6315] [9 3 6322] [8 3 6328] [4 3 6360] [7 3 6384] [1 3 6442] [9 6 6475] [4 8 6531] [0 3 6541] [3 8 6548] [2 4 6569] [6 4 6574] [1 5 6583] [8 9 6590] [2 5 6604] [6 5 6604] [6 9 6606] [8 6 6610] [3 4 6611] [10 4 6616] [8 4 6620] [7 5 6627] [8 5 6627] [5 10 6631] [5 4 6638] [7 6 6641] [4 10 6643] [5 9 6651] [8 8 6656] [6 7 6659] [9 10 6664] [5 8 6679] [9 5 6682] [9 9 6687] [3 7 6688] [6 10 6688] [6 6 6693] [9 4 6694] [4 9 6699] [5 5 6701] [10 5 6709] [7 4 6711] [3 10 6717] [2 9 6720] [8 10 6721] [5 7 6727] [7 8 6732] [1 6 6734] [9 7 6734] [1 7 6741] [0 4 6743] [0 6 6749] [4 6 6749] [9 8 6753] [10 10 6755] [4 5 6757] [6 8 6758] [10 8 6760] [2 7 6763] [10 6 6763] [8 7 6766] [7 9 6769] [3 6 6771] [0 7 6777] [1 10 6777] [7 7 6779] [3 9 6783] [4 7 6786] [7 10 6787] [0 5 6794] [1 9 6796] [10 7 6803] [4 4 6823] [1 4 6824] [1 8 6827] [2 10 6829] [2 6 6847] [10 9 6852] [0 10 6859] [0 9 6863] [5 6 6876] [0 8 6893] [2 8 6895] [3 5 6901] [1 0 7971] [2 0 8009] [0 0 8152] [3 0 8644] [4 0 9535] [5 0 10000] [6 0 10000] [7 0 10000] [8 0 10000] [9 0 10000] [10 0 10000])
+
 
   )
