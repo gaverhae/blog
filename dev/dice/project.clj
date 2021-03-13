8de42a07aa1c4b0d239210996a2c142b12b8d3ec
Gary Verhaegen <gary.verhaegen@gmail.com>
2021-03-13 21:11:16 +0100

    dev/dice: sw reup stats

--- dev/dice/project.clj
+++ dev/dice/project.clj
@@ -1,4 +1,4 @@
 (defproject t "app"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :plugins [[lein-gorilla "0.4.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :plugins [[org.clojars.benfb/lein-gorilla "0.6.0"]]
   :repl-options {:init-ns t.core})
