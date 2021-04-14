ed1acdc9f61db2392d0f1e87b6ae5838e891a30f
Gary Verhaegen <gary.verhaegen@gmail.com>
2021-04-14 19:13:35 +0200

    blog/monads-1: final draft

--- blog/minicode/2021-04-18/java/Fiber.java
+++ blog/minicode/2021-04-18/java/Fiber.java
@@ -65,20 +65,45 @@ public interface Fiber<A> {
         return rets;
     }
 
+    static <A> Fiber<A> _return(A a) {
+        return pure(a);
+    }
+
+    static Fiber<Void> debug(String msg) {
+        System.out.println(msg);
+        return pure(null);
+    }
+
+
     public static void main(String[] args) {
-        Fiber<Double> f1 = pure(Integer.valueOf(5))
-            .bind(a -> pure(a + 3)
-            .bind(b -> pure(b + 5)
-            .bind(c -> pure(b * c)
-            .bind(i -> pure(1.0 * i)))));
-        Fiber<Double> f2 = Fiber.pure(1)
-            .bind(a -> pure(2)
-            .bind(b -> pure(1)
-            .bind(c -> pure(b * b)
-            .bind(b2 -> pure(a * c)
-            .bind(ac -> pure(b2 - 4 * ac)
-            .bind(delta -> pure(Math.sqrt(delta))
-            .bind(rac -> pure((-b + rac) / (2 * a)))))))));
+        Fiber<Double> f1 =
+            pure          (5) .bind(a ->
+            debug("thread 1") .bind(   _1 ->
+            pure      (a + 3) .bind(b -> 
+            debug("thread 1") .bind(   _2 ->
+            pure      (b + 5) .bind(c ->
+            debug("thread 1") .bind(   _3 ->
+            pure      (b * c) .bind(i ->
+            debug("thread 1") .bind(   _4 ->
+            _return (1.0 * i)
+            ))))))));
+        Fiber<Double> f2 =
+            pure                (1) .bind(a ->
+            debug("thread 2") .bind(    _1 ->
+            pure                (2) .bind(b ->
+            debug("thread 2") .bind(    _2 ->
+            pure                (1) .bind(c ->
+            debug("thread 2") .bind(    _3 ->
+            pure            (b * b) .bind(b2 ->
+            debug("thread 2") .bind(    _4 ->
+            pure            (a * c) .bind(ac ->
+            debug("thread 2") .bind(    _5 ->
+            pure      (b2 - 4 * ac) .bind(delta ->
+            debug("thread 2") .bind(    _6 ->
+            pure (Math.sqrt(delta)) .bind(rac ->
+            debug("thread 2") .bind(    _7 ->
+            _return ((-b + rac) / (2 * a))
+            ))))))))))))));
 
         System.out.println(exec(new LinkedList<Fiber<Double>>() {{ add(f1); add(f2); }}));
         System.out.println(exec(new LinkedList<Fiber<Double>>() {{ add(f2); add(f1); }}));
