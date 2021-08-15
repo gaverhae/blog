97ebf760f4a3376c99f34bda5b0f91ad1c54d0d8
Gary Verhaegen <gary.verhaegen@gmail.com>
2021-08-15 15:33:43 +0200

    blog: more careful deploy script

--- blog/deploy.sh
+++ blog/deploy.sh
@@ -7,6 +7,11 @@ COUNT=$(git rev-list --count $SHA)
 VERSION=$(git show -s --format=%cd-$COUNT-%h --date=format:%Y%m%d --abbrev=8 $SHA)
 S3_FILE=s3://cuddly-octo-palm-tree/public/$VERSION.tar.gz
 
+if ! [ -z "$(git status --porcelain)" ]; then
+    echo "Not working on dirty worktree."
+    exit 1
+fi
+
 if aws s3 ls $S3_FILE; then
     echo "File already exists, no need to build."
 else
