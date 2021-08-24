e7991667db7cee8c92a496dc318cc71858f67f05
Gary Verhaegen <gary.verhaegen@gmail.com>
2021-08-24 20:43:34 +0200

    blog: refactor deploy script

--- blog/deploy.sh
+++ blog/deploy.sh
@@ -10,6 +10,30 @@ COUNT=$(git rev-list --count $SHA)
 VERSION=$(git show -s --format=%cd-$COUNT-%h --date=format:%Y%m%d --abbrev=8 $SHA)
 S3_FILE=s3://cuddly-octo-palm-tree/public/$VERSION.tar.gz
 
+update_deploy_set() {
+    local jq_expr
+    jq_expr="$1"
+
+    jq -c --arg version $VERSION "$jq_expr" < tf/deployed > tf/deployed.tmp
+    mv tf/deployed.tmp tf/deployed
+}
+
+deploy() {
+    ( cd tf && terraform apply -var="blog_version=$(cat deployed)" )
+}
+
+wait_for() {
+    while ! { RESP=$(curl --fail \
+                          $(if [ "$1" == "background" ]; then
+                              echo "--connect-to cuddly-octo-palm-tree.com:443:$(cat tf/out):443"
+                            fi) \
+                          https://cuddly-octo-palm-tree.com/version.txt) \
+          && [[ "$VERSION" == "$RESP" ]] ;}; do
+        echo $RESP
+        sleep 5
+    done
+}
+
 if ! [ -z "$(git status --porcelain)" ]; then
     echo "Not working on dirty worktree."
     exit 1
@@ -39,28 +63,16 @@ else
     rm public.tar.gz
 fi
 
-jq -c --arg version $VERSION '. + [$version]' < tf/deployed > tf/deployed.tmp
-mv tf/deployed.tmp tf/deployed
+update_deploy_set '. + [$version]'
+deploy
+wait_for background
 
-( cd tf && terraform apply -var="blog_version=$(cat deployed)" )
+update_deploy_set 'reverse'
+deploy
+wait_for redirect
 
-while ! { RESP=$(curl --fail \
-                      --connect-to cuddly-octo-palm-tree.com:443:$(cat tf/out):443 \
-                      https://cuddly-octo-palm-tree.com/version.txt) \
-      && [[ "$VERSION" == "$RESP" ]] ;}; do
-    echo $RESP
-    sleep 5
-done
-
-jq -c 'reverse' < tf/deployed > tf/deployed.tmp
-mv tf/deployed.tmp tf/deployed
-
-( cd tf && terraform apply -var="blog_version=$(cat deployed)" )
-
-jq -c '.[0:1]' < tf/deployed > tf/deployed.tmp
-mv tf/deployed.tmp tf/deployed
-
-( cd tf && terraform apply -var="blog_version=$(cat deployed)" )
+update_deploy_set '.[0:1]'
+deploy
 
 git add tf/deployed
 git commit -m "auto: deployed $VERSION"
