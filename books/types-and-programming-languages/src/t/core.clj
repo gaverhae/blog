a850f2ad0a21004a74facafcc64292cc8d722b46
Gary Verhaegen <gary.verhaegen@gmail.com>
2021-10-12 21:48:42 +0200

    books/tapl: I need a monad :'(

--- books/types-and-programming-languages/src/t/core.clj
+++ books/types-and-programming-languages/src/t/core.clj
@@ -1,1 +1,27 @@
 (ns t.core)
+
+(defn value-3-1
+  [exp]
+  (contains? #{true false} exp))
+
+(defn term-3-1
+  [exp]
+  (or (value-3-1 exp)
+      (and (list? exp)
+           (= 4 (count exp))
+           (= 'if (first exp))
+           (every? term-3-1 (rest exp)))))
+
+(defn step-3-1
+  [exp]
+  (cond
+    (value-3-1 exp) [:value exp]
+    (not (term-3-1 exp)) [:not-a-term exp]
+    :else
+    (let [[_ t1 t2 t3] exp]
+      (case t1
+        true [:if-true t2]
+        false [:if-false t3]
+        (let [[r t] (step-3-1 t1)]
+          (prn r)
+          [:if (list 'if t t2 t3)])))))
--- books/types-and-programming-languages/test/t/core_test.clj
+++ books/types-and-programming-languages/test/t/core_test.clj
@@ -1,3 +1,27 @@
 (ns t.core-test
   (:require [clojure.test :refer :all]
             [t.core :as t]))
+
+(deftest lang-3-1
+  (are [v] (t/value-3-1 v)
+       true
+       false)
+  (are [t] (t/term-3-1 t)
+       true
+       false
+       '(if true false true)
+       '(if (if true false true) true (if false false true)))
+  (are [not-t] (not (t/term-3-1 not-t))
+       1
+       nil
+       ()
+       [:a :b :c]
+       '(if true 4 false))
+  (are [out in] (= out (t/step-3-1 in))
+       [:not-a-term 4] 4
+       [:value true] true
+       [:value false] false
+       [:if-true true] '(if true true false)
+       [:if-false false] '(if false true false)
+       [:if-true '(if true true false)] '(if true (if true true false) false)
+       [:if '(if true true false)] '(if (if false false true) true false)))
