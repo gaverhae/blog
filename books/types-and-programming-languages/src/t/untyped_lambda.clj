26afc0aac3ee7227c47ae60cc69cb840025145d0
Gary Verhaegen <gary.verhaegen@gmail.com>
2021-11-22 19:45:43 +0100

    books/tapl: slightly less heavy abs syntax

--- books/types-and-programming-languages/src/t/untyped_lambda.clj
+++ books/types-and-programming-languages/src/t/untyped_lambda.clj
@@ -5,7 +5,7 @@
 (defn value?
   [exp]
   (match exp
-    [:fn [v] t] true
+    [:fn v t] true
     _ false))
 
 (defn free-variables
@@ -14,26 +14,26 @@
     [:var x] #{x}
     [:app t1 t2] (set/union (free-variables t1)
                             (free-variables t2))
-    [:fn [x] t] (disj (free-variables t)
-                      x)))
+    [:fn x t] (disj (free-variables t)
+                    x)))
 
 (defn substitute
   [var-name with in]
   (match in
     [:var var-name] with
     [:var other] in
-    [:fn [var-name] t] in
-    [:fn [other] t] (let [fv (free-variables with)]
-                      (if (contains? fv other)
-                        :undefined
-                        [:fn [other] (substitute var-name with t)]))
+    [:fn var-name t] in
+    [:fn other t] (let [fv (free-variables with)]
+                    (if (contains? fv other)
+                      :undefined
+                      [:fn other (substitute var-name with t)]))
     [:app t1 t2] [:app (substitute var-name with t1)
                        (substitute var-name with t2)]))
 
 (defn step
   [exp]
   (match exp
-    [:app [:fn [x] t] (v :guard value?)] (substitute x v t)
+    [:app [:fn x t] (v :guard value?)] (substitute x v t)
     [:app (v :guard value?) t] (when-let [t' (step t)]
                                  [:app v t'])
     [:app t1 t2] (when-let [t1' (step t1)]
--- books/types-and-programming-languages/test/t/untyped_lambda_test.clj
+++ books/types-and-programming-languages/test/t/untyped_lambda_test.clj
@@ -3,29 +3,29 @@
             [t.untyped-lambda :as t]))
 
 (def id
-  [:fn ["x"] [:var "x"]])
+  [:fn "x" [:var "x"]])
 
 (deftest basic
   (are [x] (t/value? x)
-       [:fn ["x"] [:app [:var "x"] [:var "x"]]]
-       [:fn ["x"] :t])
+       [:fn "x" [:app [:var "x"] [:var "x"]]]
+       [:fn "x" :t])
   (are [x _ y] (= y (t/step x))
        [:var "x"] => nil
        [:app id id] => id
        [:app id [:app id id]] => [:app id id]
        [:app [:app id id] [:app id id]] => [:app id [:app id id]]
-       [:app [:fn ["x"] [:var "x"]] id] => id
-       [:app [:fn ["x"] [:var "y"]] id] => [:var "y"]
-       [:app [:fn ["x"] id] [:fn ["y"] [:var "y"]]] => id
-       [:app [:fn ["x"] [:app [:var "x"] [:var "x"]]] id] => [:app id id]
+       [:app [:fn "x" [:var "x"]] id] => id
+       [:app [:fn "x" [:var "y"]] id] => [:var "y"]
+       [:app [:fn "x" id] [:fn "y" [:var "y"]]] => id
+       [:app [:fn "x" [:app [:var "x"] [:var "x"]]] id] => [:app id id]
 
-       [:app [:fn ["x"] [:fn ["y"] [:app [:var "x"] [:var "y"]]]] [:fn ["z"] [:var "z"]]]
-       => [:fn ["y"] [:app [:fn ["z"] [:var "z"]] [:var "y"]]]
+       [:app [:fn "x" [:fn "y" [:app [:var "x"] [:var "y"]]]] [:fn "z" [:var "z"]]]
+       => [:fn "y" [:app [:fn "z" [:var "z"]] [:var "y"]]]
 
-       [:app [:fn ["x"] [:fn ["y"] [:app [:var "x"] [:var "y"]]]] [:fn ["z"] [:app [:var "z"] [:var "t"]]]]
-       => [:fn ["y"] [:app [:fn ["z"] [:app [:var "z"] [:var "t"]]] [:var "y"]]]
+       [:app [:fn "x" [:fn "y" [:app [:var "x"] [:var "y"]]]] [:fn "z" [:app [:var "z"] [:var "t"]]]]
+       => [:fn "y" [:app [:fn "z" [:app [:var "z"] [:var "t"]]] [:var "y"]]]
 
-       [:app [:fn ["x"] [:fn ["y"] [:app [:var "x"] [:var "y"]]]] [:fn ["z"] [:app [:var "z"] [:var "y"]]]]
+       [:app [:fn "x" [:fn "y" [:app [:var "x"] [:var "y"]]]] [:fn "z" [:app [:var "z"] [:var "y"]]]]
        => :undefined
        ;=> [:fn ["a"] [:app [:fn ["z"] [:app [:var "z"] [:var "y"]]] [:var "a"]]]
        ))
