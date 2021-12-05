32daf1799919f199f99c5113f4909ed53d46b4ce
Gary Verhaegen <gary.verhaegen@gmail.com>
2021-12-05 06:48:10 +0100

    aoc/5: reduce duplication

--- advent_of_code/2021/clj/src/t/day5.clj
+++ advent_of_code/2021/clj/src/t/day5.clj
@@ -3,40 +3,19 @@
             [clojure.set :as set]
             [t.util :refer [transpose]]))
 
-(defn parse
-  [lines]
-  (->> lines
-       (map (fn [line]
-              (let [[_ x1 y1 x2 y2] (re-matches #"(\d+),(\d+) -> (\d+),(\d+)" line)]
-                [[(Long/parseLong x1) (Long/parseLong y1)]
-                 [(Long/parseLong x2) (Long/parseLong y2)]])))))
+(defn sign
+  [x]
+  (cond (zero? x) 0
+        (pos? x) 1
+        (neg? x) -1))
 
-(defn part1
-  [input]
-  (->> input
-       (filter (fn [[[x1 y1] [x2 y2]]]
-                 (or (= x1 x2) (= y1 y2))))
-       (mapcat (fn [[[x1 y1] [x2 y2]]]
-                 (for [x (range (min x1 x2) (inc (max x1 x2)))
-                       y (range (min y1 y2) (inc (max y1 y2)))]
-                   [x y])))
-       frequencies
-       vals
-       (filter (fn [x] (>= x 2)))
-       count))
-
-
-(defn part2
-  [input]
-  (->> input
-       (mapcat (fn [[[x1 y1] [x2 y2]]]
-                 (let [[dx dy] [(cond (= x1 x2) 0
-                                      (< x1 x2) 1
-                                      :else -1)
-                                (cond (= y1 y2) 0
-                                      (< y1 y2) 1
-                                      :else -1)]
-                       num-step (max (Math/abs (long (- x1 x2))) (Math/abs (long (- y1 y2))))]
+(defn count-overlaps
+  [segments]
+  (->> segments
+       (mapcat (fn [[x1 y1 x2 y2]]
+                 (let [[dx dy] [(sign (- x2 x1)) (sign (- y2 y1))]
+                       num-step (max (Math/abs (long (- x1 x2)))
+                                     (Math/abs (long (- y1 y2))))]
                    (for [n (range (inc num-step))]
                      [(+ x1 (* n dx))
                       (+ y1 (* n dy))]))))
@@ -44,3 +23,23 @@
        vals
        (filter (fn [x] (>= x 2)))
        count))
+
+(defn parse
+  [lines]
+  (->> lines
+       (map (fn [line]
+              (->> (re-matches #"(\d+),(\d+) -> (\d+),(\d+)" line)
+                   rest
+                   (map #(Long/parseLong %)))))))
+
+(defn part1
+  [input]
+  (->> input
+       (filter (fn [[x1 y1 x2 y2]]
+                 (or (= x1 x2) (= y1 y2))))
+       count-overlaps))
+
+(defn part2
+  [input]
+  (->> input
+       count-overlaps))
