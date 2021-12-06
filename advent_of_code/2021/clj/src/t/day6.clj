f37458f7b3f3277bfc66c4d38048cf7c9e44b111
Gary Verhaegen <gary.verhaegen@gmail.com>
2021-12-06 06:45:23 +0100

    aoc/6: cleanup

--- advent_of_code/2021/clj/src/t/day6.clj
+++ advent_of_code/2021/clj/src/t/day6.clj
@@ -1,37 +1,35 @@
 (ns t.day6
   (:require [clojure.string :as string]))
 
+(defn one-day-later
+  [old-pop]
+  (reduce-kv (fn [new-pop age n]
+               (merge-with + new-pop
+                           (if (zero? age)
+                             {8 n 6 n}
+                             {(dec age) n})))
+             {}
+             old-pop))
+
+(defn n-days-later
+  [init-pop n-days]
+  (-> (iterate one-day-later init-pop)
+      (nth n-days)
+      vals
+      (->> (reduce +))))
+
 (defn parse
   [lines]
-  (-> lines first (string/split #",") (->> (map #(Long/parseLong %)))))
+  (-> lines
+      first
+      (string/split #",")
+      (->> (map #(Long/parseLong %)))
+      frequencies))
 
 (defn part1
   [input]
-  (->> input
-       frequencies
-       (iterate (fn [ages]
-                  (apply merge-with +
-                              (map (fn [[age n]]
-                                     (if (zero? age)
-                                       {8 n 6 n}
-                                       {(dec age) n}))
-                                   ages))))
-       (drop 80)
-       first
-       vals
-       (reduce +)))
+  (n-days-later input 80))
 
 (defn part2
   [input]
-  (->> input
-       frequencies
-       (iterate (fn [ages]
-                  (apply merge-with +
-                              (map (fn [[age n]]
-                                     (if (zero? age)
-                                       {8 n 6 n}
-                                       {(dec age) n})) ages))))
-       (drop 256)
-       first
-       vals
-       (reduce +)))
+  (n-days-later input 256))
