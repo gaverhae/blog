bd2ecfd1dfab4b721ae892e273bfb54662eee887
Gary Verhaegen <gary.verhaegen@gmail.com>
2021-12-10 06:37:41 +0100

    aoc/10: cleanup

--- advent_of_code/2021/clj/src/t/day10.clj
+++ advent_of_code/2021/clj/src/t/day10.clj
@@ -1,42 +1,41 @@
 (ns t.day10)
 
+(let [matching {\( \), \{ \}, \[ \], \< \>}
+      incorrect-score {\) 3, \] 57, \} 1197, \> 25137}
+      incomplete-score (fn [stack]
+                         (->> stack
+                              (map {\( 1, \[ 2, \{ 3, \< 4})
+                              (reduce (fn [acc el]
+                                        (+ (* 5 acc)  el)))))]
+  (defn parse-line
+    [line]
+    (loop [stack [(first line)]
+           [c & r] (rest line)]
+      (cond (and (nil? c) (empty? stack)) [:correct]
+            (nil? c) [:incomplete (incomplete-score stack)]
+            (contains? matching c) (recur (cons c stack) r)
+            (= (matching (first stack)) c) (recur (rest stack) r)
+            :else [:incorrect (incorrect-score c)]))))
+
+(defn median
+  [ls]
+  (nth (sort ls) (quot (count ls) 2)))
+
 (defn parse
   [lines]
-  (->> lines))
-
-(def matching
-  {\( \), \{ \}, \[ \], \< \>})
+  (->> lines
+       (map parse-line)))
 
 (defn part1
   [input]
   (->> input
-       (map (fn [s]
-              (loop [stack [(first s)]
-                     [c & r] (rest s)]
-                (cond (and (nil? c) (empty? stack)) :correct
-                      (nil? c) :incomplete
-                      (contains? matching c) (recur (cons c stack) r)
-                      (= (matching (first stack)) c) (recur (rest stack) r)
-                      :else ({\) 3, \] 57, \} 1197, \> 25137} c)))))
-       (filter int?)
+       (filter #(= :incorrect (first %)))
+       (map second)
        (reduce +)))
 
 (defn part2
   [input]
   (->> input
-       (map (fn [s]
-              (loop [stack [(first s)]
-                     [c & r] (rest s)]
-                (cond (and (nil? c) (empty? stack)) :correct
-                      (nil? c) (->> stack
-                                    (map {\( 1, \[ 2, \{ 3, \< 4})
-                                    (reduce (fn [acc el]
-                                              (+ (* 5 acc)  el))))
-                      (contains? matching c) (recur (cons c stack) r)
-                      (= (matching (first stack)) c) (recur (rest stack) r)
-                      :else :corrupted))))
-       (filter int?)
-       sort
-       ((fn [scores]
-          (nth scores (/ (dec (count scores)) 2))))))
-
+       (filter #(= :incomplete (first %)))
+       (map second)
+       median))
