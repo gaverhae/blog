a930e320d080671be4584bcd7167bb54b06b4829
Gary Verhaegen <gary.verhaegen@gmail.com>
2021-12-13 07:35:41 +0100

    aoc/13: cleanup

--- advent_of_code/2021/clj/src/t/day13.clj
+++ advent_of_code/2021/clj/src/t/day13.clj
@@ -14,47 +14,31 @@
                rest
                (map (fn [l]
                       (let [[_ coord n] (re-matches #"fold along (.)=(.*)" l)]
-                        [(keyword coord) (Long/parseLong n)]))))})
+                        [({"x" 0, "y" 1} coord) (Long/parseLong n)]))))})
+
+(defn apply-folds
+  [{:keys [folds dots]}]
+  (let [fold (fn [n pos]
+               (fn [v] (update v pos #(if (> % n) (- (* 2 n) %) %))))]
+    (->> (reductions (fn [dots [dir n]]
+                       (->> dots
+                            (map (fold n dir))
+                            set))
+                     dots
+                     folds))))
 
 (defn part1
-  [{:keys [folds dots]}]
-  (->> (reductions (fn [dots fold]
-                 (match fold
-                   [:x n] (->> dots
-                               (map (fn [[x y]]
-                                      [(if (> x n) (- (* 2 n) x) x)
-                                       y]))
-                               set)
-                   [:y n] (->> dots
-                               (map (fn [[x y]]
-                                      [x
-                                       (if (> y n) (- (* 2 n) y) y)]))
-                               set)))
-               dots
-               folds)
+  [input]
+  (->> (apply-folds input)
        second
        count))
 
 (defn part2
-  [{:keys [folds dots]}]
-  (let [final (reduce (fn [dots fold]
-                        (match fold
-                          [:x n] (->> dots
-                                      (map (fn [[x y]]
-                                             [(if (> x n) (- (* 2 n) x) x)
-                                              y]))
-                                      set)
-                          [:y n] (->> dots
-                                      (map (fn [[x y]]
-                                             [x
-                                              (if (> y n) (- (* 2 n) y) y)]))
-                                      set)))
-                      dots
-                      folds)
+  [input]
+  (let [final (last (apply-folds input))
         [w h] (reduce (fn [[w h] [x y]] [(max w x) (max h y)]) final)]
     (->> (for [y (range (inc h))]
           (for [x (range (inc w))]
             (if (contains? final [x y])
               \# \.)))
          (map (fn [l] (apply str l))))))
-
