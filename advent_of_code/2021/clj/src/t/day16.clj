abeb854e13a0bacedb3ec0e376eac0ad02f16332
Gary Verhaegen <gary.verhaegen@gmail.com>
2021-12-16 07:34:26 +0100

    aoc/16: parse second example

--- advent_of_code/2021/clj/src/t/day16.clj
+++ advent_of_code/2021/clj/src/t/day16.clj
@@ -4,15 +4,20 @@
 
 (def grammar
   "
-  S = packet <zeros>
+  S = packet leftover
   packet = literal-packet | operator-packet
   literal-packet = version <'100'> literal
   <literal> = (repeated-lit)* ending-lit
-  <version> = #'...'
-  <repeated-lit> = <'1'> #'....'
-  <ending-lit> = <'0'> #'....'
-  operator-packet = '11111'
-  zeros = '0'*
+  <version> = #'.{3}'
+  <repeated-lit> = <'1'> #'.{4}'
+  <ending-lit> = <'0'> #'.{4}'
+  operator-packet = version operator-type-id length payload
+  <operator-type-id> = '000' | '001' | '010' | '011' | '101' | '110' | '111'
+  <length> = length-bits | length-packets
+  length-bits = <'0'> #'.{15}'
+  length-packets = <'1'> #'.{11}'
+  <payload> = #'.*'
+  leftover = #'.*'
   ")
 
 (def to-ast (insta/parser grammar))
@@ -21,11 +26,22 @@
   [tree]
   (prn tree)
   (match tree
-    [:S packet] (parse-ast packet)
+    [:leftover ""] ()
+    [:leftover s] (parse-ast (to-ast s))
+    [:S packet leftover] (cons (parse-ast packet) (parse-ast leftover))
     [:packet packet] (parse-ast packet)
-    [:literal-packet version & lit] {:version (Long/parseLong version 2)
-                                     :type :literal
-                                     :value (Long/parseLong (apply str lit) 2)}))
+    [:literal-packet version & lit]
+    {:version (Long/parseLong version 2)
+     :type [:literal]
+     :value (Long/parseLong (apply str lit) 2)}
+    [:operator-packet version type [length-type length] payload]
+    {:version (Long/parseLong version 2)
+     :type [:operator (Long/parseLong type 2)]
+     :payload (let [length (Long/parseLong length 2)]
+                (case length-type
+                  :length-bits (parse-ast (to-ast (subs payload 0 length)))
+                  :length-packets (take length (parse-ast (to-ast payload)))))}
+    ))
 
 (defn parse
   [lines]
