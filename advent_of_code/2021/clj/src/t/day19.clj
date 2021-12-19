5e292a740f0e8a4813ecd9b60f0794fdd016a322
Gary Verhaegen <gary.verhaegen@gmail.com>
2021-12-19 07:53:11 +0100

    aoc/19: rotations

--- advent_of_code/2021/clj/src/t/day19.clj
+++ advent_of_code/2021/clj/src/t/day19.clj
@@ -14,6 +14,46 @@
                            (Long/parseLong z)]))
                       %)))))
 
+(defn all-beacons-as-origin
+  [probe]
+  (->> probe
+       (mapv (fn [[x0 y0 z0]]
+               (mapv (fn [[x1 y1 z1]]
+                       [(- x1 x0)
+                        (- y1 y0)
+                        (- z1 z0)]))))))
+
+(defn all-orientations
+  [probe]
+  (let [rotations [(fn [[x y z]] [(- x) (- y) z])
+                   (fn [[x y z]] [(- x) (- z) (- y)])
+                   (fn [[x y z]] [(- x) y (- z)])
+                   (fn [[x y z]] [(- x) z y])
+                   (fn [[x y z]] [(- y) (- x) (- z)])
+                   (fn [[x y z]] [(- y) (- z) x])
+                   (fn [[x y z]] [(- y) x z])
+                   (fn [[x y z]] [(- y) z (- x)])
+                   (fn [[x y z]] [(- z) (- x) y])
+                   (fn [[x y z]] [(- z) (- y) (- x)])
+                   (fn [[x y z]] [(- z) x (- y)])
+                   (fn [[x y z]] [(- z) y x])
+                   (fn [[x y z]] [x (- y) (- z)])
+                   (fn [[x y z]] [x (- z) y])
+                   (fn [[x y z]] [x y z])
+                   (fn [[x y z]] [x z (- y)])
+                   (fn [[x y z]] [y (- x) z])
+                   (fn [[x y z]] [y (- z) (- x)])
+                   (fn [[x y z]] [y x (- z)])
+                   (fn [[x y z]] [y z x])
+                   (fn [[x y z]] [z (- x) (- y)])
+                   (fn [[x y z]] [z (- y) x])
+                   (fn [[x y z]] [z x y])
+                   (fn [[x y z]] [z y (- x)])]]
+    (->> rotations
+         (map (fn [rot]
+                (->> probe
+                     (map rot)))))))
+
 (defn part1
   [input])
 
