dae0bac962f689a0f2246ec70d632af20e92a2da
Gary Verhaegen <gary.verhaegen@gmail.com>
2021-12-19 06:20:27 +0100

    aoc/19: parse

---
+++ advent_of_code/2021/clj/src/t/day19.clj
@@ -0,0 +1,21 @@
+(ns t.day19)
+
+(defn parse
+  [lines]
+  (let [scanner-line? (fn [l] (re-matches #"--- scanner \d+ ---" l))]
+    (->> lines
+         (partition-by scanner-line?)
+         (remove (fn [g] (scanner-line? (first g))))
+         (map #(remove #{""} %))
+         (mapv #(mapv (fn [l]
+                        (let [[_ x y z] (re-matches #"(-?\d+),(-?\d+),(-?\d+)" l)]
+                          [(Long/parseLong x)
+                           (Long/parseLong y)
+                           (Long/parseLong z)]))
+                      %)))))
+
+(defn part1
+  [input])
+
+(defn part2
+  [input])
