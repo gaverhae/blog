53d8f2deb0b2c4f79f064c775441b0e964584a8b
Gary Verhaegen <gary.verhaegen@gmail.com>
2021-12-20 06:28:26 +0100

    aoc/20: sample working, but no luck :(

--- advent_of_code/2021/clj/src/t/day20.clj
+++ advent_of_code/2021/clj/src/t/day20.clj
@@ -5,8 +5,31 @@
   {:alg (->> lines first (mapv {\. 0 \# 1}))
    :img (->> lines (drop 2) (mapv #(mapv {\. 0 \# 1} %)))})
 
+(defn neigh
+  [img x y]
+  (Long/parseLong (apply str (for [y [(dec y) y (inc y)]
+                                   x [(dec x) x (inc x)]]
+                               (get-in img [y x] 0))) 2))
+
+(defn improve
+  [{:keys [alg img]}]
+  (let [start-x -1
+        start-y -1
+        end-y (count img)
+        end-x (count img)]
+    {:alg alg
+     :img (vec (for [y (range start-y (inc end-y))]
+                 (vec (for [x (range start-x (inc end-x))]
+                        (get alg (neigh img x y))))))}))
+
 (defn part1
-  [input])
+  [input]
+  (->> (iterate improve input)
+       (drop 2)
+       first
+       :img
+       (mapcat (fn [line] (keep #{1} line)))
+       count))
 
 (defn part2
   [input])
