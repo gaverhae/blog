ae56f4ec9ccfa9594d7159bf42289e8a1001c1c0
Gary Verhaegen <gary.verhaegen@gmail.com>
2021-12-20 21:35:01 +0100

    aoc: record optimization needs

--- advent_of_code/2021/clj/src/t/core.clj
+++ advent_of_code/2021/clj/src/t/core.clj
@@ -7,6 +7,11 @@
   [f]
   (->> (crit/benchmark (f) {}) :mean first))
 
+(defn fmt
+  [f i]
+  (print (format f i))
+  (flush))
+
 (defn -main
   [& args]
   (doseq [i (map inc (range 20))]
@@ -17,33 +22,20 @@
           part2 (ns-resolve ns (symbol "part2"))
           data (string/split-lines (slurp (str "data/day" i)))
           input (parse data)]
-      (print (format (str "Day %02d,") i))
-      (flush)
-      #_(print (format (str " part 1: %15d\n")
-                     (part1 input)))
-      (flush)
-      #_(print (format (str "        part 2: %15d\n")
-                     (part2 input)))
-      (flush)
-      (when true
-        #_(print (format (str "        parse -> %14.3f\n")
-                       (bench #(parse data))))
-        (flush)
-        #_(print (format (str "        part1 -> %14.3f\n")
-                       (bench #(part1 input))))
-        (flush)
-        #_(print (format (str "        part2 -> %14.3f\n")
-                       (bench #(part2 input))))
-        (flush)
-        (print (format (str "        all   -> %14.3f\n")
-                       (bench #(do (parse data)
-                                   (part1 input)
-                                   (part2 input)))))
-        (flush))
-      #_(when (= i 12)
+      (when (= i 9) ;; 15 17 18 19 20
+        (fmt "Day %02d," i)
+        (fmt " part 1: %15d\n" (part1 input))
+        (fmt "        part 2: %15d\n" (part2 input))
+        (fmt "        parse -> %14.3f\n" (bench #(parse data)))
+        (fmt "        part1 -> %14.3f\n" (bench #(part1 input)))
+        (fmt "        part2 -> %14.3f\n" (bench #(part2 input)))
+        (fmt "        all   -> %14.3f\n" (bench #(do (parse data)
+                                                     (part1 input)
+                                                     (part2 input)))))
+      (when false
         (println "Waiting for profiler.")
         (read-line)
-        #_(crit/bench (parse data))
-        #_(crit/bench (part1 input))
-        (crit/bench (part2 input)))
+        (bench (parse data))
+        (bench (part1 input))
+        (bench (part2 input)))
       (flush))))
