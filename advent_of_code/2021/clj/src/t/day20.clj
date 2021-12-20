d0652c2156a473649ddb9fe25d7d4c867ec66760
Gary Verhaegen <gary.verhaegen@gmail.com>
2021-12-20 06:12:50 +0100

    aoc/20: parse

--- advent_of_code/2021/clj/src/t/day20.clj
+++ advent_of_code/2021/clj/src/t/day20.clj
@@ -1,7 +1,9 @@
 (ns t.day20)
 
 (defn parse
-  [lines])
+  [lines]
+  {:alg (->> lines first (mapv {\. 0 \# 1}))
+   :img (->> lines (drop 2) (mapv #(mapv {\. 0 \# 1} %)))})
 
 (defn part1
   [input])
