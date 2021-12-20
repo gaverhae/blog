5ff0cff0ce5e8d9181e12865faf901bc4cb3d8b3
Gary Verhaegen <gary.verhaegen@gmail.com>
2021-12-20 06:33:15 +0100

    aoc/20: but... I thought this would help... :'(

--- advent_of_code/2021/clj/src/t/day20.clj
+++ advent_of_code/2021/clj/src/t/day20.clj
@@ -3,16 +3,17 @@
 (defn parse
   [lines]
   {:alg (->> lines first (mapv {\. 0 \# 1}))
-   :img (->> lines (drop 2) (mapv #(mapv {\. 0 \# 1} %)))})
+   :img (->> lines (drop 2) (mapv #(mapv {\. 0 \# 1} %)))
+   :default (->> lines first first {\. 0 \# 1})})
 
 (defn neigh
-  [img x y]
+  [img x y default]
   (Long/parseLong (apply str (for [y [(dec y) y (inc y)]
                                    x [(dec x) x (inc x)]]
-                               (get-in img [y x] 0))) 2))
+                               (get-in img [y x] default))) 2))
 
 (defn improve
-  [{:keys [alg img]}]
+  [{:keys [alg img default]}]
   (let [start-x -1
         start-y -1
         end-y (count img)
@@ -20,7 +21,10 @@
     {:alg alg
      :img (vec (for [y (range start-y (inc end-y))]
                  (vec (for [x (range start-x (inc end-x))]
-                        (get alg (neigh img x y))))))}))
+                        (get alg (neigh img x y default))))))
+     :default (case default
+                0 (first alg)
+                1 (last alg))}))
 
 (defn part1
   [input]
