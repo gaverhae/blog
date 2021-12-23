01a4bac5a4d50f0e6eafef72d0c0fbcbb021449f
Gary Verhaegen <gary.verhaegen@gmail.com>
2021-12-23 06:14:09 +0100

    aoc/23: parse (?)

--- advent_of_code/2021/clj/src/t/day23.clj
+++ advent_of_code/2021/clj/src/t/day23.clj
@@ -1,7 +1,15 @@
-(ns t.day23)
+(ns t.day23
+  (:require [t.util :refer [transpose]]))
 
 (defn parse
-  [lines])
+  [lines]
+  {:hallway [0 0 0 0 0 0 0 0 0 0 0]
+   :halls (->> lines
+               (drop 2)
+               (take 2)
+               (map (fn [line] (keep {\A 1, \B 10, \C 100, \D 1000} line)))
+               transpose
+               (mapv vec))})
 
 (defn part1
   [input])
