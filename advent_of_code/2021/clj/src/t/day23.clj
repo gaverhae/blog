ba587e8b75a9c1cc830464d51841313c88fd6707
Gary Verhaegen <gary.verhaegen@gmail.com>
2021-12-23 06:31:10 +0100

    aoc/23: parse again

--- advent_of_code/2021/clj/src/t/day23.clj
+++ advent_of_code/2021/clj/src/t/day23.clj
@@ -3,16 +3,33 @@
 
 (defn parse
   [lines]
-  {:hallway [0 0 0 0 0 0 0 0 0 0 0]
-   :halls (->> lines
-               (drop 2)
-               (take 2)
-               (map (fn [line] (keep {\A 1, \B 10, \C 100, \D 1000} line)))
-               transpose
-               (mapv vec))})
+  (->> lines
+       (drop 2)
+       (take 2)
+       (map-indexed (fn [y line]
+                      (map-indexed (fn [x c]
+                                     (when-let [c ({\A 1 \B 10 \C 100 \D 1000} c)]
+                                       [[(- x 1) (inc y)] c]))
+                                   line)))
+       (apply concat)
+       (remove nil?)
+       (into {})))
+
+;(defn possible-moves
+;  [{:keys [hallway halls]}]
+;  (->> (concat (->> hallway
+;                    (map-indexed (fn [idx c] (when (pos? c) [idx c])))
+;                    (remove nil?))
+;               (->> halls
+;                    (map (fn [hall] (->> (map-indexed (fn [idx c] (when (pos? c) [idx c])) hall)
+;                                         (remove nil?)
+;                                         first)))
+;                    (remove nil?)))
+;       (map (fn [
 
 (defn part1
-  [input])
+  [input]
+  #_(possible-moves input))
 
 (defn part2
   [input])
