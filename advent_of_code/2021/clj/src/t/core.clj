76e9efba1083d2d19bbec1558e5819ab17c14087
Gary Verhaegen <gary.verhaegen@gmail.com>
2021-12-28 15:28:17 +0100

    aoc: more detailed runtimes

--- advent_of_code/2021/clj/src/t/core.clj
+++ advent_of_code/2021/clj/src/t/core.clj
@@ -15,8 +15,8 @@
      (quot (- end# start#) 1000000)))
 
 (defn fmt
-  [f i]
-  (println (format f i)))
+  [f & args]
+  (println (apply format f args)))
 
 (defn -main
   [& args]
@@ -28,15 +28,20 @@
                                 parse (ns-resolve ns (symbol "parse"))
                                 part1 (ns-resolve ns (symbol "part1"))
                                 part2 (ns-resolve ns (symbol "part2"))
+                                sam (string/split-lines (slurp (str "data/sample" day)))
+                                psam (parse sam)
                                 data (string/split-lines (slurp (str "data/day" day)))
                                 input (parse data)]
                             [day
                              {:total #(time-ms (do (parse data) (part1 input) (part2 input) nil))
+                              :parse-sam #(parse sam)
                               :parse #(parse data)
                               :part1 #(part1 input)
-                              :part2 #(part2 input)}])))
+                              :part1-sam #(part1 psam)
+                              :part2 #(part2 input)
+                              :part2-sam #(part2 psam)}])))
                   (into {}))]
-    (do
+    #_(do
       (->> days
            (map (fn [[day {:keys [total]}]]
                   [(total) day]))
@@ -45,10 +50,14 @@
            (map (fn [[t d]]
                   (println (format "Day %02d: %15dms" d t))))
            doall))
-    #_(doseq [[day {:keys [parse part1 part2]}] days]
-      (fmt "Day %02d, parse: %.3fms" day (bench (parse)))
-      (fmt "Day %02d, part1: %.3fms" day (bench (part1)))
-      (fmt "Day %02d, part2: %.3fms" day (bench (part2))))
+    (doseq [[day {:keys [parse part1 part2 parse-sam part1-sam part2-sam]}]
+            (select-keys days [23])]
+      (fmt "Day %02d, parse (sample): %8.3fs" day (bench parse-sam))
+      (fmt "Day %02d, parse (input):  %8.3fs" day (bench parse))
+      (fmt "Day %02d, part1 (sample): %8.3fs" day (bench part1-sam))
+      (fmt "Day %02d, part1 (input):  %8.3fs" day (bench part1))
+      (fmt "Day %02d, part2 (sample): %8.3fs" day (bench part2-sam))
+      (fmt "Day %02d, part2 (input):  %8.3fs" day (bench part2)))
     #_(let [to-profile (get-in days [23 :part2])]
       (println (format "Single run: %d." (time-ms (to-profile))))
       #_(println "Waiting for profiler.")
