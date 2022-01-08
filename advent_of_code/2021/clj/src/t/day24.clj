882d35d97bf274b0bfbab91544798654e354cc72
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-01-08 19:32:28 +0100

    aoc/24: compile expressions (~7s)

--- advent_of_code/2021/clj/src/t/day24.clj
+++ advent_of_code/2021/clj/src/t/day24.clj
@@ -78,6 +78,128 @@
                :else op))
     expr))
 
+(defmacro mdo
+  [bindings]
+  (if (#{0 1} (count bindings))
+    (throw
+      (RuntimeException. "invalid number of elements in mdo bindings"))
+    (let [[n v & r] bindings]
+      (if (empty? r)
+        v
+        [:bind v `(fn [~n] (mdo ~r))]))))
+
+(defn to-bindings
+  ([m] (to-bindings m {:bindings [], :counter 0}))
+  ([m state]
+   (match m
+     [:return v] [v state]
+     [:bind ma f] (let [[v state] (to-bindings ma state)]
+                    (to-bindings (f v) state))
+     [:emit expr] (let [r (symbol (str "r-" (:counter state)))]
+                    [r (-> state
+                           (update :counter inc)
+                           (update :bindings concat
+                                   [r expr]))])
+     [:emit-2 expr] (let [r1 (symbol (str "r-" (+ 0 (:counter state))))
+                          r2 (symbol (str "r-" (+ 1 (:counter state))))
+                          r3 (symbol (str "r-" (+ 2 (:counter state))))]
+                      [[r2 r3]
+                       (-> state
+                           (update :counter + 3)
+                           (update :bindings concat
+                                   [r1 expr r2 `(nth ~r1 0) r3 `(nth ~r1 1)]))]))))
+
+(defn compile-expr
+  [expr]
+  (let [h (fn rec [expr]
+            (match expr
+              [:reg r] [:emit-2 `(get ~'state ~r)]
+              [:inp] [:emit-2 'input]
+              [:lit n] [:emit-2 [n n]]
+              [:add e1 e2] (mdo [[m1 M1] (rec e1)
+                                 [m2 M2] (rec e2)
+                                 _ [:emit-2 [`(+ ~m1 ~m2)
+                                             `(+ ~M1 ~M2)]]])
+              [:mul e1 e2] (mdo [[m1 M1] (rec e1)
+                                 [m2 M2] (rec e2)
+                                 a [:emit `(* ~m1 ~m2)]
+                                 b [:emit `(* ~m1 ~M2)]
+                                 c [:emit `(* ~M1 ~m2)]
+                                 d [:emit `(* ~M1 ~M2)]
+                                 r1 [:emit `(min ~a ~b ~c ~d)]
+                                 r2 [:emit `(max ~a ~b ~c ~d)]
+                                 _ [:emit-2 [r1 r2]]])
+              [:div e [:lit n]] (mdo [[m M] (rec e)
+                                      _ [:emit-2 `(sort [(quot ~m ~n) (quot ~M ~n)])]])
+              [:mod e [:lit n]] (mdo [[m M] (rec e)
+                                      _ [:emit-2 `(if (or (> (- ~M ~m) ~n)
+                                                          (> (rem ~m ~n) (rem ~M ~n)))
+                                                    [0 (dec ~n)]
+                                                    [(rem ~m ~n) (rem ~M ~n)])]])
+              [:eql e1 e2] (mdo [[m1 M1] (rec e1)
+                                 [m2 M2] (rec e2)
+                                 _ [:emit-2 `(cond (< ~M2 ~m1) [0 0]
+                                                   (< ~M1 ~m2) [0 0]
+                                                   (= ~m1 ~M1 ~m2 ~M2) [1 1]
+                                                   :else [0 1])]])))
+        [result {:keys [bindings]}] (to-bindings (h expr))]
+    (eval `(fn [~'state ~'input]
+             (let [~@bindings]
+               ~result)))))
+
+(comment
+
+(def input (parse (clojure.string/split-lines (slurp "data/day24"))))
+
+(->> input
+     to-exprs
+     remove-unneeded-registers
+     simplify-expr
+     (map :z))
+([:add [:mul [:reg :z] [:add [:mul [:lit 25] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit 10]] [:inp]] [:lit 0]]] [:lit 1]]] [:mul [:add [:inp] [:lit 2]] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit 10]] [:inp]] [:lit 0]]]]
+
+ [:add [:mul [:reg :z] [:add [:mul [:lit 25] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit 15]] [:inp]] [:lit 0]]] [:lit 1]]] [:mul [:add [:inp] [:lit 16]] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit 15]] [:inp]] [:lit 0]]]]
+
+ [:add [:mul [:reg :z] [:add [:mul [:lit 25] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit 14]] [:inp]] [:lit 0]]] [:lit 1]]] [:mul [:add [:inp] [:lit 9]] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit 14]] [:inp]] [:lit 0]]]]
+
+ [:add [:mul [:reg :z] [:add [:mul [:lit 25] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit 15]] [:inp]] [:lit 0]]] [:lit 1]]] [:mul [:inp] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit 15]] [:inp]] [:lit 0]]]]
+
+ [:add [:mul [:div [:reg :z] [:lit 26]] [:add [:mul [:lit 25] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit -8]] [:inp]] [:lit 0]]] [:lit 1]]] [:mul [:add [:inp] [:lit 1]] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit -8]] [:inp]] [:lit 0]]]]
+
+ [:add [:mul [:reg :z] [:add [:mul [:lit 25] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit 10]] [:inp]] [:lit 0]]] [:lit 1]]] [:mul [:add [:inp] [:lit 12]] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit 10]] [:inp]] [:lit 0]]]]
+
+ [:add [:mul [:div [:reg :z] [:lit 26]] [:add [:mul [:lit 25] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit -16]] [:inp]] [:lit 0]]] [:lit 1]]] [:mul [:add [:inp] [:lit 6]] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit -16]] [:inp]] [:lit 0]]]]
+
+ [:add [:mul [:div [:reg :z] [:lit 26]] [:add [:mul [:lit 25] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit -4]] [:inp]] [:lit 0]]] [:lit 1]]] [:mul [:add [:inp] [:lit 6]] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit -4]] [:inp]] [:lit 0]]]]
+
+ [:add [:mul [:reg :z] [:add [:mul [:lit 25] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit 11]] [:inp]] [:lit 0]]] [:lit 1]]] [:mul [:add [:inp] [:lit 3]] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit 11]] [:inp]] [:lit 0]]]]
+
+ [:add [:mul [:div [:reg :z] [:lit 26]] [:add [:mul [:lit 25] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit -3]] [:inp]] [:lit 0]]] [:lit 1]]] [:mul [:add [:inp] [:lit 5]] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit -3]] [:inp]] [:lit 0]]]]
+
+ [:add [:mul [:reg :z] [:add [:mul [:lit 25] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit 12]] [:inp]] [:lit 0]]] [:lit 1]]] [:mul [:add [:inp] [:lit 9]] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit 12]] [:inp]] [:lit 0]]]]
+
+ [:add [:mul [:div [:reg :z] [:lit 26]] [:add [:mul [:lit 25] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit -7]] [:inp]] [:lit 0]]] [:lit 1]]] [:mul [:add [:inp] [:lit 3]] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit -7]] [:inp]] [:lit 0]]]]
+
+  [:add [:mul [:div [:reg :z] [:lit 26]] [:add [:mul [:lit 25] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit -15]] [:inp]] [:lit 0]]] [:lit 1]]] [:mul [:add [:inp] [:lit 2]] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit -15]] [:inp]] [:lit 0]]]]
+
+  )
+
+(def step [:add [:mul [:div [:reg :z] [:lit 26]] [:add [:mul [:lit 25] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit -7]] [:inp]] [:lit 0]]] [:lit 1]]] [:mul [:add [:inp] [:lit 3]] [:eql [:eql [:add [:mod [:reg :z] [:lit 26]] [:lit -7]] [:inp]] [:lit 0]]]])
+
+(compute-range-expr step [1 9] init-state)
+[4 12]
+
+(compile-expr step)
+
+((eval (compile-expr step)) init-state [1 9])
+[4 12]
+
+
+
+
+
+  )
+
 (defn compute-range-expr
   [exp input state]
   (match exp
@@ -119,13 +241,15 @@
                          (map (fn [step]
                                 (->> step
                                      (map (fn [[reg expr]]
-                                            [reg (simplify-expr expr)]))
+                                            [reg (-> expr
+                                                     simplify-expr
+                                                     compile-expr)]))
                                      (into {})))))
         h (fn rec [state exprs input-so-far]
             (let [[m M] (:z (reduce (fn [state expr]
                                       (->> expr
-                                           (map (fn [[reg expr]]
-                                                  [reg (compute-range-expr expr [1 9] state)]))
+                                           (map (fn [[reg f]]
+                                                  [reg (f state [1 9])]))
                                            (into {})))
                                     state
                                     exprs))]
@@ -139,8 +263,8 @@
                          ((fn [s] (if reverse? (reverse s) s)))
                          (some (fn [next-input]
                                  (rec (->> (first exprs)
-                                           (map (fn [[reg expr]]
-                                                  [reg (compute-range-expr expr [next-input next-input] state)]))
+                                           (map (fn [[reg f]]
+                                                  [reg (f state [next-input next-input])]))
                                            (into {}))
                                       (rest exprs)
                                       (+ (* 10 input-so-far) next-input))))))))]
