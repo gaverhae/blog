10fea5ded3de96eb78280ed1872b043a788756b4
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-01-23 19:05:58 +0100

    blog: opt-clj-2 final draft

--- blog/minicode/2022-01-16/src/t/core.clj
+++ blog/minicode/2022-01-16/src/t/core.clj
@@ -1,5 +1,6 @@
 (ns t.core
-  (:require [taoensso.tufte :as tufte :refer (p profiled)]
+  (:require [taoensso.tufte :as tufte :refer [p]]
+            [criterium.core :as crit]
             [clojure.string :as string]
             [clojure.set :as set])
   (:gen-class))
@@ -11,15 +12,23 @@
        (mapcat (fn [[a b]] [{a #{b}} {b #{a}}]))
        (apply merge-with set/union {})))
 
-(defn small?
+(defn p-small?
   [^String s]
   (p :small? (= s (.toLowerCase s))))
 
-(defn ends?
+(defn small?
+  [^String s]
+  (= s (.toLowerCase s)))
+
+(defn p-ends?
   [[path _]]
   (p :ends? (= (last path) "end")))
 
-(defn part2
+(defn ends?
+  [[path _]]
+  (= (last path) "end"))
+
+(defn p-part2
   [input]
   (loop [num-paths 0
          paths [[["start"] #{"start"} false]]]
@@ -34,21 +43,47 @@
                                          (not twice?))))]
                    [(p :conj-path (conj path next-step))
                     (p :visited
-                       (if (small? next-step)
+                       (if (p-small? next-step)
                          (conj visited next-step)
                          visited))
                     (p :twice-check
                        (or twice?
                            (boolean (visited next-step))))])]
-        (recur (p :filter-path (->> path (filter ends?) count (+ num-paths)))
-               (p :remove-ends (->> path (remove ends?))))))))
+        (recur (p :filter-path
+                  (->> path (filter p-ends?) count (+ num-paths)))
+               (p :remove-ends
+                  (->> path (remove p-ends?))))))))
+(defn part2
+  [input]
+  (loop [num-paths 0
+         paths [[["start"] #{"start"} false]]]
+    (if (empty? paths)
+      num-paths
+      (let [path (for [[path visited twice?] paths
+                       next-step (get input (last path))
+                       :when (or (not (visited next-step))
+                                 (and (not= "start" next-step)
+                                      (not twice?)))]
+                   [(conj path next-step)
+                    (if (small? next-step)
+                      (conj visited next-step)
+                      visited)
+                    (or twice?
+                        (boolean (visited next-step)))])]
+        (recur (->> path (filter ends?) count (+ num-paths))
+               (->> path (remove ends?)))))))
+
+(defn bench
+  [f]
+  (->> (crit/benchmark (f) {}) :mean first))
 
 (defn -main
   [& args]
+  (tufte/add-handler! :my-custom-dev-null (fn [m]))
   (let [input (-> (slurp "day12")
                   (string/split-lines)
                   parse)]
-    (->> (profiled {} (part2 input))
-         second
-         deref
-         pprint)))
+    (doseq [[tag f] [["no perf log" #(part2 input)]
+                     ["disabled" #(p-part2 input)]
+                     ["enabled" #(tufte/profile {} (p-part2 input))]]]
+      (println (format "%15s: %.3e" tag (bench f))))))
