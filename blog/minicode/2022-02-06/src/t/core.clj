861f9fdd23a753eb0356d562a9f9f48ee9e3c5d4
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-02-06 18:41:00 +0100

    blog: opt-clj-4 first draft

--- blog/minicode/2022-02-06/src/t/core.clj
+++ blog/minicode/2022-02-06/src/t/core.clj
@@ -11,14 +11,16 @@
   (loop [total 0, steps 1000000]
     (if (zero? steps)
       total
-      (recur (+ total steps) (dec steps)))))
+      (recur (+ total steps)
+             (dec steps)))))
 
 (defn unchecked
   []
   (loop [total 0, steps 1000000]
     (if (zero? steps)
       total
-      (recur (unchecked-add total steps) (unchecked-dec steps)))))
+      (recur (unchecked-add total steps)
+             (unchecked-dec steps)))))
 
 ;; this is the correct place to set the flag
 (set! *unchecked-math* true)
@@ -27,12 +29,52 @@
   (loop [total 0, steps 1000000]
     (if (zero? steps)
       total
-      (recur (+ total steps) (dec steps)))))
+      (recur (+ total steps)
+             (dec steps)))))
 (set! *unchecked-math* false)
+
+(defn boxed-add
+  [a b]
+  (+ a b))
+
+(defn boxed
+  []
+  (loop [total 0, steps 1000000]
+    (if (zero? steps)
+      total
+      (recur (boxed-add total steps)
+             (dec steps)))))
+
+(defn non-boxed-add
+  ^long [^long a ^long b]
+  (+ a b))
+
+(defn non-boxed
+  []
+  (loop [total 0, steps 1000000]
+    (if (zero? steps)
+      total
+      (recur (non-boxed-add total steps)
+             (dec steps)))))
+
+(defn non-boxed-unchecked-add
+  ^long [^long a ^long b]
+  (unchecked-add a b))
+
+(defn non-boxed-unchecked
+  []
+  (loop [total 0, steps 1000000]
+    (if (zero? steps)
+      total
+      (recur (non-boxed-unchecked-add total steps)
+             (unchecked-dec steps)))))
 
 (defn -main
   [& args]
-  (doseq [[n f] [["checked" checked]
-                 ["unchecked" unchecked]
-                 ["also-unchecked" also-unchecked]]]
+  (doseq [[n f] [#_["checked" checked]
+                 #_["unchecked" unchecked]
+                 #_["also-unchecked" also-unchecked]
+                 ["boxed" boxed]
+                 ["non-boxed" non-boxed]
+                 ["non-boxed-unchecked" non-boxed-unchecked]]]
     (println (format "%-15s: %.2e (%d)" n (bench f) (f)))))
