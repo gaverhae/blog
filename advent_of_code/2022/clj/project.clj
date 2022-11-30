3d29210036c474d12878d01b5d75f80a193e514c
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-11-30 22:25:16 +0100

    aoc22: bump deps

--- advent_of_code/2022/clj/project.clj
+++ advent_of_code/2022/clj/project.clj
@@ -1,12 +1,12 @@
 (defproject t "app"
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/core.match "1.0.0"]
-                 [instaparse "1.4.10"]
+                 [instaparse "1.4.12"]
                  [criterium "0.4.6"]
-                 [com.taoensso/tufte "2.2.0"]]
+                 [com.taoensso/tufte "2.4.5"]]
   :global-vars {*warn-on-reflection* true}
   :main ^:skip-aot t.core
-  :plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]]
+  :plugins [[com.jakemccrary/lein-test-refresh "0.25.0"]]
   :target-path "target/%s"
   :test-refresh {:quiet true
                  :changes-only true}
