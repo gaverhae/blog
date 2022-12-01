6880382aed53ac046718fca7816a4718f46de0cd
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-12-01 06:07:51 +0100

    aoc22: day 01

--- advent_of_code/2022/clj/src/t/day01.clj
+++ advent_of_code/2022/clj/src/t/day01.clj
@@ -3,15 +3,22 @@
             [clojure.string :as string]
             [clojure.test :refer [deftest are]]))
 
-(def parse lib/parse-integers)
+(defn parse
+  [input]
+  (->> input
+       (partition-by #(= % ""))
+       (remove #{[""]})
+       (map (fn [s] (reduce + 0 (map (fn [i] (Integer/parseInt i)) s))))))
+
+
 
 (defn part1
   [input]
-  0)
+  (reduce max input))
 
 (defn part2
   [input]
-  0)
+  (->> input sort reverse (take 3) (reduce + 0)))
 
 (deftest tests
   (are [expected input f]
@@ -19,8 +26,7 @@
                        string/split-lines
                        parse
                        f))
-       0 "sample" part1
-       ;0 "full" part1
-       ;0 "sample" part2
-       ;0 "full" part2
-       ))
+       24000 "sample" part1
+       75622 "full" part1
+       45000 "sample" part2
+       213159 "full" part2))
