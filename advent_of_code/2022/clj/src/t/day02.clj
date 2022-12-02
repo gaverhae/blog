57ac22f6e9a3fb2dc9c8beaf6dda27b2679df37e
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-12-02 06:31:38 +0100

    aoc22: day 02 clean-up

--- advent_of_code/2022/clj/src/t/day02.clj
+++ advent_of_code/2022/clj/src/t/day02.clj
@@ -4,45 +4,37 @@
             [clojure.string :as string]
             [clojure.test :refer [deftest are]]))
 
+(def rules
+  [[1 1 3] [1 2 6] [1 3 0]
+   [2 1 0] [2 2 3] [2 3 6]
+   [3 1 6] [3 2 0] [3 3 3]])
+
 (defn parse
   [input]
   input)
 
+(defn solve
+  [parse-second score-fn input]
+  (let [score (->> rules
+                   (map score-fn)
+                   (into {}))]
+        (->> input
+             (map (fn [s] [({\A 1, \B 2, \C 3} (get s 0))
+                           (parse-second (get s 2))]))
+             (map score)
+             (reduce + 0))))
+
 (defn part1
   [input]
-  (->> input
-       (map (fn [s] [({\A :rock, \B :paper, \C :scissors} (get s 0))
-                     ({\X :rock, \Y :paper, \Z :scissors} (get s 2))]))
-       (map (fn [[a b]]
-              (match [a b]
-                [:rock :rock] (+ 1 3)
-                [:rock :paper] (+ 2 6)
-                [:rock :scissors] (+ 3 0)
-                [:paper :rock] (+ 1 0)
-                [:paper :paper] (+ 2 3)
-                [:paper :scissors] (+ 3 6)
-                [:scissors :rock] (+ 1 6)
-                [:scissors :paper] (+ 2 0)
-                [:scissors :scissors] (+ 3 3))))
-       (reduce + 0)))
+  (solve {\X 1, \Y 2, \Z 3}
+         (fn [[a b c]] [[a b] (+ b c)])
+         input))
 
 (defn part2
   [input]
-  (->> input
-       (map (fn [s] [({\A :rock, \B :paper, \C :scissors} (get s 0))
-                     ({\X 0, \Y 3, \Z 6} (get s 2))]))
-       (map (fn [[a b]]
-              (match [a b]
-                [:rock 0] (+ 0 3)
-                [:rock 3] (+ 3 1)
-                [:rock 6] (+ 6 2)
-                [:paper 0] (+ 0 1)
-                [:paper 3] (+ 3 2)
-                [:paper 6] (+ 6 3)
-                [:scissors 0] (+ 0 2)
-                [:scissors 3] (+ 3 3)
-                [:scissors 6] (+ 6 1))))
-       (reduce + 0)))
+  (solve {\X 0, \Y 3, \Z 6}
+         (fn [[a b c]] [[a c] (+ b c)])
+         input))
 
 (deftest tests
   (are [expected input f]
@@ -53,5 +45,4 @@
        15 "sample" part1
        13268 "full" part1
        12 "sample" part2
-       15508 "full" part2
-       ))
+       15508 "full" part2))
