42c227f8ca9c06ef170df837246d1a66e390eb6e
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-12-03 16:24:01 +0100

    aoc22: day 03 clean-up

--- advent_of_code/2022/clj/src/t/day03.clj
+++ advent_of_code/2022/clj/src/t/day03.clj
@@ -2,37 +2,30 @@
   (:require [clojure.set :as set]
             [t.lib :as lib]))
 
-(defn parse
-  [input]
-  (->> input))
+(defn solve
+  [groups]
+  (->> groups
+       (map (fn [s] (->> s (map set) (apply set/intersection) first)))
+       (map (fn [c] (if (<= (int \a) (int c) (int \z))
+                      (+ 1 (- (int c) (int \a)))
+                      (+ 27 (- (int c) (int \A))))))
+       (reduce + 0)))
 
 (defn part1
   [input]
   (->> input
        (map (fn [line] (let [s (count line)]
-                         [(set (take (/ s 2) line))
-                          (set (drop (/ s 2) line))])))
-       (map (fn [[a b]] (set/intersection a b)))
-       (map first)
-       (map (fn [c] (if (<= (int \a) (int c) (int \z))
-                      (+ 1 (- (int c) (int \a)))
-                      (+ 27 (- (int c) (int \A))))))
-       (reduce + 0)))
+                         [(take (/ s 2) line)
+                          (drop (/ s 2) line)])))
+       solve))
 
 (defn part2
   [input]
   (->> input
        (partition 3)
-       (map (fn [[l1 l2 l3]]
-              (set/intersection (set l1) (set l2) (set l3))))
-       (map first)
-       (map (fn [c] (if (<= (int \a) (int c) (int \z))
-                      (+ 1 (- (int c) (int \a)))
-                      (+ 27 (- (int c) (int \A))))))
-       (reduce + 0)))
+       solve))
 
 (lib/check
-  parse
+  identity
   part1 157 7716
-  part2 70 2973
-  )
+  part2 70 2973)
