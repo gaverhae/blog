35e64cd5c2531c49376708a2a55e2b616f014f26
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-12-03 06:10:37 +0100

    aoc22: day 03

--- advent_of_code/2022/clj/src/t/day03.clj
+++ advent_of_code/2022/clj/src/t/day03.clj
@@ -1,19 +1,38 @@
 (ns t.day03
-  (:require [t.lib :as lib]))
+  (:require [clojure.set :as set]
+            [t.lib :as lib]))
 
 (defn parse
   [input]
-  input)
+  (->> input))
 
 (defn part1
   [input]
-  0)
+  (->> input
+       (map (fn [line] (let [s (count line)]
+                         [(set (take (/ s 2) line))
+                          (set (drop (/ s 2) line))])))
+       (map (fn [[a b]] (set/intersection a b)))
+       (map first)
+       (map (fn [c] (if (<= (int \a) (int c) (int \z))
+                      (+ 1 (- (int c) (int \a)))
+                      (+ 27 (- (int c) (int \A))))))
+       (reduce + 0)))
 
 (defn part2
   [input]
-  0)
+  (->> input
+       (partition 3)
+       (map (fn [[l1 l2 l3]]
+              (set/intersection (set l1) (set l2) (set l3))))
+       (map first)
+       (map (fn [c] (if (<= (int \a) (int c) (int \z))
+                      (+ 1 (- (int c) (int \a)))
+                      (+ 27 (- (int c) (int \A))))))
+       (reduce + 0)))
 
 (lib/check
   parse
-  part1
+  part1 157 7716
+  part2 70 2973
   )
