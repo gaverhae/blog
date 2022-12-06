9f62ec61ae1b9c58e333413d7a2564569c828f4c
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-12-07 00:41:12 +0100

    aoc22: day 07 prep

---
+++ advent_of_code/2022/clj/src/t/day07.clj
@@ -0,0 +1,22 @@
+(ns t.day07
+  (:require [t.lib :as lib]
+            [clojure.string :as string]
+            [clojure.set :as set]))
+
+(defn parse
+  [lines]
+  )
+
+(defn part1
+  [input]
+  )
+
+(defn part2
+  [input]
+  )
+
+(lib/check
+  parse
+  ;part1 0
+  ;part2 4 804
+  )
