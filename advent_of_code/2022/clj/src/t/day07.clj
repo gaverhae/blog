b1736cbcd664febd2565f87ceb69679b135b65c1
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-12-08 23:03:05 +0100

    aoc22: day 07: simplify parsing

--- advent_of_code/2022/clj/src/t/day07.clj
+++ advent_of_code/2022/clj/src/t/day07.clj
@@ -1,17 +1,6 @@
 (ns t.day07
   (:require [t.lib :as lib :refer [->long]]
-            [clojure.core.match :refer [match]]
-            [instaparse.core :as insta]))
-
-(def parser
-  (insta/parser
-    "S = cd | ls | dir | file
-    cd = <'$ cd '> word
-    ls = <'$ ls'>
-    dir = <'dir '> word
-    file = number <' '> word
-    <number> = #'\\d+'
-    <word> = #'[a-zA-Z0-9./]+'"))
+            [clojure.core.match :refer [match]]))
 
 (defn prefixes
   [v]
@@ -22,26 +11,24 @@
 (defn parse
   [lines]
   (->> lines
-       (map parser)
-       (map second)
-       (map (fn [line] (if (= :file (first line))
-                         (update line 1 ->long)
-                         line)))
+       (keep (fn [line]
+               (or (when-let [[_ dir] (re-matches #"\$ cd (\S+)" line)]
+                     [:cd dir])
+                   (when-let [[_ size] (re-matches #"(\d+) \S+" line)]
+                     [:file (->long size)]))))
        (reduce (fn [acc el]
                  (match el
                    [:cd "/"] (assoc acc :cwd ["/"])
                    [:cd ".."] (update acc :cwd pop)
                    [:cd child] (update acc :cwd conj child)
-                   [:ls] acc
-                   [:dir d] acc
-                   [:file size _] (assoc acc :tree
-                                         (reduce
-                                           (fn [acc el]
-                                             (update acc el (fnil + 0) size))
-                                           (:tree acc)
-                                           (prefixes (:cwd acc))))))
+                   [:file size] (assoc acc :sizes
+                                       (reduce
+                                         (fn [acc el]
+                                           (update acc el (fnil + 0) size))
+                                         (:sizes acc)
+                                         (prefixes (:cwd acc))))))
                {})
-       :tree))
+       :sizes))
 
 (defn part1
   [input]
