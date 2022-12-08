1302d717330736d70f620d81fde1c75824e53d12
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-12-08 06:31:30 +0100

    aoc22: day 08 part 2

--- advent_of_code/2022/clj/src/t/day08.clj
+++ advent_of_code/2022/clj/src/t/day08.clj
@@ -32,10 +32,23 @@
 
 (defn part2
   [input]
-  )
+  (let [h (count input)
+        w (count (first input))
+        tinput (lib/transpose input)
+        scenic (for [y (range 1 (dec h))
+                      x (range 1 (dec w))
+                      :let [v (get-in input [y x])
+                            f (fn f [s] (cond (empty? s) ()
+                                              (< (first s) v) (cons (first s) (f (rest s)))
+                                              :else [(first s)]))
+                            left (count (f (reverse (take x (get input y)))))
+                            right (count (f (drop (inc x) (get input y))))
+                            above (count (f (reverse (take y (get tinput x)))))
+                            below (count (f (drop (inc y) (get tinput x))))]]
+                  (* left right above below))]
+    (apply max scenic)))
 
 (lib/check
   parse
   part1 21 1533
-  ;part2 0 0
-  )
+  part2 8 345744)
