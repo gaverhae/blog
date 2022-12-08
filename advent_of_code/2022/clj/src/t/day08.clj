3213358d19592f8ea0f77875805eaa708c9c1ca0
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-12-08 06:19:55 +0100

    aoc22: day 08 part 1

--- advent_of_code/2022/clj/src/t/day08.clj
+++ advent_of_code/2022/clj/src/t/day08.clj
@@ -7,11 +7,28 @@
 
 (defn parse
   [lines]
-  )
+  (->> lines
+       (mapv (fn [line] (mapv (fn [c] (l (str c))) line)))))
 
 (defn part1
   [input]
-  )
+  (let [h (count input)
+        w (count (first input))
+        tinput (lib/transpose input)
+        visible (for [y (range 1 (dec h))
+                      x (range 1 (dec w))
+                      :let [v (get-in input [y x])
+                            left (take x (get input y))
+                            right (drop (inc x) (get input y))
+                            above (take y (get tinput x))
+                            below (drop (inc y) (get tinput x))]
+                      :when (or (> v (apply max left))
+                                (> v (apply max right))
+                                (> v (apply max above))
+                                (> v (apply max below)))]
+                  [x y])
+        border (- (* 2 (+ h w)) 4)]
+    (+ (count visible) border)))
 
 (defn part2
   [input]
@@ -19,6 +36,6 @@
 
 (lib/check
   parse
-  part1 0 0
+  part1 21 1533
   ;part2 0 0
   )
