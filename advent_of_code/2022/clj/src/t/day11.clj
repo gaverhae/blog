e8b0a99f1f2c996d9835596dbe71805a46fce371
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-12-11 05:56:42 +0100

    aoc22: day 11 prep

---
+++ advent_of_code/2022/clj/src/t/day11.clj
@@ -0,0 +1,24 @@
+(ns t.day11
+  (:require [clojure.string :as string]
+            [clojure.set :as set]
+            [clojure.core.match :refer [match]]
+            [instaparse.core :as insta]
+            [t.lib :as lib :refer [->long]]))
+
+(defn parse
+  [lines]
+  )
+
+(defn part1
+  [input]
+  )
+
+(defn part2
+  [input]
+  )
+
+(lib/check
+  parse
+  part1 #_0
+  #_part2
+  )
