ecdcd86bc75fddc8bcca41714b2afed81fe8b26b
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-12-12 06:41:45 +0100

    aoc22: day 12 part 1

---
+++ advent_of_code/2022/clj/src/t/day12.clj
@@ -0,0 +1,64 @@
+(ns t.day12
+  (:require [clojure.string :as string]
+            [clojure.set :as set]
+            [clojure.core.match :refer [match]]
+            [instaparse.core :as insta]
+            [t.lib :as lib :refer [->long]]))
+
+(defn dijkstra-search
+  [initial final? generate-moves]
+  (let [to-visit (java.util.PriorityQueue. 100 compare)]
+    (loop [[cost state] [0 initial]
+           visited #{}]
+      (when (not (visited state))
+        (doseq [[nxt-state nxt-cost] (generate-moves [state cost])]
+          (when (not (visited nxt-state))
+            (.add to-visit [nxt-cost nxt-state]))))
+      (if (final? state)
+        cost
+        (recur (.poll to-visit)
+               (conj visited state))))))
+
+(defn parse
+  [lines]
+  (->> lines
+       (mapv (fn [line] (mapv (fn [c] (long c)) line)))))
+
+(defn neighbours
+  [[x y]]
+  #{[(inc x) y] [(dec x) y] [x (inc y)] [x (dec y)]})
+
+(defn height
+  [input pos]
+  (when-let [h (get-in input pos)]
+    (cond (== h (long \S)) (long \a)
+          (== h (long \E)) (long \z)
+          :else h)))
+
+(defn part1
+  [input]
+  (dijkstra-search
+    (loop [j 0
+           i 0]
+      (cond (== i (count (first input))) (recur (inc j) 0)
+            (== (long \S) (get-in input [j i])) [j i]
+            :else (recur j (inc i))))
+    (fn [p] (when-let [s (get-in input p)]
+              (== (long \E) s)))
+    (fn [[pos cost-so-far]]
+      (->> (neighbours pos)
+           (keep (fn [p] (when-let [c (height input p)]
+                           (when (>= 1 (- (height input p)
+                                          (height input pos)))
+                             [p (inc cost-so-far)]))))
+           set))))
+
+(defn part2
+  [input]
+  )
+
+(lib/check
+  parse
+  part1 31 383
+  #_part2
+  )
