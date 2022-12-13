4ade4ae56b4daf8ed6ab4014ead0ff47aead10f3
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-12-13 06:32:15 +0100

    aoc22: day 13 part 1

--- advent_of_code/2022/clj/src/t/day13.clj
+++ advent_of_code/2022/clj/src/t/day13.clj
@@ -1,5 +1,6 @@
 (ns t.day13
-  (:require [clojure.string :as string]
+  (:require [clojure.edn :as edn]
+            [clojure.string :as string]
             [clojure.set :as set]
             [clojure.core.match :refer [match]]
             [instaparse.core :as insta]
@@ -7,11 +8,48 @@
 
 (defn parse
   [lines]
-  )
+  (->> lines
+       (partition-by #{""})
+       (remove #{[""]})
+       (map (fn [pair] (mapv edn/read-string pair)))))
+
+(defn smaller-than
+  [left right]
+  (loop [i 0]
+    (cond (== i (count left) (count right)) :unknown
+          (== i (count left)) true
+          (== i (count right)) false
+          (and (integer? (get left i))
+               (integer? (get right i)))
+          (cond (< (get left i) (get right i))
+                true
+                (> (get left i) (get right i))
+                false
+                :equals (recur (inc i)))
+          (integer? (get left i))
+          (let [rec (smaller-than [(get left i)] (get right i))]
+            (if (= :unknown rec)
+              (recur (inc i))
+              rec))
+          (integer? (get right i))
+          (let [rec (smaller-than (get left i) [(get right i)])]
+            (if (= :unknown rec)
+              (recur (inc i))
+              rec))
+          :both-lists
+          (let [r1 (smaller-than (get left i) (get right i))]
+            (if (= :unknown r1)
+              (recur (inc i))
+              r1)))))
 
 (defn part1
   [input]
-  )
+  (->> input
+       (keep-indexed
+         (fn [idx [left right]]
+           (when (smaller-than left right) idx)))
+       (map inc)
+       (reduce + 0)))
 
 (defn part2
   [input]
@@ -19,6 +57,6 @@
 
 (lib/check
   parse
-  part1
+  part1 13 5588
   #_part2
   )
