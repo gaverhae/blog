3f80159d5d9ce964923c587696b970effc4f6d2c
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-12-14 06:36:20 +0100

    aoc22: day 14 part 2

--- advent_of_code/2022/clj/src/t/day14.clj
+++ advent_of_code/2022/clj/src/t/day14.clj
@@ -44,11 +44,38 @@
 
 (defn part2
   [input]
-  )
+  (let [grid (->> input
+                  (mapcat (fn [line] (partition 2 1 line)))
+                  (mapcat (fn [[[x1 y1] [x2 y2]]]
+                            (for [x (range (min x1 x2) (inc (max x1 x2)))
+                                  y (range (min y1 y2) (inc (max y1 y2)))]
+                              [x y])))
+                  (reduce (fn [acc p]
+                            (assoc acc p :rock))
+                          {}))
+        max-y (+ 2 (reduce (fn [acc [_ y]] (max acc y)) 0 (keys grid)))
+        lookup (fn [grid [x y]]
+                 (if (== y max-y)
+                   :rock
+                   (get grid [x y])))]
+    (loop [i 0
+           grid grid]
+      (if (= :sand (get grid [500 0]))
+        i
+        (let [next-grid (loop [[x y] [500 0]]
+                          (cond (nil? (lookup grid [x (inc y)]))
+                                (recur [x (inc y)])
+                                (nil? (lookup grid [(dec x) (inc y)]))
+                                (recur [(dec x) (inc y)])
+                                (nil? (lookup grid [(inc x) (inc y)]))
+                                (recur [(inc x) (inc y)])
+                                :else
+                                (assoc grid [x y] :sand)))]
+          (recur (inc i) next-grid))))))
 
 
 (lib/check
   parse
   part1 24 799
-  #_part2
+  part2 93 29076
   )
