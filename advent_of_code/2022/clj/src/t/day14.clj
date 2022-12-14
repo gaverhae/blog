b4c01fdf9a27112947fb631d03c5180d2c15d41d
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-12-14 06:30:24 +0100

    aoc22: day 14 part 1

---
+++ advent_of_code/2022/clj/src/t/day14.clj
@@ -0,0 +1,54 @@
+(ns t.day14
+  (:require [clojure.edn :as edn]
+            [clojure.string :as string]
+            [clojure.set :as set]
+            [clojure.core.match :refer [match]]
+            [instaparse.core :as insta]
+            [t.lib :as lib :refer [->long]]))
+
+(defn parse
+  [lines]
+  (->> lines
+       (map (fn [line]
+              (map (fn [p] (map ->long (string/split p #",")))
+                   (string/split line #" -> " ))))))
+
+(defn part1
+  [input]
+  (let [grid (->> input
+                  (mapcat (fn [line] (partition 2 1 line)))
+                  (mapcat (fn [[[x1 y1] [x2 y2]]]
+                            (for [x (range (min x1 x2) (inc (max x1 x2)))
+                                  y (range (min y1 y2) (inc (max y1 y2)))]
+                              [x y])))
+                  (reduce (fn [acc p]
+                            (assoc acc p :rock))
+                          {}))
+        max-y (reduce (fn [acc [_ y]] (max acc y)) 0 (keys grid))]
+    (loop [i 0
+           grid grid]
+      (let [next-grid (loop [[x y] [500 0]]
+                        (cond (>= y max-y)
+                              :stop
+                              (nil? (get grid [x (inc y)]))
+                              (recur [x (inc y)])
+                              (nil? (get grid [(dec x) (inc y)]))
+                              (recur [(dec x) (inc y)])
+                              (nil? (get grid [(inc x) (inc y)]))
+                              (recur [(inc x) (inc y)])
+                              :else
+                              (assoc grid [x y] :sand)))]
+        (if (= next-grid :stop)
+          i
+          (recur (inc i) next-grid))))))
+
+(defn part2
+  [input]
+  )
+
+
+(lib/check
+  parse
+  part1 24 799
+  #_part2
+  )
