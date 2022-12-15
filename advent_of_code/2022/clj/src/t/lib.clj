95c6066bca92bd1a57f6ac9779509fa669e7129b
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-12-15 10:12:06 +0100

    aoc22: lib/check: parse each file only once

--- advent_of_code/2022/clj/src/t/lib.clj
+++ advent_of_code/2022/clj/src/t/lib.clj
@@ -15,29 +15,31 @@
 (defmacro check
   [& specs]
   `(deftest ~'check
-     ~@(->> specs
-            (partition 2)
-            (map (fn [[[part input & args] expected]]
-                   (let [n (gensym)
-                         file (gensym)]
-                     `(let [~n (-> (ns-name ~*ns*)
-                                   (string/replace "t.day" "")
-                                   (Long/parseLong))
-                            ~file (format "data/day%02d-%s" ~n ~(name input))]
-                        ~(when (= (name input) "puzzle")
-                           `(when (not (.exists (io/file ~file)))
-                              (spit ~file
-                                    (-> (format "https://adventofcode.com/2022/day/%d/input" ~n)
-                                        (hc/get {:headers
-                                                 {"cookie" (format "session=%s"
-                                                                   (System/getenv "AOC_SESSION"))}})
-                                        :body))))
-                        (is (= (apply ~part (-> ~file
-                                                slurp
-                                                string/split-lines
-                                                ~'parse)
-                                      ~(vec args))
-                               ~expected)))))))))
+     (let [~'day (-> (ns-name ~*ns*)
+                     (string/replace "t.day" "")
+                     (Long/parseLong))
+           ~'sample (delay (-> (format "data/day%02d-sample" ~'day)
+                               slurp
+                               string/split-lines
+                               ~'parse))
+           ~'puzzle (delay
+                      (let [file# (format "data/day%02d-puzzle" ~'day)]
+                        (when (not (.exists (io/file file#)))
+                          (spit file#
+                                (-> (format "https://adventofcode.com/2022/day/%d/input"
+                                            ~'day)
+                                    (hc/get {:headers
+                                             {"cookie" (format "session=%s"
+                                                               (System/getenv "AOC_SESSION"))}})
+                                    :body)))
+                        (-> file#
+                            slurp
+                            string/split-lines
+                            ~'parse)))]
+       ~@(->> specs
+              (partition 2)
+              (map (fn [[[part input & args] expected]]
+                     `(is (= ~(concat [part `(force ~input)] args) ~expected))))))))
 
 (deftest tests
   (testing "transpose"
