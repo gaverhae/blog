241425db21c25438d14c78bc6306e114c3279fa7
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-12-16 07:40:15 +0100

    aoc22: day 16 part 1 slow (1011s)

--- advent_of_code/2022/clj/src/t/day16.clj
+++ advent_of_code/2022/clj/src/t/day16.clj
@@ -8,19 +8,47 @@
 
 (defn parse
   [lines]
-  lines)
+  (->> lines
+       (map (fn [line]
+              (let [[_ valve rate tunnels]
+                    (re-matches #"Valve ([A-Z]+) has flow rate=(\d+); tunnels? leads? to valves? (.*)" line)]
+                [valve {:rate (->long rate)
+                        :tunnels (string/split tunnels #", ")}])))
+       (into {})))
 
 (defn part1
   [input]
-  input)
+  (loop [i 0
+         states #{{:pos "AA", :opened? #{}, :released 0}}]
+    (prn [i (count states)])
+    (if (== 30 i)
+      (->> states (map :released) sort reverse first)
+      (recur (inc i)
+             (->> states
+                  (mapcat (fn [s]
+                            (let [r (reduce +
+                                            (:released s)
+                                            (map #(get-in input [% :rate])
+                                                 (:opened? s)))]
+                              (->> (get-in input [(:pos s) :tunnels])
+                                   (map (fn [n]
+                                          (-> s
+                                              (assoc :pos n)
+                                              (assoc :released r))))
+                                   (concat (when (and (not ((:opened? s) (:pos s)))
+                                                      (pos? (get-in input [(:pos s) :rate])))
+                                             [(-> s
+                                                  (update :opened? conj (:pos s))
+                                                  (assoc :released r))]))))))
+                  set)))))
 
 (defn part2
   [input]
   input)
 
 (lib/check
-  #_#_[part1 sample] 0
-  #_#_[part1 puzzle] 0
+  [part1 sample] 1651
+  [part1 puzzle] 0
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0
   )
