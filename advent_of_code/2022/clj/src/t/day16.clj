84cce2799614acd6cb972ada7f7bbe1607e6ee7c
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-12-16 09:01:35 +0100

    aoc22: day 16 part 2, probably, but still just as slow

--- advent_of_code/2022/clj/src/t/day16.clj
+++ advent_of_code/2022/clj/src/t/day16.clj
@@ -42,42 +42,45 @@
 
 (defn part2
   [input]
-  (loop [i 0
-         states {["AA" "AA"] {#{} 0}}]
-    (prn [i (->> states vals (mapcat vals) count)])
-    (if (== 26 i)
-      (->> states vals (mapcat vals) sort reverse first)
-      (recur (inc i)
-             (->> states
-                  (mapcat (fn [[[cur-h cur-e] states]]
-                            (->> states
-                                 (mapcat (fn [[opened? released]]
-                                           (let [r (reduce +
-                                                           released
-                                                           (map #(get-in input [% :rate])
-                                                                opened?))]
-                                             (for [h (conj (get-in input [cur-h :tunnels]) cur-h)
-                                                   e (conj (get-in input [cur-e :tunnels]) cur-e)
-                                                   :when (or (and (not= h cur-h)
-                                                                  (not= e cur-e))
-                                                             (and (not= h cur-h)
-                                                                  (not (opened? cur-e))
-                                                                  (pos? (get-in input [cur-e :rate])))
-                                                             (and (not= e cur-e)
-                                                                  (not (opened? cur-h))
-                                                                  (pos? (get-in input [cur-h :rate])))
-                                                             (and (not (opened? cur-e))
-                                                                  (pos? (get-in input [cur-e :rate]))
-                                                                  (not (opened? cur-h))
-                                                                  (pos? (get-in input [cur-h :rate]))))]
-                                               [(vec (sort [h e]))
-                                                [(cond-> opened?
-                                                   (= h cur-h) (conj h)
-                                                   (= e cur-e) (conj e))
-                                                 r]])))))))
-                  (reduce (fn [states [pos [opened released]]]
-                            (update-in states [pos opened] (fnil max 0) released))
-                          {}))))))
+  (let [all-valves (->> input keys set)]
+    (loop [i 0
+           states {["AA" "AA"] {#{} 0}}]
+      (prn [i (->> states vals (mapcat vals) count)])
+      (if (== 26 i)
+        (->> states vals (mapcat vals) sort reverse first)
+        (recur (inc i)
+               (->> states
+                    (mapcat (fn [[[cur-h cur-e] states]]
+                              (->> states
+                                   (mapcat (fn [[opened? released]]
+                                             (let [r (reduce +
+                                                             released
+                                                             (map #(get-in input [% :rate])
+                                                                  opened?))]
+                                               (if (= opened? all-valves)
+                                                 [:done [all-valves r]]
+                                                 (for [h (conj (get-in input [cur-h :tunnels]) cur-h)
+                                                       e (conj (get-in input [cur-e :tunnels]) cur-e)
+                                                       :when (or (and (not= h cur-h)
+                                                                      (not= e cur-e))
+                                                                 (and (not= h cur-h)
+                                                                      (not (opened? cur-e))
+                                                                      (pos? (get-in input [cur-e :rate])))
+                                                                 (and (not= e cur-e)
+                                                                      (not (opened? cur-h))
+                                                                      (pos? (get-in input [cur-h :rate])))
+                                                                 (and (not (opened? cur-e))
+                                                                      (pos? (get-in input [cur-e :rate]))
+                                                                      (not (opened? cur-h))
+                                                                      (pos? (get-in input [cur-h :rate]))))]
+                                                   [(vec (sort [h e]))
+                                                    [(cond-> opened?
+                                                       (= h cur-h) (conj h)
+                                                       (= e cur-e) (conj e))
+                                                     r]]))))))))
+                    (reduce (fn [states [pos [opened released]]]
+                              (update-in states [pos opened] (fnil max 0) released))
+                            {})))))))
 
 (lib/check
   #_#_[part1 sample] 1651
