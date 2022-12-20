0d6f7538531ad096aff9230d52e5edabcc809148
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-12-20 10:42:43 +0100

    aoc22: day 20 part 1

--- advent_of_code/2022/clj/src/t/day20.clj
+++ advent_of_code/2022/clj/src/t/day20.clj
@@ -12,17 +12,19 @@
        (mapv ->long)))
 
 (defn rotate
-  [acc ^long el]
-  (let [current-index (.indexOf ^java.util.List acc el)
+  [acc [^long idx ^long el]]
+  (let [current-index (.indexOf ^java.util.List acc [idx el])
         centered (concat (rest (drop current-index acc))
                          (take current-index acc))
-        c (cycle (if (neg? el) (reverse centered) centered))
-        r (take (count acc) (cons el (drop (Math/abs el) c)))]
-    (if (neg? el) (reverse r) r)))
+        rev (if (neg? el) reverse identity)
+        c (cycle (rev centered))
+        r (take (count acc) (cons [idx el] (drop (Math/abs el) c)))]
+    (rev r)))
 
 (defn part1
   [input]
-  (let [v (vec (reduce rotate input input))
+  (let [marked (->> input (map-indexed vector))
+        v (mapv second (reduce rotate marked marked))
         zero-idx (.indexOf ^java.util.List v 0)]
     (+ (v (mod (+ zero-idx 1000) (count v)))
        (v (mod (+ zero-idx 2000) (count v)))
