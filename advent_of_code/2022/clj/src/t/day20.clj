bfbab064237adffea9e6eeea5539d3ca202d5fa5
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-12-20 09:50:50 +0100

    aoc22: day 20 part 1: ok for sample, bad for puzzle

--- advent_of_code/2022/clj/src/t/day20.clj
+++ advent_of_code/2022/clj/src/t/day20.clj
@@ -8,18 +8,42 @@
 
 (defn parse
   [lines]
-  lines)
+  (->> lines
+       (mapv ->long)))
+
+(defn rotate
+  [acc el]
+  (let [len (count acc)
+        current-index (.indexOf ^java.util.List acc el)
+        target-index (let [t (+ el current-index)]
+                       (cond (neg? t) (dec (mod t len))
+                             (>= t len) (inc (mod t len))
+                             :else t))
+        steps (Math/abs (long (- current-index target-index)))]
+    (if (> current-index target-index)
+      (vec (concat (subvec acc 0 target-index)
+                   [el]
+                   (subvec acc target-index current-index)
+                   (subvec acc (inc current-index))))
+      (vec (concat (subvec acc 0 current-index)
+                   (subvec acc (inc current-index) (inc target-index))
+                   [el]
+                   (subvec acc (inc target-index)))))))
 
 (defn part1
   [input]
-  input)
+  (let [v (reduce rotate input input)
+        zero-idx (.indexOf ^java.util.List v 0)]
+    (+ (v (mod (+ zero-idx 1000) (count v)))
+       (v (mod (+ zero-idx 2000) (count v)))
+       (v (mod (+ zero-idx 3000) (count v))))))
 
 (defn part2
   [input]
   input)
 
 (lib/check
-  #_#_[part1 sample] 0
-  #_#_[part1 puzzle] 0
+  [part1 sample] 3
+  [part1 puzzle] 0
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
