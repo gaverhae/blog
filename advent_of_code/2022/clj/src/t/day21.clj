c9e66374f89ca84bb1dbba91c03ac051ef2062d4
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-12-21 09:53:24 +0100

    aoc22: day 21 part 1

--- advent_of_code/2022/clj/src/t/day21.clj
+++ advent_of_code/2022/clj/src/t/day21.clj
@@ -8,18 +8,30 @@
 
 (defn parse
   [lines]
-  lines)
+  (->> lines
+     (map (fn [line]
+            (if-let [[_ n i] (re-matches #"([a-z]+): (\d+)" line)]
+              [n [:lit (->long i)]]
+              (let [[_ n op1 f op2] (re-matches #"([a-z]+): ([a-z]+) (\+|-|\*|/) ([a-z]+)" line)]
+                [n [:op f op1 op2]]))))
+     (into {})))
 
 (defn part1
   [input]
-  input)
+  (let [ev (fn ev [env op]
+             (match op
+               [:lit i] i
+               [:op f n1 n2] (({"+" +, "-" -, "*" *, "/" /} f)
+                              (ev env (env n1))
+                              (ev env (env n2)))))]
+    (ev input (input "root"))))
 
 (defn part2
   [input]
   input)
 
 (lib/check
-  [part1 sample] 0
-  [part1 puzzle] 0
-  [part2 sample] 0
-  [part2 puzzle] 0)
+  [part1 sample] 152
+  [part1 puzzle] 72664227897438
+  #_#_[part2 sample] 0
+  #_#_[part2 puzzle] 0)
