863f208840b21621ea0aebb899dabda16381d66d
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-12-24 06:37:44 +0100

    aoc22: day 24: prep

--- advent_of_code/2022/clj/src/t/day23.clj
+++ advent_of_code/2022/clj/src/t/day23.clj
@@ -23,64 +23,8 @@
    [[y (dec x)] [[y (dec x)] [(inc y) (dec x)] [(dec y) (dec x)]]]
    [[y (inc x)] [[y (inc x)] [(inc y) (inc x)] [(dec y) (inc x)]]]])
 
-(defn part1
-  [input]
-  (loop [n 0
-         directions 0
-         positions (->> input (map-indexed (fn [idx p] [p idx]))
-                        (into {}))]
-    (if (== 10 n)
-      (let [[xmin xmax ymin ymax] (reduce (fn [[xmin xmax ymin ymax] [y x]]
-                                            [(min xmin x)
-                                             (max xmax x)
-                                             (min ymin y)
-                                             (max ymax y)])
-                                          [(get (first (keys positions)) 1)
-                                           (get (first (keys positions)) 1)
-                                           (get (first (keys positions)) 0)
-                                           (get (first (keys positions)) 0)]
-                                          (rest (keys positions)))]
-        (count (for [x (range xmin (inc xmax))
-                     y (range ymin (inc ymax))
-                     :when (not (positions [y x]))]
-                 1)))
-      (let [proposals (->> positions
-                           (map (fn [pos] [pos (->> (neighbours (key pos))
-                                                    cycle
-                                                    (drop directions)
-                                                    (take 4))]))
-                           (remove (fn [[pos neighs]]
-                                     (->> neighs
-                                          (mapcat (fn [[_ adj]] adj))
-                                          (map positions)
-                                          (every? nil?))))
-                           (reduce (fn [acc [pos neighs]]
-                                     (let [prop (->> neighs
-                                                     (some (fn [[move looks]]
-                                                             (when (every? nil? (map positions looks))
-                                                               move))))]
-                                       (assoc acc (val pos) prop)))
-                                   {}))
-            conflicts (->> proposals
-                           vals
-                           (remove nil?)
-                           frequencies
-                           (filter (fn [[k v]] (>= v 2)))
-                           keys
-                           set)]
-        (recur (inc n)
-               (mod (inc directions) 4)
-               (->> positions
-                    (map (fn [[p idx]]
-                           (let [to (proposals idx)]
-                             (if (and to (not (conflicts to)))
-                               [to idx]
-                               [p idx]))))
-                    (into {})))))))
-
-
-(defn part2
-  [input]
+(defn solve
+  [input stop?]
   (loop [n 0
          directions 0
          positions (->> input (map-indexed (fn [idx p] [p idx]))
@@ -115,15 +59,39 @@
                                       (if (and to (not (conflicts to)))
                                         [to idx]
                                         [p idx]))))
-                             (into {}))]
-      (if (= new-positions positions)
-        (inc n)
-        (recur (inc n)
+                             (into {}))
+          n (inc n)]
+      (if (stop? n positions new-positions)
+        [n new-positions]
+        (recur n
                (mod (inc directions) 4)
                new-positions)))))
 
+(defn part1
+  [input]
+  (let [[_ positions] (solve input (fn [n _ _] (== n 10)))
+        [xmin xmax ymin ymax] (reduce (fn [[xmin xmax ymin ymax] [y x]]
+                                        [(min xmin x)
+                                         (max xmax x)
+                                         (min ymin y)
+                                         (max ymax y)])
+                                      [(get (first (keys positions)) 1)
+                                       (get (first (keys positions)) 1)
+                                       (get (first (keys positions)) 0)
+                                       (get (first (keys positions)) 0)]
+                                      (rest (keys positions)))]
+    (count (for [x (range xmin (inc xmax))
+                 y (range ymin (inc ymax))
+                 :when (not (positions [y x]))]
+             1))))
+
+(defn part2
+  [input]
+  (let [[n _] (solve input (fn [_ p1 p2] (= p1 p2)))]
+    n))
+
 (lib/check
-  [part1 sample] 110
-  [part1 puzzle] 3996
-  [part2 sample] 20
-  [part2 puzzle] 908)
+  #_#_[part1 sample] 110
+  #_#_[part1 puzzle] 3996
+  #_#_[part2 sample] 20
+  #_#_[part2 puzzle] 908)
