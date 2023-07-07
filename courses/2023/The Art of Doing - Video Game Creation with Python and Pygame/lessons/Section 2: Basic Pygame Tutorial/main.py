254d76b06fd2dffca8f80cec7ff7231ade4f90c1
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-07-07 14:09:09 +0200

    aodpg: lesson 2.2: drawing shapes

--- courses/2023/The Art of Doing - Video Game Creation with Python and Pygame/lessons/Section 2: Basic Pygame Tutorial/main.py
+++ courses/2023/The Art of Doing - Video Game Creation with Python and Pygame/lessons/Section 2: Basic Pygame Tutorial/main.py
@@ -3,17 +3,34 @@ import pygame
 pygame.init()
 
 width = 600
-height = 300
+height = 600
 
 display = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Hello World!")
+pygame.display.set_caption("Drawing Objects")
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+yellow = (255, 255, 0)
+cyan = (0, 255, 255)
+magenta = (255, 0, 255)
+
+display.fill(blue)
+pygame.draw.line(display, red, (0, 0), (100, 100), 5)
+pygame.draw.line(display, green, (100, 100), (200, 300), 1)
+pygame.draw.circle(display, white, (width//2, height//2), 200, 6)
+pygame.draw.circle(display, yellow, (width//2, height//2), 195, 0)
+pygame.draw.rect(display, cyan, (500, 0, 100, 100))
+pygame.draw.rect(display, magenta, (500, 100, 50, 100))
 
 running = True
 
 while running:
     for event in pygame.event.get():
-        print(event)
         if event.type == pygame.QUIT:
             running = False
+    pygame.display.update()
 
 pygame.quit()
