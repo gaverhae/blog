1f87ef45303e58213f841111ac14cd95c203205f
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-07-07 14:38:17 +0200

    aodpg: lesson 2.4: blitting text

--- courses/2023/The Art of Doing - Video Game Creation with Python and Pygame/lessons/Section 2: Basic Pygame Tutorial/main.py
+++ courses/2023/The Art of Doing - Video Game Creation with Python and Pygame/lessons/Section 2: Basic Pygame Tutorial/main.py
@@ -1,5 +1,12 @@
 import pygame
 
+pygame.init()
+
+dragon_left = pygame.image.load("assets/dragon_left.png")
+dragon_right = pygame.image.load("assets/dragon_right.png")
+system_font = pygame.font.SysFont('calibri', 64)
+custom_font = pygame.font.Font('assets/AttackGraffiti.ttf', 32)
+
 width = 600
 height = 300
 
@@ -7,6 +14,7 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255, 0, 0)
 green = (0, 255, 0)
+dark_green = (10, 50, 10)
 blue = (0, 0, 255)
 yellow = (255, 255, 0)
 cyan = (0, 255, 255)
@@ -21,9 +29,6 @@ def draw_shapes(disp):
     pygame.draw.rect(disp, cyan, (500, 0, 100, 100))
     pygame.draw.rect(disp, magenta, (500, 100, 50, 100))
 
-dragon_left = pygame.image.load("assets/dragon_left.png")
-dragon_right = pygame.image.load("assets/dragon_right.png")
-
 def draw_dragons(disp):
     dragon_left_rect = dragon_left.get_rect()
     dragon_left_rect.topleft = (0, 0)
@@ -33,10 +38,18 @@ def draw_dragons(disp):
     disp.blit(dragon_right, dragon_right_rect)
     pygame.draw.line(disp, white, (0, 75), (width, 75), 4)
 
-pygame.init()
+def render_text(disp):
+    sys_t = system_font.render("Dragons Rule!", True, green, dark_green)
+    sys_tr = sys_t.get_rect()
+    sys_tr.center = (width // 2, height // 2)
+    cus_t = custom_font.render("Move the dragon soon!", True, green)
+    cus_tr = cus_t.get_rect()
+    cus_tr.center = (width // 2, height // 2 + 100)
+    disp.blit(sys_t, sys_tr)
+    disp.blit(cus_t, cus_tr)
 
 display = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Blitting Images!")
+pygame.display.set_caption("Blitting Text!")
 
 running = True
 
@@ -44,7 +57,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    draw_dragons(display)
+    render_text(display)
     pygame.display.update()
 
 pygame.quit()
