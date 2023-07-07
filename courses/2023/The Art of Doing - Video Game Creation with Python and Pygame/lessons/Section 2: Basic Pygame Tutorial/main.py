dbd3e5523070a9b2980284c5a96fb36a36687180
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-07-07 14:52:04 +0200

    aodpg: refactor

--- courses/2023/The Art of Doing - Video Game Creation with Python and Pygame/lessons/Section 2: Basic Pygame Tutorial/main.py
+++ courses/2023/The Art of Doing - Video Game Creation with Python and Pygame/lessons/Section 2: Basic Pygame Tutorial/main.py
@@ -1,12 +1,5 @@
 import pygame
 
-pygame.init()
-
-dragon_left = pygame.image.load("assets/dragon_left.png")
-dragon_right = pygame.image.load("assets/dragon_right.png")
-system_font = pygame.font.SysFont('calibri', 64)
-custom_font = pygame.font.Font('assets/AttackGraffiti.ttf', 32)
-
 width = 600
 height = 300
 
@@ -20,7 +13,13 @@ yellow = (255, 255, 0)
 cyan = (0, 255, 255)
 magenta = (255, 0, 255)
 
-def draw_shapes(disp):
+def load_assets():
+    return {"dragon": {"left": pygame.image.load("assets/dragon_left.png"),
+                       "right": pygame.image.load("assets/dragon_right.png")},
+            "fonts": {"calibri": pygame.font.SysFont('calibri', 64),
+                      "attack": pygame.font.Font('assets/AttackGraffiti.ttf', 32)}}
+
+def draw_shapes(disp, assets):
     disp.fill(blue)
     pygame.draw.line(disp, red, (0, 0), (100, 100), 5)
     pygame.draw.line(disp, green, (100, 100), (200, 300), 1)
@@ -29,35 +28,47 @@ def draw_shapes(disp):
     pygame.draw.rect(disp, cyan, (500, 0, 100, 100))
     pygame.draw.rect(disp, magenta, (500, 100, 50, 100))
 
-def draw_dragons(disp):
-    dragon_left_rect = dragon_left.get_rect()
+def draw_dragons(disp, assets):
+    disp.fill(black)
+    dragon_left_rect = assets["dragon"]["left"].get_rect()
     dragon_left_rect.topleft = (0, 0)
-    dragon_right_rect = dragon_right.get_rect()
+    dragon_right_rect = assets["dragon"]["right"].get_rect()
     dragon_right_rect.topright = (width, 0)
-    disp.blit(dragon_left, dragon_left_rect)
-    disp.blit(dragon_right, dragon_right_rect)
+    disp.blit(assets["dragon"]["left"], dragon_left_rect)
+    disp.blit(assets["dragon"]["right"], dragon_right_rect)
     pygame.draw.line(disp, white, (0, 75), (width, 75), 4)
 
-def render_text(disp):
-    sys_t = system_font.render("Dragons Rule!", True, green, dark_green)
+def render_text(disp, assets):
+    disp.fill(black)
+    sys_t = assets["fonts"]["calibri"].render("Dragons Rule!", True, green, dark_green)
     sys_tr = sys_t.get_rect()
     sys_tr.center = (width // 2, height // 2)
-    cus_t = custom_font.render("Move the dragon soon!", True, green)
+    cus_t = assets["fonts"]["attack"].render("Move the dragon soon!", True, green)
     cus_tr = cus_t.get_rect()
     cus_tr.center = (width // 2, height // 2 + 100)
     disp.blit(sys_t, sys_tr)
     disp.blit(cus_t, cus_tr)
 
-display = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Blitting Text!")
+def main():
+    pygame.init()
+    assets = load_assets()
 
-running = True
+    display = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Blitting Text!")
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    render_text(display)
-    pygame.display.update()
+    states = [draw_shapes, draw_dragons, render_text]
+    state_pos = 0
 
-pygame.quit()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if pygame.event.event_name(event.type) == "MouseButtonUp":
+                state_pos = (state_pos + 1) % len(states)
+        states[state_pos](display, assets)
+        pygame.display.update()
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
