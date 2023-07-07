30e144fe376ae62f91459e79447986b08682c16a
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-07-07 14:20:56 +0200

    aodpg: lesson 2.3: blitting images

--- courses/2023/The Art of Doing - Video Game Creation with Python and Pygame/lessons/Section 2: Basic Pygame Tutorial/main.py
+++ courses/2023/The Art of Doing - Video Game Creation with Python and Pygame/lessons/Section 2: Basic Pygame Tutorial/main.py
@@ -1,12 +1,7 @@
 import pygame
 
-pygame.init()
-
 width = 600
-height = 600
-
-display = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Drawing Objects")
+height = 300
 
 black = (0, 0, 0)
 white = (255, 255, 255)
@@ -17,13 +12,31 @@ yellow = (255, 255, 0)
 cyan = (0, 255, 255)
 magenta = (255, 0, 255)
 
-display.fill(blue)
-pygame.draw.line(display, red, (0, 0), (100, 100), 5)
-pygame.draw.line(display, green, (100, 100), (200, 300), 1)
-pygame.draw.circle(display, white, (width//2, height//2), 200, 6)
-pygame.draw.circle(display, yellow, (width//2, height//2), 195, 0)
-pygame.draw.rect(display, cyan, (500, 0, 100, 100))
-pygame.draw.rect(display, magenta, (500, 100, 50, 100))
+def draw_shapes(disp):
+    disp.fill(blue)
+    pygame.draw.line(disp, red, (0, 0), (100, 100), 5)
+    pygame.draw.line(disp, green, (100, 100), (200, 300), 1)
+    pygame.draw.circle(disp, white, (width//2, height//2), 200, 6)
+    pygame.draw.circle(disp, yellow, (width//2, height//2), 195, 0)
+    pygame.draw.rect(disp, cyan, (500, 0, 100, 100))
+    pygame.draw.rect(disp, magenta, (500, 100, 50, 100))
+
+dragon_left = pygame.image.load("assets/dragon_left.png")
+dragon_right = pygame.image.load("assets/dragon_right.png")
+
+def draw_dragons(disp):
+    dragon_left_rect = dragon_left.get_rect()
+    dragon_left_rect.topleft = (0, 0)
+    dragon_right_rect = dragon_right.get_rect()
+    dragon_right_rect.topright = (width, 0)
+    disp.blit(dragon_left, dragon_left_rect)
+    disp.blit(dragon_right, dragon_right_rect)
+    pygame.draw.line(disp, white, (0, 75), (width, 75), 4)
+
+pygame.init()
+
+display = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Blitting Images!")
 
 running = True
 
@@ -31,6 +44,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    draw_dragons(display)
     pygame.display.update()
 
 pygame.quit()
