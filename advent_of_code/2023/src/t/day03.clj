d298824215f67392638d3c513fe4df47e17b1335
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-03 11:10:38 +0100

    aoc/23: day03 part1

--- advent_of_code/2023/src/t/day03.clj
+++ advent_of_code/2023/src/t/day03.clj
@@ -3,49 +3,48 @@
             [clojure.string :as s]
             [t.lib :as lib :refer [->long]]))
 
-(defn all-indices
-  [s pat]
-  (loop [ps []
-         idx 0]
-    (if-let [i (s/index-of s pat idx)]
-      (recur (conj ps i) (long (inc i)))
-      ps)))
-
 (defn parse
   [lines]
-  {:numbers (->> lines
-                 (map-indexed (fn [y line]
-                                (let [nums (re-seq #"[0-9]+" line)]
-                                  (->> nums
-                                       (map (fn [n]
-                                              (->> (all-indices line n)
-                                                   (map (fn [x]
-                                                          [n [y x]])))))
-                                       (apply concat)))))
-                 (apply concat))
-   :symbols (->> lines
-                 (map-indexed (fn [y line]
-                                (let [syms (re-seq #"[^0-9.]" line)]
-                                  (->> syms
-                                       (map (fn [s]
-                                              (->> (all-indices line s)
-                                                   (map (fn [x]
-                                                          [s [y x]])))))
-                                       (apply concat)))))
-                 (apply concat))})
+  (->> lines
+       (map-indexed
+         (fn [y line]
+           (loop [p 0
+                  numbers []
+                  symbols []]
+             (let [c ^char (get line p)]
+               (cond (== p (count line))
+                     [numbers symbols]
+                     (= \. c)
+                     (recur (inc p) numbers symbols)
+                     (Character/isDigit c)
+                     (let [n (apply str (take-while #(Character/isDigit ^char %) (subs line p)))]
+                       (recur (+ p (count n))
+                              (conj numbers [n [y p]])
+                              symbols))
+                     :else
+                     (recur (inc p)
+                            numbers
+                            (conj symbols [c [y p]])))))))
+       (reduce (fn [acc [numbers symbols]]
+                 (-> acc
+                     (update :numbers (fnil concat []) numbers)
+                     (update :symbols (fnil concat []) symbols)))
+               {})))
 
 (defn part1
   [{:keys [symbols numbers]}]
   (let [adj (->> symbols
-                 (mapcat (fn [[_ [y x]]]
-                           [[(dec y) (dec x)] [(dec y) x] [(dec y) (inc x)]
-                            [     y  (dec x)] [     y  x] [     y  (inc x)]
-                            [(inc y) (dec x)] [(inc y) x] [(inc y) (inc x)]]))
-                 set)]
+                 (map (fn [[s [y x]]]
+                        [[s y x] (set [[(dec y) (dec x)] [(dec y) x] [(dec y) (inc x)]
+                                       [     y  (dec x)] [     y  x] [     y  (inc x)]
+                                       [(inc y) (dec x)] [(inc y) x] [(inc y) (inc x)]])])))]
     (->> numbers
          (filter (fn [[n [y x0]]]
                    (let [ps (set (for [x (range x0 (+ x0 (count n)))] [y x]))]
-                     (seq (set/intersection ps adj)))))
+                     (->> adj
+                          (filter (fn [[s as]] (seq (set/intersection as ps))))
+                          (map first)
+                          seq))))
          (map (comp ->long first))
          (reduce + 0))))
 
@@ -55,6 +54,6 @@
 
 (lib/check
   [part1 sample] 4361
-  #_#_[part1 puzzle] 0
+  [part1 puzzle] 559667
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
