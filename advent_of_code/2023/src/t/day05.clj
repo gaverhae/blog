3d9f68ff9b6389da5e34d6bed4d8ad1d5d04c5ab
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-05 09:37:27 +0100

    aoc/23: day05 prep

---
+++ advent_of_code/2023/src/t/day05.clj
@@ -0,0 +1,23 @@
+(ns t.day05
+  (:require [clojure.math :as math]
+            [clojure.set :as set]
+            [clojure.string :as s]
+            [t.lib :as lib :refer [->long]]))
+
+(defn parse
+  [lines]
+  lines)
+
+(defn part1
+  [input]
+  input)
+
+(defn part2
+  [input]
+  input)
+
+(lib/check
+  #_#_[part1 sample] 0
+  #_#_[part1 puzzle] 0
+  #_#_[part2 sample] 0
+  #_#_[part2 puzzle] 0)
