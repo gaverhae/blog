18201d44a4f6a4725145e0dfca12b340bd4ca181
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-05 12:02:04 +0100

    aoc/23: day05 switch to intervals, sample but not puzzle

--- advent_of_code/2023/src/t/day05.clj
+++ advent_of_code/2023/src/t/day05.clj
@@ -21,42 +21,69 @@
   (let [[[_ [_ & seeds]] & maps] (parser (apply str (interpose "\n" lines)))]
     {:seeds (->> seeds (map parse-long))
      :maps (->> maps
-                (map-indexed (fn [step [_ & nums]]
+                (map (fn [[_ & nums]]
                        (->> nums
                             (map (fn [[_ & nums]]
                                    (map parse-long nums)))
                             (map (fn [[dest src rng]]
-                                   (fn [in]
-                                     (when (<= src in (dec (+ src rng)))
-                                       (+ dest (- in src))))))
-                            ((fn [fns]
-                               (fn [in]
-                                 (or (first (keep (fn [f] (f in)) fns))
-                                     in))))))))}))
+                                   [src (dec (+ src rng)) (- dest src)]))
+                            ((fn [ranges]
+                               (let [ranges (sort ranges)]
+                                 (fn [ins]
+                                   (prn [:ins ins])
+                                   (loop [to-process ins
+                                          to-match ranges
+                                          done []]
+                                     (if (or (empty? to-process)
+                                             (empty? to-match))
+                                       (sort (set (concat to-process done)))
+                                       (let [[p0 p1] (first to-process)
+                                             [m0 m1 offset] (first to-match)]
+                                         (cond (< p1 m0) (recur (rest to-process)
+                                                                to-match
+                                                                (cons [p0 p1] done))
+                                               (< m1 p0) (recur to-process
+                                                                (rest to-match)
+                                                                done)
+                                               (and (<= m0 p0) (< m1 p1)) (recur (cons [(inc m1) p1] to-process)
+                                                                                 (rest to-match)
+                                                                                 (cons [(+ offset p0) (+ offset m1)] done))
+                                               (and (<= m0 p0) (<= p1 m1)) (recur (rest to-process)
+                                                                                  to-match
+                                                                                  (cons [(+ offset p0) (+ offset p1)] done))
+                                               (and (< p0 m0) (< m1 p1)) (recur (cons [(inc m1) p1] to-process)
+                                                                                (rest to-match)
+                                                                                (concat [[p0 (dec m0)] [(+ offset m0) (+ offset m1)]]
+                                                                                        done))
+                                               (and (< p0 m0) (<= p1 m1)) (recur (rest to-process)
+                                                                                 to-match
+                                                                                 (concat [[p0 (dec m0)] [(+ offset m0) (+ offset p1)]]
+                                                                                         done))))))))))))))}))
 
 (defn part1
   [{:keys [seeds maps]}]
   (->> seeds
-       (map (fn [seed]
-              (reduce (fn [acc el] (el acc))
-                      seed
-                      maps)))
+       (mapcat (fn [seed]
+                 (reduce (fn [acc el] (el acc))
+                         [[seed seed]]
+                         maps)))
+       (map first)
        (reduce min)))
+
 
 
 (defn part2
   [{:keys [seeds maps]}]
   (->> seeds
        (partition 2)
-       (mapcat (fn [[a b]] (range a (+ a b))))
-       (map (fn [seed]
-              (reduce (fn [acc el] (el acc))
-                      seed
-                      maps)))
-       (reduce min)))
+       (map (fn [[a b]] [a (+ a b)]))
+       ((fn [seed-ranges]
+          (reduce (fn [acc el] (el acc)) seed-ranges maps)))
+       #_(map first)
+       #_(reduce min)))
 
 (lib/check
-  [part1 sample] 35
+  #_#_[part1 sample] 35
   #_#_[part1 puzzle] 240320250
-  [part2 sample] 46
-  #_#_[part2 puzzle] 0)
+  #_#_[part2 sample] 46
+  [part2 puzzle] -10)
