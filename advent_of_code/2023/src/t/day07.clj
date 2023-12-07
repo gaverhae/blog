8879d781739b18baaca464267b21bdc257ee9c17
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-07 07:17:47 +0100

    aoc/23: day07 part2

--- advent_of_code/2023/src/t/day07.clj
+++ advent_of_code/2023/src/t/day07.clj
@@ -5,47 +5,80 @@
             [instaparse.core :as insta]
             [t.lib :as lib]))
 
-(def values
-  (->> "AKQJT98765432"
-       (map-indexed (fn [i c] [c i]))
-       (into {})))
-
-(defn score
-  [hand]
-  (let [f (frequencies hand)]
-    (case (sort (vals f))
-      [5] 0
-      [1 4] 1
-      [2 3] 2
-      [1 1 3] 3
-      [1 2 2] 4
-      [1 1 1 2] 5
-      [1 1 1 1 1] 6)))
-
 (defn parse
   [lines]
   (->> lines
        (map (fn [line] (s/split line #" ")))
-       (map (fn [[a b]] [(->> a (map values) score)
-                         (->> a (map values) vec)
-                         (parse-long b)]))))
+       (map (fn [[a b]] [a (parse-long b)]))))
+
+
+
 
 (defn part1
   [input]
-  (->> input
-       sort
-       reverse
-       (map-indexed (fn [idx [_ _ bid]]
-                      (* (inc idx) bid)))
-       (reduce + 0)))
+  (let [values (->> "AKQJT98765432"
+                    (map-indexed (fn [i c] [c i]))
+                    (into {}))
+        score (fn [hand] (case (sort (vals (frequencies hand)))
+                           [5] 0
+                           [1 4] 1
+                           [2 3] 2
+                           [1 1 3] 3
+                           [1 2 2] 4
+                           [1 1 1 2] 5
+                           [1 1 1 1 1] 6))]
+    (->> input
+         (map (fn [[a b]] [(score a)
+                           (->> a (map values) vec)
+                           b]))
+         sort
+         reverse
+         (map-indexed (fn [idx [_ _ bid]]
+                        (* (inc idx) bid)))
+         (reduce + 0))))
 
 
 (defn part2
   [input]
-  input)
+  (let [values (->> "AKQT98765432J"
+                    (map-indexed (fn [i c] [c i]))
+                    (into {}))
+        score (fn [hand]
+                (let [numJ (->> hand (filter #{\J}) count)
+                      others (->> hand (remove #{\J}) frequencies)]
+
+                (case [numJ (sort (vals others))]
+                  [0 [5]] 0
+                  [0 [1 4]] 1
+                  [0 [2 3]] 2
+                  [0 [1 1 3]] 3
+                  [0 [1 2 2]] 4
+                  [0 [1 1 1 2]] 5
+                  [0 [1 1 1 1 1]] 6
+                  [1 [4]] 0
+                  [1 [1 3]] 1
+                  [1 [2 2]] 2
+                  [1 [1 1 2]] 3
+                  [1 [1 1 1 1]] 5
+                  [2 [3]] 0
+                  [2 [1 2]] 1
+                  [2 [1 1 1]] 3
+                  [3 [2]] 0
+                  [3 [1 1]] 1
+                  [4 [1]] 0
+                  [5 []] 0)))]
+    (->> input
+         (map (fn [[a b]] [(score a)
+                           (->> a (map values) vec)
+                           b]))
+         sort
+         reverse
+         (map-indexed (fn [idx [_ _ bid]]
+                        (* (inc idx) bid)))
+         (reduce + 0))))
 
 (lib/check
   [part1 sample] 6440
   [part1 puzzle] 248569531
-  #_#_[part2 sample] 0
-  #_#_[part2 puzzle] 0)
+  [part2 sample] 5905
+  [part2 puzzle] 250382098)
