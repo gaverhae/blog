61b1f066c624de712f950dc33246428e9bc78def
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-08 06:13:12 +0100

    aoc/23: day08 prep

---
+++ advent_of_code/2023/src/t/day08.clj
@@ -0,0 +1,25 @@
+(ns t.day08
+  (:require [clojure.math :as math]
+            [clojure.set :as set]
+            [clojure.string :as s]
+            [instaparse.core :as insta]
+            [t.lib :as lib]))
+
+(defn parse
+  [lines]
+  lines)
+
+(defn part1
+  [input]
+  input)
+
+
+(defn part2
+  [input]
+  input)
+
+(lib/check
+  [part1 sample] 0
+  #_#_[part1 puzzle] 0
+  #_#_[part2 sample] 0
+  #_#_[part2 puzzle] 0)
