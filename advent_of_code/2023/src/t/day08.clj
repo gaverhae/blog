43e441f334e1bb5ea81f4596e1e546866f47ddb1
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-08 06:22:26 +0100

    aoc/23: day 08 part1

--- advent_of_code/2023/src/t/day08.clj
+++ advent_of_code/2023/src/t/day08.clj
@@ -7,19 +7,31 @@
 
 (defn parse
   [lines]
-  lines)
+  {:directions (->> (first lines)
+                    (map {\L 0, \R 1}))
+   :nodes (->> (drop 2 lines)
+               (map (fn [line]
+                      (let [[_ start left right] (re-find #"([A-Z]+) = \(([A-Z]+), ([A-Z]+)\)" line)]
+                        [start [left right]])))
+               (into {}))})
 
 (defn part1
-  [input]
-  input)
-
+  [{:keys [directions nodes]}]
+  (loop [step 0
+         directions (cycle directions)
+         pos "AAA"]
+    (if (= pos "ZZZ")
+      step
+      (recur (inc step)
+             (rest directions)
+             (get-in nodes [pos (first directions)])))))
 
 (defn part2
   [input]
   input)
 
 (lib/check
-  [part1 sample] 0
-  #_#_[part1 puzzle] 0
+  [part1 sample] 2
+  [part1 puzzle] 15517
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
