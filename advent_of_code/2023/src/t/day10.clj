22bff5489bd1ba8c32d452e41d516011365a81fd
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-10 06:03:20 +0100

    aoc/23: day10 prep

---
+++ advent_of_code/2023/src/t/day10.clj
@@ -0,0 +1,22 @@
+(ns t.day10
+  (:require [clojure.math :as math]
+            [clojure.set :as set]
+            [clojure.string :as s]
+            [instaparse.core :as insta]
+            [t.lib :as lib]))
+
+(defn parse
+  [lines]
+  lines)
+
+(defn part1
+  [input])
+
+(defn part2
+  [input])
+
+(lib/check
+  [part1 sample] 0
+  #_#_[part1 puzzle] 0
+  #_#_[part2 sample] 0
+  #_#_[part2 puzzle] 0)
