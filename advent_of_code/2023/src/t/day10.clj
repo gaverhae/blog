772abac13850b4f3186a949ea87df76296bd24be
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-10 06:42:44 +0100

    aoc/23: day10 part1 sample

--- advent_of_code/2023/src/t/day10.clj
+++ advent_of_code/2023/src/t/day10.clj
@@ -7,16 +7,43 @@
 
 (defn parse
   [lines]
-  lines)
+  (vec lines))
+
+(defn first-connected-neighbour
+  [[y0 x0] grid seen?]
+  (first (for [[y x c1 c2] [[(dec y0) x0 #{\| \L \J \S} #{\| \7 \F \S}]
+                            [y0  (dec x0) #{\- \J \7 \S} #{\- \L \F \S}]
+                            [y0  (inc x0) #{\- \F \L \S} #{\- \J \7 \S}]
+                            [(inc y0) x0 #{\| \7 \F \S} #{\| \L \J \S}]]
+               :let [pipe (get-in grid [y x])]
+               :when (and pipe
+                          (c1 (get-in grid [y0 x0]))
+                          (c2 pipe)
+                          (not (seen? [y x])))]
+           [y x])))
 
 (defn part1
-  [input])
+  [input]
+  (let [start (->> input
+                   (keep-indexed (fn [y line]
+                                   (->> line (keep-indexed (fn [x c] (when (= \S c) [y x]))))))
+                   (apply concat)
+                   first)]
+    (loop [step 0
+           so-far #{}
+           pos start]
+      (if (and (pos? step) (= start pos))
+        (quot step 2)
+        (let [pos (first-connected-neighbour pos input so-far)]
+          (recur (inc step)
+                 (conj so-far pos)
+                 pos))))))
 
 (defn part2
   [input])
 
 (lib/check
-  [part1 sample] 0
+  [part1 sample] 8
   #_#_[part1 puzzle] 0
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
