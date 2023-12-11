8c538f22a3af10b22dbd4afadb62a82636a90275
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-11 06:13:26 +0100

    aoc/23: day11 part1 sample

--- advent_of_code/2023/src/t/day11.clj
+++ advent_of_code/2023/src/t/day11.clj
@@ -7,18 +7,41 @@
 
 (defn parse
   [lines]
-  lines)
+  (->> lines
+       (mapcat (fn [line]
+                 (if (every? #{\.} line)
+                   [line line]
+                   [line])))
+       lib/transpose
+       (mapcat (fn [line]
+                 (if (every? #{\.} line)
+                   [line line]
+                   [line])))
+       lib/transpose
+       (keep-indexed
+         (fn [y line]
+           (keep-indexed
+             (fn [x c]
+               (when (= c \#)
+                 [y x]))
+             line)))
+       (apply concat)))
 
 (defn part1
   [input]
-  input)
+  (->> (for [g1 input
+             g2 input
+             :when (and (= 1 (compare g1 g2))
+                        (not= g1 g2))]
+         (lib/manhattan g1 g2))
+       (reduce + 0)))
 
 (defn part2
   [input]
   input)
 
 (lib/check
-  [part1 sample] 0
+  [part1 sample] 374
   #_#_[part1 puzzle] 0
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
