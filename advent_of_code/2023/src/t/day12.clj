f289b490bf0910b54bfd5b20989139994a16f174
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-12 06:42:10 +0100

    aoc/23: day12 part1 refactor

--- advent_of_code/2023/src/t/day12.clj
+++ advent_of_code/2023/src/t/day12.clj
@@ -14,15 +14,15 @@
                  (->> (re-seq #"\d+" bounds)
                       (map parse-long))])))))
 
-(defn part1
-  [input]
+(defn solve
+  [in]
   (let [matches? (fn [pattern]
                    (fn [line]
                      (->> line
                           (re-seq #"#+")
                           (map count)
                           (= pattern))))]
-    (->> input
+    (->> in
          (map (fn [[symbols pattern]]
                 (->> (loop [to-process symbols
                             processed []]
@@ -41,38 +41,21 @@
                      count)))
          (reduce + 0))))
 
+(defn part1
+  [input]
+  (->> input
+       solve))
+
 (defn part2
   [input]
-  (let [matches? (fn [pattern]
-                   (fn [line]
-                     (->> line
-                          (re-seq #"#+")
-                          (map count)
-                          (= pattern))))]
-    (->> input
-         (map (fn [[symbols pattern]]
-                [(apply str (repeat 5 symbols))
-                 (apply concat (repeat 5 pattern))]))
-         (map (fn [[symbols pattern]]
-                (->> (loop [to-process symbols
-                            processed []]
-                       (if (empty? to-process)
-                         processed
-                         (let [s (first to-process)
-                               to-process (rest to-process)]
-                           (recur to-process
-                                  (->> (if (= \? s) [\. \#] [s])
-                                       (mapcat (fn [new-s]
-                                                 (if (empty? processed)
-                                                   [new-s]
-                                                   (->> processed
-                                                        (map (fn [prev] (str prev new-s))))))))))))
-                     (filter (matches? pattern))
-                     count)))
-         (reduce + 0))))
+  (->> input
+       (map (fn [[symbols pattern]]
+              [(apply str (repeat 5 symbols))
+               (apply concat (repeat 5 pattern))]))
+       solve))
 
 (lib/check
   [part1 sample] 21
   [part1 puzzle] 7090
-  [part2 sample] 0
+  #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
