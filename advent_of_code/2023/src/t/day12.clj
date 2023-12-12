51915215f4a13c39978ebbde7bb07d506b74180e
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-12 13:35:15 +0100

    aoc/23: day12 part2 sample fast enough? (~4s)

--- advent_of_code/2023/src/t/day12.clj
+++ advent_of_code/2023/src/t/day12.clj
@@ -14,43 +14,55 @@
                  (->> (re-seq #"\d+" bounds)
                       (map parse-long))])))))
 
+(defn match-line
+  [[symbols pattern]]
+  (let [matches? (fn [line]
+                   (->> line
+                        (re-seq #"#+")
+                        (map count)
+                        (= pattern)))
+        pre-matches? (fn [line]
+                       (let [line-p (->> line
+                                         (re-seq #"#+")
+                                         (map count))]
+                         (and (<= (count line-p) (count pattern))
+                              (= (butlast (take (count line-p) pattern))
+                                 (butlast line-p)))))]
+    (->> (loop [to-process symbols
+                processed []]
+           (if (empty? to-process)
+             processed
+             (let [s (first to-process)
+                   to-process (rest to-process)]
+               (recur to-process
+                      (->> (if (= \? s) [\. \#] [s])
+                           (mapcat (fn [new-s]
+                                     (if (empty? processed)
+                                       [(str new-s)]
+                                       (->> processed
+                                            (map (fn [prev] (str prev new-s)))))))
+                           (filter pre-matches?))))))
+         (filter matches?)
+         count)))
+
 (defn part1
   [input]
-  (let [matches? (fn [pattern]
-                   (fn [line]
-                     (->> line
-                          (re-seq #"#+")
-                          (map count)
-                          (= pattern))))]
-    (->> input
-         (map (fn [[symbols pattern]]
-                (->> (loop [to-process symbols
-                            processed []]
-                       (if (empty? to-process)
-                         processed
-                         (let [s (first to-process)
-                               to-process (rest to-process)]
-                           (recur to-process
-                                  (->> (if (= \? s) [\. \#] [s])
-                                       (mapcat (fn [new-s]
-                                                 (if (empty? processed)
-                                                   [new-s]
-                                                   (->> processed
-                                                        (map (fn [prev] (str prev new-s))))))))))))
-                     (filter (matches? pattern))
-                     count)))
-         (reduce + 0))))
+  (->> input
+       (map match-line)
+       (reduce + 0)))
 
 (defn part2
   [input]
   (->> input
        (map (fn [[symbols pattern]]
-              [(apply str (interpose \? (repeat 5 symbols)))
-               (apply concat (repeat 5 pattern))]))
-       ))
+              (let [a (match-line [symbols pattern])
+                    b (match-line [(str symbols "?" symbols) (concat pattern pattern)])
+                    c (quot b a)]
+                (* a c c c c))))
+       (reduce + 0)))
 
 (lib/check
   [part1 sample] 21
   [part1 puzzle] 7090
-  #_#_[part2 sample] 525152
+  [part2 sample] 525152
   #_#_[part2 puzzle] 0)
