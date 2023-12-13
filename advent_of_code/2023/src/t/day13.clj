1676e6a4cc52f8dc331132e6661672a72ab51cd7
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-13 06:26:59 +0100

    aoc/23/13: part1 sample

--- advent_of_code/2023/src/t/day13.clj
+++ advent_of_code/2023/src/t/day13.clj
@@ -9,18 +9,36 @@
 
 (defn parse
   [lines]
-  lines)
+  (->> lines
+       (partition-by #{""})
+       (remove #{[""]})))
+
+(defn find-pal
+  [pat]
+  (first (for [pos (range 1 (count pat))
+               :let [[up down] [(take pos pat) (drop pos pat)]
+                     size (min (count up) (count down))
+                     [up down] (if (> (count up) (count down))
+                                 [(drop (- pos size) up) down]
+                                 [up (take size down)])]
+               :when (= up (reverse down))]
+           pos)))
 
 (defn part1
   [input]
-  input)
+  (->> input
+       (map (fn [pat]
+              (if-let [p (find-pal pat)]
+                (* 100 p)
+                (find-pal (lib/transpose pat)))))
+       (reduce + 0)))
 
 (defn part2
   [input]
   input)
 
 (lib/check
-  [part1 sample] 0
+  [part1 sample] 405
   #_#_[part1 puzzle] 0
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
