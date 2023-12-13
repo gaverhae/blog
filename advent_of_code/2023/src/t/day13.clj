3df7b5176382a801848bd5ccaaa59ff38d949b55
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-13 06:35:25 +0100

    aoc/23/13: part2

--- advent_of_code/2023/src/t/day13.clj
+++ advent_of_code/2023/src/t/day13.clj
@@ -33,12 +33,34 @@
                 (find-pal (lib/transpose pat)))))
        (reduce + 0)))
 
+(defn find-pal-smudge
+  [pat]
+  (first (for [pos (range 1 (count pat))
+               :let [[up down] [(take pos pat) (drop pos pat)]
+                     size (min (count up) (count down))
+                     [up down] (if (> (count up) (count down))
+                                 [(drop (- pos size) up) down]
+                                 [up (take size down)])]
+               :when (->> (map vector up (reverse down))
+                          (map (fn [[line-up line-bottom]]
+                                 (->> (map vector line-up line-bottom)
+                                      (keep (fn [[a b]] (when (not= a b) 1)))
+                                      (reduce + 0))))
+                          (reduce + 0)
+                          (= 1))]
+           pos)))
+
 (defn part2
   [input]
-  input)
+  (->> input
+       (map (fn [pat]
+              (if-let [p (find-pal-smudge pat)]
+                (* 100 p)
+                (find-pal-smudge (lib/transpose pat)))))
+       (reduce + 0)))
 
 (lib/check
   [part1 sample] 405
-  [part1 puzzle] 0
-  #_#_[part2 sample] 0
-  #_#_[part2 puzzle] 0)
+  [part1 puzzle] 31956
+  [part2 sample] 400
+  [part2 puzzle] 37617)
