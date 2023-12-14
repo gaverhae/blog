151e5437206d18257f9b80cd20b7803d6cea014d
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-14 23:40:43 +0100

    aoc/23/12: establish a baseline for optimization attempts

--- advent_of_code/2023/src/t/day12.clj
+++ advent_of_code/2023/src/t/day12.clj
@@ -132,19 +132,19 @@
   #_#_[part1 sample] 21
   #_#_[part1 puzzle] 7090
   #_#_[part2 sample false] 525152
-  #_#_[part2 puzzle true] 0)
+  [part2 puzzle true] 0)
 
 (defn benchmark
   []
   (->> @puzzle
-       (take 3)
+       (take 10)
        (map (fn [[s p]] (solve-line (str s \? s \? s \? s) (concat p p p p))))
        doall)
   nil)
 
 (comment
 
+;; 500d98820b7f5 50058
   (lib/timed (benchmark))
-36669
 
   )
