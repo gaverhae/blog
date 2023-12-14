5b8fe9ef1e8ce5b1035c77590291cd7d4512101c
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-14 06:22:49 +0100

    aoc/23/14: part1 sample

--- advent_of_code/2023/src/t/day14.clj
+++ advent_of_code/2023/src/t/day14.clj
@@ -13,14 +13,36 @@
 
 (defn part1
   [input]
-  input)
+  (->> (cons (repeat (count (first input)) \#)
+             input)
+       lib/transpose
+       (map (fn [col]
+              (->> (apply str col)
+                   (re-seq #"#+[.O]*")
+                   (mapcat (fn [s]
+                             (let [f (frequencies s)]
+                               (concat (repeat (f \#) \#)
+                                       (repeat (f \O 0) \O)
+                                       (repeat (f \. 0) \,)))))
+                   reverse
+                   (apply str))))
+       (map (fn [line]
+              (loop [idx 0
+                     ld 0]
+                (if (== idx (count line))
+                  ld
+                  (recur (inc idx)
+                         (+ (if (= \O (get line idx)) (inc idx) 0)
+                            ld))))))
+       (reduce + 0)))
+
 
 (defn part2
   [input]
   input)
 
 (lib/check
-  #_#_[part1 sample] 0
+  [part1 sample] 136
   #_#_[part1 puzzle] 0
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
