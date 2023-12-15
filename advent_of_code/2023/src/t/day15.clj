0eef2e3758ade66e713b296999fc440ff4c427dc
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-15 06:38:48 +0100

    aoc/23/15: part2 sample but not puzzle

--- advent_of_code/2023/src/t/day15.clj
+++ advent_of_code/2023/src/t/day15.clj
@@ -13,29 +13,62 @@
       first
       (s/split #",")))
 
+(defn h
+  [word]
+  (->> word
+       (map int)
+       (reduce (fn [acc el]
+                 (-> acc
+                     (+ el)
+                     (* 17)
+                     (rem 256)))
+               0)))
+
 (defn part1
   [input]
   (->> input
-       (map (fn [word]
-              (->> word
-                   (map int)
-                   (reduce (fn [acc el]
-                             (-> acc
-                                 (+ el)
-                                 (* 17)
-                                 (rem 256)))
-                           0))))
+       (map h)
        (reduce + 0)))
-
-
 
 (defn part2
   [input]
-  input)
+  (->> input
+       (map (fn [word]
+              (let [label (take 2 word)
+                    box (h label)
+                    op (get word 2)
+                    arg (drop 3 word)]
+                (if (= op \=)
+                  [:= box (apply str label) (parse-long (apply str arg))]
+                  [:- box (apply str label)]))))
+       (reduce (fn [acc op]
+                 (case (first op)
+                   := (let [[_ box label n] op]
+                        (update acc box (fn [lenses]
+                                          (if (some #(= label (first %)) lenses)
+                                            (mapv (fn [lens]
+                                                    (if (= label (first lens))
+                                                      [label n]
+                                                      lens))
+                                                  lenses)
+                                            (conj (or lenses []) [label n])))))
+                   :- (let [[_ box label] op]
+                        (update acc box (fn [lenses]
+                                          (->> lenses
+                                               (remove #(= label (first %)))
+                                               vec))))))
+               {})
+       (mapcat (fn [[box lenses]]
+                 (->> lenses
+                      (map-indexed (fn [slot [label focus]]
+                                     (* (inc box)
+                                        (inc slot)
+                                        focus))))))
+       (reduce + 0)))
 
 (lib/check
   [part1 sample1] 52
   [part1 sample] 1320
   [part1 puzzle] 501680
-  #_#_[part2 sample] 0
-  #_#_[part2 puzzle] 0)
+  [part2 sample] 145
+  [part2 puzzle] 0)
