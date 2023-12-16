5506f434f766f87112e27c1e4b2b2c724166d03b
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-16 06:59:29 +0100

    aoc/23/16: part1 sample but not puzzle

--- advent_of_code/2023/src/t/day16.clj
+++ advent_of_code/2023/src/t/day16.clj
@@ -9,18 +9,55 @@
 
 (defn parse
   [lines]
-  lines)
+  (vec lines))
+
+(defn step
+  [dy dx c]
+  (let [left [0 -1], right [0 1], up [-1 0], down [1 0]
+        m {[left \.] [left]
+           [left \\] [up]
+           [left \/] [down]
+           [left \|] [up down]
+           [left \-] [left]
+           [right \.] [right]
+           [right \\] [down]
+           [right \/] [up]
+           [right \|] [up down]
+           [right \-] [right]
+           [up \.] [up]
+           [up \\] [left]
+           [up \/] [right]
+           [up \|] [up]
+           [up \-] [left right]
+           [down \.] [down]
+           [down \\] [right]
+           [down \/] [left]
+           [down \|] [down]
+           [down \-] [left right]}]
+    (get m [[dy dx] c])))
 
 (defn part1
   [input]
-  input)
+  (loop [ps [[[0 0] [0 1]]]
+         energized #{[0 0]}
+         seen? #{}]
+    (cond (empty? ps) (count energized)
+          (seen? (first ps)) (recur (rest ps) energized seen?)
+          :else (let [p (first ps), ps (rest ps)
+                      seen? (conj seen? p)
+                      [[y x] [dy dx]] p
+                      nxt [(+ y dy) (+ x dx)]]
+                  (if-let [c (get-in input nxt)]
+                    (recur (reduce (fn [acc el] (conj acc [nxt el])) ps (step dy dx c))
+                           (conj energized nxt) seen?)
+                    (recur ps energized seen?))))))
 
 (defn part2
   [input]
   (->> input))
 
 (lib/check
-  #_#_[part1 sample] 0
+  [part1 sample] 46
   #_#_[part1 puzzle] 0
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
