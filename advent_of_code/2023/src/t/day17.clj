2524352154c6a97fac0449c3c99bbe8ea6d4f9a7
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-17 07:40:32 +0100

    aoc/23/17: part1 sample

--- advent_of_code/2023/src/t/day17.clj
+++ advent_of_code/2023/src/t/day17.clj
@@ -9,11 +9,44 @@
 
 (defn parse
   [lines]
-  lines)
+  (->> lines
+       (mapv (fn [line]
+               (->> line
+                    (mapv (comp parse-long str)))))))
 
 (defn part1
   [input]
-  input)
+  (let [end [(dec (count input)) (dec (count (first input)))]
+        left [0 -1] right [0 1] up [-1 0] down [1 0]
+        h (fn [p] (lib/manhattan end p))]
+    (loop [to-visit [[(h [0 0]) 0 [0 0] ()]]
+           visited? #{}]
+      (if (empty? to-visit)
+        :error
+        (let [[g c p hist] (first to-visit)]
+          (if (= end p)
+            c
+            (recur (->> (rest to-visit)
+                        (concat (->> [left right up down]
+                                     (remove (fn [[dy dx]]
+                                               (when-let [[last-dy last-dx] (first hist)]
+                                                 (= [dy dx] [(- last-dy) (- last-dx)]))))
+                                     (remove (fn [[dy dx]]
+                                               (= (repeat 3 [dy dx])
+                                                  (take 3 hist))))
+                                     (keep (fn [[dy dx]]
+                                             (let [[y x] p
+                                                   y (+ y dy)
+                                                   x (+ x dx)]
+                                               (when-let [loss (get-in input [y x])]
+                                                 [(+ loss c (h [y x]))
+                                                  (+ loss c)
+                                                  [y x]
+                                                  (cons [dy dx] hist)]))))))
+                        (remove (fn [[g c p hist]]
+                                  (visited? [p (take 3 hist)])))
+                        (sort-by first))
+                   (conj visited? [p (take 3 hist)]))))))))
 
 (defn part2
   [input]
