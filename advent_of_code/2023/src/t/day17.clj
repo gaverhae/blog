6337735134a3a1d5ed414f4ddc2b476a7c657010
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-17 09:14:09 +0100

    aoc/23/17: much faster part1 on sample

--- advent_of_code/2023/src/t/day17.clj
+++ advent_of_code/2023/src/t/day17.clj
@@ -19,34 +19,28 @@
   (let [end [(dec (count input)) (dec (count (first input)))]
         left [0 -1] right [0 1] up [-1 0] down [1 0]
         h (fn [p] (lib/manhattan end p))]
-    (loop [to-visit [[(h [0 0]) 0 [0 0] ()]]
+    (loop [to-visit [[(h [0 0]) 0 [0 0] 0]
+                     [(h [0 0]) 0 [0 0] 1]]
            visited? #{}]
       (if (empty? to-visit)
         :error
-        (let [[g c p hist] (first to-visit)]
+        (let [[g c p dir] (first to-visit)]
           (if (= end p)
             c
-            (recur (->> (rest to-visit)
-                        (concat (->> [left right up down]
-                                     (remove (fn [[dy dx]]
-                                               (when-let [[last-dy last-dx] (first hist)]
-                                                 (= [dy dx] [(- last-dy) (- last-dx)]))))
-                                     (remove (fn [[dy dx]]
-                                               (= (repeat 3 [dy dx])
-                                                  (take 3 hist))))
-                                     (keep (fn [[dy dx]]
-                                             (let [[y x] p
-                                                   y (+ y dy)
-                                                   x (+ x dx)]
-                                               (when-let [loss (get-in input [y x])]
-                                                 [(+ loss c (h [y x]))
-                                                  (+ loss c)
-                                                  [y x]
-                                                  (cons [dy dx] hist)]))))))
-                        (remove (fn [[g c p hist]]
-                                  (visited? [p (take 3 hist)])))
+            (recur (->> (for [dist (range 1 4)
+                              sign [+ -]
+                              :let [new-p (update p dir sign dist)]
+                              :when (get-in input new-p)]
+                          [new-p
+                           (reduce (fn [acc el]
+                                     (+ acc (get-in input (update p dir sign (inc el)))))
+                                   c
+                                   (range dist))])
+                        (map (fn [[p c]] [(+ c (h p)) c p (- 1 dir)]))
+                        (concat to-visit)
+                        (remove (fn [[g c p dir]] (visited? [p dir])))
                         (sort-by first))
-                   (conj visited? [p (take 3 hist)]))))))))
+                   (conj visited? [p dir]))))))))
 
 (defn part2
   [input]
