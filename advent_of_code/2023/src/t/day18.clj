061f03429cb3ba10925374243f2773bd146084d6
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-18 15:56:33 +0100

    aoc/23/18: print both to try and see the issue

--- advent_of_code/2023/src/t/day18.clj
+++ advent_of_code/2023/src/t/day18.clj
@@ -44,6 +44,73 @@
                             (mapcat (fn [[_ y0 y1 _]] [y0 (inc y0) y1 (inc y1)]))
                             set
                             sort)]
+    (let [trench (loop [to-dig (->> lines
+                                  (mapcat (fn [[dir dist]] (repeat dist dir))))
+                      [y x] [0 0]
+                      [py px] [0 1]
+                      trench {[0 0] \F}]
+                 (if (empty? to-dig)
+                   (assoc trench [y x] \F)
+                   (let [[[dy dx] & to-dig] to-dig]
+                     (recur to-dig
+                            [(+ y dy) (+ x dx)]
+                            [y x]
+                            (assoc trench [y x] (match [(- y py) (- x px) dy dx]
+                                                [ 0  1  1  0] \7
+                                                [-1  0  0 -1] \7
+                                                [-1  0  0  1] \F
+                                                [ 0 -1  1  0] \F
+                                                [ _  0  _  0] \|
+                                                [ 0  _  0  _] \-
+                                                [ 1  0  0  1] \L
+                                                [ 0 -1 -1  0] \L
+                                                [ 0  1 -1  0] \J
+                                                [ 1  0  0 -1] \J))))))
+          min-y (->> trench keys (map first) (reduce min))
+          max-y (->> trench keys (map first) (reduce max) inc)
+          min-x (->> trench keys (map second) (reduce min))
+          max-x (->> trench keys (map second) (reduce max) inc)
+          dug (->> interesting-ys
+                   (map (fn [y]
+                          [y (->> verticals
+                                  (filter (fn [[_ y0 y1 _]] (<= y0 y y1)))
+                                  (map (fn [[x _ _ in?]] [x in?])))]))
+                   (map (fn [[y xs]]
+                          [y (reduce (fn [acc el]
+                                       (let [done (vec (butlast acc))
+                                             [x0 in0] (last acc)
+                                             [x1 in1] el]
+                                         (cond (= in0 in1 true) (conj done [x0 true])
+                                               (= in0 in1 false) (conj done [x1 false])
+                                               :else (conj done [x0 in0] [x1 in1]))))
+                                     [(first xs)]
+                                     (rest xs))]))
+                   (map (fn [[y xs]]
+                          [y (->> xs (map first) (partition 2))]))
+                   (partition 2 1)
+                   (mapcat (fn [[[y0 xs] [y1 _]]]
+                             (let [height (- y1 y0)]
+                               (->> (range height)
+                                    (mapcat (fn [dy]
+                                              (->> xs
+                                                   (mapcat (fn [[x0 x1]]
+                                                             (->> (range (inc (- x1 x0)))
+                                                                  (map (fn [dx] [(+ y0 dy) (+ x0 dx)]))))))))))))
+                   (into #{}))
+          draw-trench (->> (range min-y max-y)
+                       (map (fn [y]
+                              (->> (range min-x max-x)
+                                   (map (fn [x] (trench [y x] \space)))
+                                   (apply str)))))
+          draw-dug (->> (range min-y max-y)
+                        (map (fn [y]
+                               (->> (range min-x max-x)
+                                    (map (fn [x] (if (dug [y x]) \# \space)))
+                                    (apply str)))))]
+      (println)
+      (->> draw-trench (map println) doall)
+      (->> draw-dug (map println) doall)
+      (println))
     (->> interesting-ys
          (map (fn [y]
                 [y (->> verticals
@@ -79,6 +146,6 @@
 
 (lib/check
   [part1 sample] 62
-  #_#_[part1 puzzle] 48503
+  [part1 puzzle] 48503
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
