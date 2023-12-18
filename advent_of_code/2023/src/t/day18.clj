e5021b368490b517a5a90ea1563514e81b4cc3e6
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-18 13:32:24 +0100

    aoc/23/18: change approach

--- advent_of_code/2023/src/t/day18.clj
+++ advent_of_code/2023/src/t/day18.clj
@@ -10,68 +10,32 @@
 
 (defn parse
   [lines]
-  (->> lines
-       (map (fn [line]
-              (let [[dir dist color] (s/split line #" ")
-                    dir (get {"R" [0 1], "D" [1 0], "L" [0 -1], "U" [-1 0]} dir)
-                    dist (parse-long dist)]
-                [dir dist color])))))
+  (let [lines (->> lines
+                   (map (fn [line]
+                          (let [[dir dist color] (s/split line #" ")
+                                dir (get {"R" [0 1], "D" [1 0], "L" [0 -1], "U" [-1 0]} dir)
+                                dist (parse-long dist)
+                                color (re-find #"[0-9a-f]{6}" color)
+                                dir2 (get {\0 [0 1], \1 [1 0], \2 [0 -1], \3 [-1 0]}
+                                          (get color 5))
+                                dist2 (Long/parseLong (subs color 0 5) 16)]
+                            [dir dist [dir2 dist2]]))))]
+    {:part1 (->> lines (map (fn [line] (->> line (map (fn [[dir dist _]] [dir dist]))))))
+     :part2 (->> lines (map (fn [line] (->> line (map (fn [[_ _ [dir dist]]] [dir dist]))))))}))
+
+(defn solve
+  [lines])
 
 (defn part1
   [input]
-  (let [left [0 -1], right [0 1], up [-1 0], down [1 0]
-        dug? (loop [to-dig (->> input
-                                (mapcat (fn [[dir dist _]] (repeat dist dir))))
-                    [y x] [0 0]
-                    [py px] [0 1]
-                    dug? {[0 0] \F}]
-               (if (empty? to-dig)
-                 (assoc dug? [y x] \F)
-                 (let [[[dy dx] & to-dig] to-dig]
-                   (recur to-dig
-                          [(+ y dy) (+ x dx)]
-                          [y x]
-                          (assoc dug? [y x] (match [(- y py) (- x px) dy dx]
-                                              [ 0  1  1  0] \7
-                                              [-1  0  0 -1] \7
-                                              [-1  0  0  1] \F
-                                              [ 0 -1  1  0] \F
-                                              [ _  0  _  0] \|
-                                              [ 0  _  0  _] \-
-                                              [ 1  0  0  1] \L
-                                              [ 0 -1 -1  0] \L
-                                              [ 0  1 -1  0] \J
-                                              [ 1  0  0 -1] \J))))))
-        min-y (->> dug? keys (map first) (reduce min))
-        max-y (->> dug? keys (map first) (reduce max) inc)
-        min-x (->> dug? keys (map second) (reduce min))
-        max-x (->> dug? keys (map second) (reduce max) inc)]
-    (->> (range min-y max-y)
-         (map (fn [y]
-                (->> (range min-x max-x)
-                     (map (fn [x] (dug? [y x] \space)))
-                     (apply str))))
-         (map (fn [line]
-                (->> line
-                     (re-seq #" +|\||F-*7|F-*J|L-*J|L-*7")
-                     (reduce (fn [[in? so-far] el]
-                               (condp re-find el
-                                 #" +" [in? (if in? (+ so-far (count el)) so-far)]
-                                 #"\|" [(not in?) (inc so-far)]
-                                 #"F-*7" [in? (+ so-far (count el))]
-                                 #"F-*J" [(not in?) (+ so-far (count el))]
-                                 #"L-*J" [in? (+ so-far (count el))]
-                                 #"L-*7" [(not in?) (+ so-far (count el))]))
-                             [false 0])
-                     second)))
-         (reduce + 0))))
+  (solve (:part1 input)))
 
 (defn part2
   [input]
-  input)
+  (solve (:part2 input)))
 
 (lib/check
-  [part1 sample] 62
-  [part1 puzzle] 0
+  #_#_[part1 sample] 62
+  #_#_[part1 puzzle] 48503
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
