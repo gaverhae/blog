c8663c680a0c56594bbd43c4e01999f7106415e5
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-18 14:51:54 +0100

    aoc/23/18: checkpoint

--- advent_of_code/2023/src/t/day18.clj
+++ advent_of_code/2023/src/t/day18.clj
@@ -25,11 +25,62 @@
 
 (defn solve
   [lines]
-  (let [verticals (reduce (fn [acc [dir dist]]
-                            acc)
-                          [[0 0] []]
-                          lines)]
-    verticals))
+  (let [verticals (->> lines
+                       (reduce (fn [[y0 x0 verts] [[dy dx] dist]]
+                                 (let [y1 (+ y0 (* dy dist))
+                                       x1 (+ x0 (* dx dist))]
+                                   [y1 x1 (if (zero? dx)
+                                            (conj verts [x0 y0 y1])
+                                            verts)]))
+                               [0 0 []])
+                       (drop 2)
+                       first
+                       sort
+                       (map (fn [[x y0 y1]]
+                              (if (< y0 y1)
+                                [x y0 y1 false]
+                                [x y1 y0 true]))))
+        entering (->> verticals (filter #(get % 3)))
+        exiting (->> verticals (remove #(get % 3)))]
+    (loop [entering entering
+           p []
+           cells 0]
+      (if (empty? entering)
+        [cells p]
+        (let [[[x y0 y1] & entering] entering
+              exits (->> exiting
+                         (filter (fn [[e-x e-y0 e-y1]]
+                                   (and (> e-x x)
+                                        (or (<= e-y0 y0 e-y1 y1)
+                                            (<= y0 e-y0 e-y1 y1)
+                                            (<= e-y0 y0 y1 e-y1)
+                                            (<= y0 e-y0 y1 e-y1)))))
+                         (map (fn [[e-x e-y0 e-y1]]
+                                [e-x (max e-y0 y0) (min e-y1 y1)])))
+              ys (->> exits
+                      (mapcat (fn [[e-x e-y0 e-y1]] [e-y0 e-y1]))
+                      set sort)
+              intervals (reduce (fn [acc el]
+                                  (prn [:acc acc :el el])
+                                  (conj acc [(inc (second (last acc))) el]))
+                                [(vec (take 2 ys))]
+                                (drop 2 ys))
+              new-cells (->> intervals
+                             (map (fn [[i-y0 i-y1]]
+                                    (let [x-end (->> exits
+                                                     (filter (fn [[e-x e-y0 e-y1]]
+                                                               (or (<= e-y0 i-y0 e-y1 i-y1)
+                                                                   (<= i-y0 e-y0 e-y1 i-y1)
+                                                                   (<= e-y0 i-y0 i-y1 e-y1)
+                                                                   (<= i-y0 e-y0 i-y1 e-y1))))
+                                                     first first)
+                                          width (inc (- x-end x))
+                                          height (inc (- i-y1 i-y0))]
+                                      (* width height))))
+                             (reduce +))]
+          (recur entering
+                 (conj p [[x y0 y1 new-cells]])
+                 0))))))
 
 
 (defn part1
