096ac7c2935e2994caba17266f21718b9934fa01
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-18 06:24:13 +0100

    aoc/23/18: part1 sample wrong

--- advent_of_code/2023/src/t/day18.clj
+++ advent_of_code/2023/src/t/day18.clj
@@ -9,18 +9,51 @@
 
 (defn parse
   [lines]
-  lines)
+  (->> lines
+       (map (fn [line]
+              (let [[dir dist color] (s/split line #" ")
+                    dir (get {"R" [0 1], "D" [1 0], "L" [0 -1], "U" [-1 0]} dir)
+                    dist (parse-long dist)]
+                [dir dist color])))))
 
 (defn part1
   [input]
-  input)
+  (let [dug? (loop [to-dig input
+                    pos [0 0]
+                    dug [[0 0]]]
+               (if (empty? to-dig)
+                 (set dug)
+                 (let [[[dir dist] & to-dig] to-dig]
+                   (recur to-dig
+                          (mapv (fn [c d] (+ c (* d dist))) pos dir)
+                          (reduce (fn [acc dist]
+                                    (conj acc
+                                          (mapv (fn [c d] (+ c (* d dist)))
+                                                pos dir)))
+                                  dug
+                                  (range 1 (inc dist)))))))
+        h (->> dug? (map first) (reduce max) inc)
+        w (->> dug? (map second) (reduce max) inc)
+        excavated (->> (range h)
+                       (mapcat (fn [y]
+                                 (->> (range w)
+                                      (reduce (fn [[dig? dug] x]
+                                                (if (dug? [y x])
+                                                  [(not dig?)
+                                                   (conj dug [y x])]
+                                                  [dig? (if dig?
+                                                          (conj dug [y x])
+                                                          dug)]))
+                                              [false []])
+                                      second))))]
+    (count excavated)))
 
 (defn part2
   [input]
   input)
 
 (lib/check
-  [part1 sample] 0
+  [part1 sample] 62
   #_#_[part1 puzzle] 0
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
