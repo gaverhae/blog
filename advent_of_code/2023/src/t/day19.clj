d665e32f0f5bda938e80693b7e6d01dcda0e9751
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-19 05:57:39 +0100

    aoc/23/19: prep

---
+++ advent_of_code/2023/src/t/day19.clj
@@ -0,0 +1,27 @@
+(ns t.day19
+  (:require [clojure.core.async :as async]
+            [clojure.core.match :refer [match]]
+            [clojure.math :as math]
+            [clojure.set :as set]
+            [clojure.string :as s]
+            [instaparse.core :as insta]
+            [t.lib :as lib])
+  (:import [java.util Arrays]))
+
+(defn parse
+  [lines]
+  lines)
+
+(defn part1
+  [input]
+  input)
+
+(defn part2
+  [input]
+  input)
+
+(lib/check
+  [part1 sample] 0
+  #_#_[part1 puzzle] 0
+  #_#_[part2 sample] 0
+  #_#_[part2 puzzle] 0)
