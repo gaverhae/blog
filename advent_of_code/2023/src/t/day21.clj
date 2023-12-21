6e4e995f788693bc4d16d8f4774b32d2936e26bf
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-21 06:09:52 +0100

    aoc/23/21: part1 sample

--- advent_of_code/2023/src/t/day21.clj
+++ advent_of_code/2023/src/t/day21.clj
@@ -10,18 +10,39 @@
 
 (defn parse
   [lines]
-  lines)
+  {:grid lines
+   :start (->> lines
+               (map-indexed (fn [y line]
+                              (->> line
+                                   (keep-indexed (fn [x c]
+                                                   (when (= c \S)
+                                                     [y x]))))))
+               (apply concat)
+               first)})
 
 (defn part1
-  [input]
-  input)
+  [input max-steps]
+  (loop [step 0
+         ps [(:start input)]]
+    (println ps)
+    (if (= max-steps step)
+      (count ps)
+      (recur (inc step)
+             (->> ps
+                  (mapcat (fn [[y x]]
+                            (for [[dy dx] [[-1 0] [1 0] [0 1] [0 -1]]
+                                  :let [y (+ y dy)
+                                        x (+ x dx)]
+                                  :when (#{\S \.} (get-in input [:grid y x]))]
+                              [y x])))
+                  set)))))
 
 (defn part2
   [input]
   input)
 
 (lib/check
-  [part1 sample] 0
+  [part1 sample 6] 16
   #_#_[part1 puzzle] 0
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
