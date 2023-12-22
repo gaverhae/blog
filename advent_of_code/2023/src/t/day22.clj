38a2cd75fe551e4a3a1b5da8245b57383c6c9ee2
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-22 11:45:42 +0100

    aoc/23/22: bricks seem to fall correctly

--- advent_of_code/2023/src/t/day22.clj
+++ advent_of_code/2023/src/t/day22.clj
@@ -21,35 +21,30 @@
   [[[x0 y0 z0] [x1 y1 z1]]]
   (cond (= [x0 y0 z0] [x1 y1 z1]) [[x0 y0 z0]]
         (= [x0 y0] [x1 y1]) (->> (range z0 (inc z1))
-                                 (map (fn [z] [x0 y0 z])))))
-
+                                 (map (fn [z] [x0 y0 z])))
+        (= [x0 z0] [x1 z1]) (->> (range y0 (inc y1))
+                                 (map (fn [y] [x0 y z0])))
+        (= [y0 z0] [y1 z1]) (->> (range x0 (inc x1))
+                                 (map (fn [x] [x y0 z0])))))
 
 (defn fall
   [bricks]
   (loop [falling (->> bricks
-                      (sort-by (fn [[[x0 y0 z0] [x1 y1 z1]]] (min z0 z1)))
-                      (into clojure.lang.PersistentQueue/EMPTY))
+                      (sort-by (fn [[[x0 y0 z0] [x1 y1 z1]]] (min z0 z1))))
          fallen []
          occupied-cells #{}]
     (if (empty? falling)
       fallen
-      (let [[[[x0 y0 z0] [x1 y1 z1] :as brick] falling] ((juxt peek pop) falling)
+      (let [[[[x0 y0 z0] [x1 y1 z1] :as brick] & falling] falling
             cells (brick->cells brick)]
-        (if (or (zero? z0) (zero? z1))
+        (if (or (= 1 z0) (= 1 z1)
+                (->> cells (map (fn [[x y z]] [x y (dec z)])) (some occupied-cells)))
           (recur falling (conj fallen brick) (reduce conj occupied-cells cells))
-          :todo)))))
+          (recur (cons [[x0 y0 (dec z0)] [x1 y1 (dec z1)]] falling) fallen occupied-cells))))))
 
 (defn part1
   [input]
-  [(->> input
-       (every? (fn [[[x0 y0 z0] [x1 y1 z1]]] (and (<= x0 x1) (<= y0 y1) (<= z0 z1)))))
-   (->> input
-        (every? (fn [[[x0 y0 z0] [x1 y1 z1]]]
-                  (or (= [x0 y0 z0] [x1 y1 z1])
-                      (= [x0 y0] [x1 y1])
-                      (= [x0 z0] [x1 z1])
-                      (= [y0 z0] [y1 z1])))))]
-  #_(fall input))
+  (->> (fall input)))
 
 
 (defn part2
@@ -58,6 +53,6 @@
 
 (lib/check
   [part1 sample] 0
-  [part1 puzzle] 0
+  #_#_[part1 puzzle] 0
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
