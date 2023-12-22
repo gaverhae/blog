56751f2d0a369f7a9bb2d4b50ab36589522473ae
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-22 12:08:03 +0100

    aoc/23/22: part2 (slow: ~20s)

--- advent_of_code/2023/src/t/day22.clj
+++ advent_of_code/2023/src/t/day22.clj
@@ -11,11 +11,12 @@
 (defn parse
   [lines]
   (->> lines
-       (map (fn [line]
-              (-> line
-                  (s/split #"~")
-                  (->> (map #(s/split % #","))
-                       (mapv (fn [s] (mapv parse-long s)))))))))
+       (map-indexed (fn [idx line]
+                      [idx (-> line
+                               (s/split #"~")
+                               (->> (map #(s/split % #","))
+                                    (mapv (fn [s] (mapv parse-long s)))))]))
+       (into {})))
 
 (defn brick->cells
   [[[x0 y0 z0] [x1 y1 z1]]]
@@ -30,17 +31,17 @@
 (defn fall
   [bricks]
   (loop [falling (->> bricks
-                      (sort-by (fn [[[x0 y0 z0] [x1 y1 z1]]] (min z0 z1))))
-         fallen []
+                      (sort-by (fn [[_ [[x0 y0 z0] [x1 y1 z1]]]] (min z0 z1))))
+         fallen {}
          occupied-cells #{}]
     (if (empty? falling)
       fallen
-      (let [[[[x0 y0 z0] [x1 y1 z1] :as brick] & falling] falling
+      (let [[[idx [[x0 y0 z0] [x1 y1 z1] :as brick]] & falling] falling
             cells (brick->cells brick)]
         (if (or (= 1 z0) (= 1 z1)
                 (->> cells (map (fn [[x y z]] [x y (dec z)])) (some occupied-cells)))
-          (recur falling (conj fallen brick) (reduce conj occupied-cells cells))
-          (recur (cons [[x0 y0 (dec z0)] [x1 y1 (dec z1)]] falling) fallen occupied-cells))))))
+          (recur falling (assoc fallen idx brick) (reduce conj occupied-cells cells))
+          (recur (cons [idx [[x0 y0 (dec z0)] [x1 y1 (dec z1)]]] falling) fallen occupied-cells))))))
 
 (defn part1
   [input]
@@ -61,7 +62,7 @@
          (reduce + 0))))
 
 (lib/check
-  #_#_[part1 sample] 5
-  #_#_[part1 puzzle] 468
+  [part1 sample] 5
+  [part1 puzzle] 468
   [part2 sample] 7
-  [part2 puzzle] 0)
+  [part2 puzzle] 75358)
