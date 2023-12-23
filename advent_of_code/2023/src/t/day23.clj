d225b1f1436a930dca16eec03e7b3c9f6f161659
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-23 11:33:05 +0100

    aoc/23/23: part2 sample

--- advent_of_code/2023/src/t/day23.clj
+++ advent_of_code/2023/src/t/day23.clj
@@ -40,11 +40,27 @@
                    (max cost))))))))
 
 (defn part2
-  [input stop-m]
-  input)
+  [input]
+  (let [{:keys [start end grid]} input]
+    (loop [todo [[0 start #{start}]]
+           best-cost-so-far 0]
+      (if (empty? todo)
+        best-cost-so-far
+        (let [[[cost [y x :as pos] seen?] & todo] todo]
+          (recur (->> [[0 1] [0 -1] [1 0] [-1 0]]
+                      (map (fn [[dy dx]] [(+ y dy) (+ x dx)]))
+                      (remove seen?)
+                      (remove (fn [p] (= \# (get-in grid p \#))))
+                      (map (fn [p] [(inc cost) p (conj seen? p)]))
+                      (concat todo)
+                      (sort-by first)
+                      reverse)
+                 (cond-> best-cost-so-far
+                   (= end pos)
+                   (max cost))))))))
 
 (lib/check
   [part1 sample] 94
   [part1 puzzle] 2202
-  #_#_[part2 sample] 0
-  #_#_[part2 puzzle] 0)
+  [part2 sample] 154
+  [part2 puzzle] 0)
