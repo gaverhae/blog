e6f7b5f665adbfd0086c5c2a42029cbe62d48e95
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-24 17:21:17 +0100

    aoc/23/07: fix case warning

--- advent_of_code/2023/src/t/day07.clj
+++ advent_of_code/2023/src/t/day07.clj
@@ -44,29 +44,28 @@
                     (map-indexed (fn [i c] [c i]))
                     (into {}))
         score (fn [hand]
-                (let [numJ (->> hand (filter #{\J}) count)
-                      others (->> hand (remove #{\J}) frequencies)]
+                (let [others (->> hand (remove #{\J}) frequencies)]
 
-                (case [numJ (sort (vals others))]
-                  [0 [5]] 0
-                  [0 [1 4]] 1
-                  [0 [2 3]] 2
-                  [0 [1 1 3]] 3
-                  [0 [1 2 2]] 4
-                  [0 [1 1 1 2]] 5
-                  [0 [1 1 1 1 1]] 6
-                  [1 [4]] 0
-                  [1 [1 3]] 1
-                  [1 [2 2]] 2
-                  [1 [1 1 2]] 3
-                  [1 [1 1 1 1]] 5
-                  [2 [3]] 0
-                  [2 [1 2]] 1
-                  [2 [1 1 1]] 3
-                  [3 [2]] 0
-                  [3 [1 1]] 1
-                  [4 [1]] 0
-                  [5 []] 0)))]
+                (case (sort (vals others))
+                  [5] 0
+                  [1 4] 1
+                  [2 3] 2
+                  [1 1 3] 3
+                  [1 2 2] 4
+                  [1 1 1 2] 5
+                  [1 1 1 1 1] 6
+                  [4] 0
+                  [1 3] 1
+                  [2 2] 2
+                  [1 1 2] 3
+                  [1 1 1 1] 5
+                  [3] 0
+                  [1 2] 1
+                  [1 1 1] 3
+                  [2] 0
+                  [1 1] 1
+                  [1] 0
+                  [] 0)))]
     (->> input
          (map (fn [[a b]] [(score a)
                            (->> a (map values) vec)
