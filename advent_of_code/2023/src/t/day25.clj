15da64b5f44f25e1ae9f86cfaaae8eea41edf833
Gary Verhaegen <gary.verhaegen@gmail.com>
2023-12-27 01:45:59 +0100

    aoc/23/25: try to find out where the bug is
    Whether it's me or "him".
    
    him: https://blog.thomasjungblut.com/graph/mincut/mincut/

--- advent_of_code/2023/src/t/day25.clj
+++ advent_of_code/2023/src/t/day25.clj
@@ -85,26 +85,50 @@
                                         (map (fn [e] [e 1]))
                                         (into {}))
                                    (->> graph keys (map (fn [k] [k 1])) (into {}))]
-           [best-w part-size] [Long/MAX_VALUE 0]]
+           [best-w part-size wrong-part] [Long/MAX_VALUE 0 #{}]
+           part #{}]
       (if (= 1 (count graph))
-        part-size
+        [part-size wrong-part]
         (let [[s t w] (step graph weights)
               new-part (conj part t)]
-          (prn [(lib/now) (lib/duration-since start-time)
-                :graph (count graph)
-                :weights (->> weights vals (reduce + 0))
-                :s-t (get weights #{s t})
-                :cut [s t w]
-                :best best-w part-size])
           (recur (merge-vertices graph weights merged s t)
                  (if (< w best-w)
-                   [w (get merged t)]
-                   [best-w part-size])))))))
+                   [w (get merged t) new-part]
+                   [best-w part-size wrong-part])
+                 new-part))))))
 
 (defn part1
   [input]
-  (let [c (stoer-wagner input)]
+  (let [[c p] (stoer-wagner input)]
+
+    (loop [graph input
+           todo-nodes (->> input keys)]
+      (if (empty? todo-nodes)
+        (prn [:min-graph graph])
+        (let [n (first todo-nodes)
+              g (-> graph
+                    (dissoc n)
+                    (->> (map (fn [[k v]] [k (disj v n)]))
+                         (into {})))
+              [c p] (stoer-wagner g)]
+          (prn [:graph (count graph) :todo (count todo-nodes) :trying n])
+          (if (= c (count p))
+            (recur graph (rest todo-nodes))
+            (recur g (keys g))))))
+
     (* c (- (count input) c))))
+
+(stoer-wagner {"qnr" #{"nvd" "cmg"},
+             "bvb" #{"xhk" "cmg"},
+             "jqt" #{"nvd" "xhk"}
+             "nvd" #{"qnr" "jqt" "cmg" "pzl"}
+             "xhk" #{"bvb" "jqt"},
+             "cmg" #{"qnr" "bvb" "nvd"},
+             "pzl" #{"nvd"}})
+[1 #{"bvb" "pzl"}]
+
+(stoer-wagner @sample)
+[9 #{"lhk" "qnr" "lsr" "rzs" "frs" "nvd" "cmg" "pzl" "rsh"}]
 
 (defn part2
   [input]
@@ -112,6 +136,6 @@
 
 (lib/check
   [part1 sample] 54
-  [part1 puzzle] 527790
+  #_#_[part1 puzzle] 527790
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
