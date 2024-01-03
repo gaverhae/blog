3423e656db8b81f81ff130550ece6a2fc6245e27
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-01-04 00:22:24 +0100

    aoc/23/21: go back to plain loop

--- advent_of_code/2023/src/t/day21.clj
+++ advent_of_code/2023/src/t/day21.clj
@@ -84,68 +84,56 @@
 (defn part2
   [input max-steps]
   (let [f (walk-one-map (:grid input))
-        grids (loop [todo [[0 [0 0] {0 #{(:start input)}}]]
-                     grids {}]
-                (if (= (count grids) 13)
-                  grids
-                  (let [[[steps-so-far [gy gx :as grid] entry-points] & todo] todo]
-                    (if (grids grid)
-                      (recur todo grids)
-                      (let [[grid-filled max-s-in-grid precomputed-increases grid-exits] (f entry-points)]
-                        (recur (->> grid-exits
-                                    (map (fn [[[dy dx] m s-min]]
-                                           [(+ s-min steps-so-far) [(+ gy dy) (+ gx dx)] m]))
-                                    (remove (fn [[_ grid _]] (grids grid)))
-                                    (reduce conj todo)
-                                    (sort-by first))
-                               (assoc grids [gy gx] {:grid-filled grid-filled
-                                                     :max-s max-s-in-grid
-                                                     :precomputed precomputed-increases
-                                                     :exits (->> grid-exits
-                                                                 (map (fn [[direction points s-min]]
-                                                                        [direction (+ s-min (->> points (map first) (reduce min)))]))
-                                                                 (into {}))})))))))
-        ;; we now have something like
-        ;;                   G(-2, 0)
-        ;;          G(-1,-1) g(-1, 0) G(-1, 1)
-        ;; G( 0,-2) g( 0,-1) g( 0, 0) g( 0, 1) G(0, 2)
-        ;;          G( 1,-1) g( 1, 0) G( 1, 1)
-        ;;                   G( 2, 0)
-        ;; where the small gs are unique and the big Gs repeat ad infinitum.
-        ;;
-        ;; So the total area will be given by however many Gs it takes to fill
-        ;; up, plus special cases for the frontier.
-        ;; To start, let's compute how many grids we have in each direction:
-        #_#_max-cards (->> [[-1 0] [0 1] [1 0] [0 -1]]
-                       (map (fn [[dy dx]]
-                              [[dy dx] (-> max-steps
-                                           (- (get-in grids [[0 0] :exits [dy dx]]))
-                                           (- (get-in grids [[dy dx] :exits [dy dx]]))
-                                           (/ (get-in grids [[(* 2 dy) (* 2 dx)] :exits [dy dx]]))
-                                           long)]))
-                       (into {}))
-        #_#_middle-line (let [max-left (get max-cards [0 -1])
-                          max-right (get max-cards [0 1])]
-                      {0 (+ (get-in grids [[0 0] :precomputed 0])
-                            (get-in grids [[0 -1] :precomputed (-> (get-in grids [[0 0] :exits [0 -1]]) (mod 2))])
-                            (if (-> (get-in grids [[0 -1] :exits [0 -1]]) (mod 2) (= 0))
-                              (* (max-cards [-1 0]) (get-in grids [[0 -1] :precomputed]))))})]
-    (cond (< max-steps (-> grids (get [0 0]) :exits (->> (map (fn [[_ s]] s)) (reduce min))))
-          (part1 input max-steps)
-          (< max-steps (+ (-> grids (get [0 0]) :exits (->> (map (fn [[_ s]] s)) (reduce min)))
-                          (->> [[0 -1] [1 0] [-1 0] [0 1]]
-                               (map (fn [d] (->> (grids d) :exits (map (fn [[_ s]] s)) (reduce min)))))))
-          (+ (part1 input max-steps)))))
-
-
+        _ (println (format "Starting at: %s" (subs (str (java.time.LocalDateTime/now)) 0 19)))
+        start-time (System/currentTimeMillis)]
+    (loop [todo [[0 [0 0] {0 #{(:start input)}}]]
+           filled [0 0]
+           done? #{}
+           n 0]
+      (if (empty? todo)
+        (filled (mod max-steps 2))
+        (let [[[steps-so-far [gy gx :as grid] entry-point] & todo] todo]
+          (when (zero? (mod n 10000))
+            (let [now (System/currentTimeMillis)
+                  d (- now start-time)]
+              (println (format "%02d:%02d:%02d[%10d]: %s"
+                               (-> d (quot 1000) (quot 60) (quot 24) (mod 60))
+                               (-> d (quot 1000) (quot 60) (mod 60))
+                               (-> d (quot 1000) (mod 60))
+                               steps-so-far
+                               [:todo (count todo) :done? (count done?)]))))
+          (if (done? grid)
+            (recur todo filled done? (inc n))
+            (let [[grid-filled max-s-in-grid precomputed-increases grid-exits] (f entry-point)]
+              (recur (->> grid-exits
+                          (keep (fn [[[dy dx] m s-min]]
+                                  (when (<= s-min (- max-steps steps-so-far))
+                                    [(+ s-min steps-so-far) [(+ gy dy) (+ gx dx)] m])))
+                          (remove (fn [[_ grid _]] (done? grid)))
+                          (reduce conj todo)
+                          (sort-by first))
+                     (if (<= (+ steps-so-far max-s-in-grid) max-steps)
+                       (let [m (mod steps-so-far 2)]
+                         (-> filled
+                             (update 0 + (get precomputed-increases m))
+                             (update 1 + (get precomputed-increases (- 1 m)))))
+                       (reduce (fn [acc [_ s]]
+                                 (if (<= (+ s steps-so-far) max-steps)
+                                   (update acc (mod (+ s steps-so-far) 2) inc)
+                                   acc))
+                               filled
+                               grid-filled))
+                     (conj done? grid)
+                     (inc n)))))))))
 
 (lib/check
   [part1 sample 6] 16
   [part1 puzzle 64] 3639
-  [part2 sample 1] 2
-  [part2 sample 6] 16
-  [part2 sample 10] 50
+  #_#_[part2 sample 6] 16
+  #_#_[part2 sample 10] 50
   #_#_[part2 sample 50] 1594
+  #_#_[part2 sample 1] 2
+  #_#_[part2 sample 10] 50
   #_#_[part2 sample 100] 6536
   #_#_[part2 sample 200] 26538
   #_#_[part2 sample 300] 59895
@@ -157,9 +145,9 @@
   #_#_[part2 puzzle 100] 8829
   #_#_[part2 puzzle 200] 34889
   #_#_[part2 puzzle 400] 138314
-  #_#_[part2 puzzle 1000] 862969
-  #_#_[part2 puzzle 2000] 3445428
-  #_#_[part2 puzzle 5000] 21527301
+  [part2 puzzle 1000] 862969
+  [part2 puzzle 2000] 3445428
+  [part2 puzzle 5000] 21527301
   #_#_[part2 puzzle 26501365] 0)
 
 (defn benchmark
