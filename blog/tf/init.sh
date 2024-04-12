25ae07c24280932dbb58bc533c36acf287f45d16
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-04-12 21:02:39 +0200

    update to better nginx

--- blog/tf/init.sh
+++ blog/tf/init.sh
@@ -4,16 +4,19 @@ set -euo pipefail
 apt-get update
 apt-get upgrade -q -y
 
-apt-get install -q -y nginx certbot python3-certbot-nginx awscli
+apt-get install -q -y nginx certbot python3-certbot-nginx
+snap install aws-cli --classic
 
 ## Disable IP website
 
 cat <<'END_DISABLE_IP' > /etc/nginx/sites-available/default
 server {
   listen 80 default_server;
+  listen [::]:80 default_server;
   listen 443 ssl default_server;
-  ssl_certificate /etc/letsencrypt/live/${tofu_domain}/fullchain.pem;
-  ssl_certificate_key /etc/letsencrypt/live/${tofu_domain}/privkey.pem;
+  listen [::]:443 ssl default_server;
+  ssl_reject_handshake on;
+  server_name _;
   return 444;
 }
 END_DISABLE_IP
