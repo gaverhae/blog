eb2735016e8d4d3f304c03459e12d0c239caf0f9
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-10-02 21:51:31 +0200

    aws: remove virtual network

--- tf/main.tf
+++ tf/main.tf
@@ -86,65 +86,6 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_route_table" "open" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-}
-
-resource "aws_subnet" "open" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.0.0/24"
-}
-
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.open.id
-  route_table_id = aws_route_table.open.id
-}
-
-resource "aws_security_group" "allow_http" {
-  name   = "allow_http"
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "allow_ssh" {
-  name   = "allow_ssh"
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_s3_bucket" "bucket" {
   bucket = "cuddly-octo-palm-tree"
 }
